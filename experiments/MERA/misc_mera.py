bb2ea1d556ef192bce97a5e72fbd2edc9ce053b6
Martin <martin.ganahl@gmail.com>
2019-05-08 10:05:35 -0400

    __future__ import

--- experiments/MERA/misc_mera.py
+++ experiments/MERA/misc_mera.py
@@ -16,7 +16,9 @@
 """
 miscellaneous functions needed for MERA optimization
 """
-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
 import sys
 sys.path.append('../../')
 import tensorflow as tf
