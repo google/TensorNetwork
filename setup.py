e1c6b1f765f698cbf16370575078dc6f8778b3ba
Chase Roberts <chaseriley@google.com>
2019-05-10 10:57:51 -0700

    Fix setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 # Read in requirements
-requirements = [r.strip() for requirement in
+requirements = [requirement.strip() for requirement in
                 open('requirements.txt').readlines()]
 
 setup(
