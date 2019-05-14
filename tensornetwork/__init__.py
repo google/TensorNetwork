49f47906de3e12e5f351260e83f6fca0867480b4
kshithijiyer <kshithij.ki@gmail.com>
2019-05-14 20:52:09 +0530

    Fixing issues in __init__.py after splitting.
    Signed-off-by: kshithijiyer <kshithij.ki@gmail.com>

--- tensornetwork/__init__.py
+++ tensornetwork/__init__.py
@@ -1,4 +1,6 @@
 from __future__ import absolute_import
-from tensornetwork.tensornetwork import TensorNetwork, Node, Edge
+from tensornetwork.network import TensorNetwork
+from tensornetwork.node import Node
+from tensornetwork.edge import Edge
 from tensornetwork.ncon_interface import ncon, ncon_network
 from tensornetwork import contractors 
