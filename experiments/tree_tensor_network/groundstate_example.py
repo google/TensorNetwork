1ee081a088d8525d46e7fbb6db493b113c766f25
Ashley Milsted <ashmilsted@gmail.com>
2019-05-14 13:05:30 -0400

    Fix imports and add some documentation.

--- experiments/tree_tensor_network/groundstate_example.py
+++ experiments/tree_tensor_network/groundstate_example.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Trotter evolution of exact wavefunctions: Example script."""
+"""Tree Tensor Network for the groundstate of the Transverse Ising chain."""
 
 from __future__ import absolute_import
 from __future__ import division
