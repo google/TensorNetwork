4e0de67b1594c26204d8d662bce6222fef36dbdc
stavros11 <35475381+stavros11@users.noreply.github.com>
2019-05-14 20:49:39 -0700

    Add test for disconnected

--- tensornetwork/contractors/stochastic_contractor_test.py
+++ tensornetwork/contractors/stochastic_contractor_test.py
@@ -20,49 +20,49 @@ from __future__ import print_function
 
 import numpy as np
 import tensorflow as tf
+tf.enable_v2_behavior()
 
 from tensornetwork import tensornetwork
 from tensornetwork.contractors import stochastic_contractor
 
 
-class SimpleStochasticUtilsTest(tf.test.TestCase):
-
-  def test_edge_cost(self):
-    net = tensornetwork.TensorNetwork()
-    a = net.add_node(np.random.random([4, 5, 2]))
-    b = net.add_node(np.random.random([3, 2, 3]))
-    e = net.connect(a[2], b[1])
-    self.assertEqual(140, stochastic_contractor.edge_cost(e))
-
-  def test_get_random_edge(self):
-    net = tensornetwork.TensorNetwork()
-    a = net.add_node(np.random.random([4, 5, 2]))
-    b = net.add_node(np.random.random([3, 2, 3]))
-    e = net.connect(a[2], b[1])
-    self.assertIs(e, stochastic_contractor.get_random_edge(net))
-
-  def test_get_random_edge_with_trace_edge(self):
-    net = tensornetwork.TensorNetwork()
-    a = net.add_node(np.random.random([4, 5, 2]))
-    b = net.add_node(np.random.random([3, 2, 3]))
-    e = net.connect(a[2], b[1])
-    net.connect(b[0], b[2])
-    self.assertIs(e, stochastic_contractor.get_random_edge(net))
-
-
 class SimpleStochasticTest(tf.test.TestCase):
   # TODO: More contraction cases
 
+  def test_edge_cost(self):
+    net = tensornetwork.TensorNetwork()
+    a = net.add_node(np.ones([4, 5, 2]))
+    b = net.add_node(np.ones([3, 2, 3]))
+    e = net.connect(a[2], b[1])
+    self.assertEqual(140, stochastic_contractor.edge_cost(e))
+
   def test_contraction_sanity(self):
     net = tensornetwork.TensorNetwork()
-    a = net.add_node(np.random.random([4, 5, 2]))
-    b = net.add_node(np.random.random([3, 2, 3]))
+    a = net.add_node(np.ones([4, 5, 2]))
+    b = net.add_node(np.ones([3, 2, 3]))
     net.connect(a[2], b[1])
     net.connect(b[0], b[2])
     net = stochastic_contractor.stochastic(net, 2)
-    res = net.get_final_node().get_tensor()
-    self.assertTupleEqual(tuple(res.shape), (4, 5))
+    res = net.get_final_node()
+    self.assertAllClose(res.get_tensor(), 6 * np.ones([4, 5]))
 
+  def test_contraction_disconnected(self):
+    net = tensornetwork.TensorNetwork()
+    a = net.add_node(np.ones([4, 5, 2]))
+    b = net.add_node(np.ones([3, 2, 3]))
+    edge1 = a[0]
+    net.connect(a[2], b[1])
+    net.connect(b[0], b[2])
+    c = net.add_node(np.ones([3, 4]))
+    d = net.add_node(np.ones([4, 3]))
+    edge2 = c[0]
+    net.connect(c[1], d[0])
 
+    net = stochastic_contractor.stochastic(net, 2)
+    node1, node2 = edge1.node1, edge2.node1
+    self.assertAllClose(node1.get_tensor(), 6 * np.ones([4, 5]))
+    self.assertAllClose(node2.get_tensor(), 4 * np.ones([3, 3]))
+
+ 
 if __name__ == '__main__':
   tf.test.main()
