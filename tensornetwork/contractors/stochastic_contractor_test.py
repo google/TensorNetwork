1296d870b6190c88baeaf5c5b4760e1d0c5fe52d
stavros11 <35475381+stavros11@users.noreply.github.com>
2019-05-15 17:50:34 -0700

    Add tests for the new functions

--- tensornetwork/contractors/stochastic_contractor_test.py
+++ tensornetwork/contractors/stochastic_contractor_test.py
@@ -22,34 +22,50 @@ import numpy as np
 import tensorflow as tf
 tf.enable_v2_behavior()
 
-from tensornetwork import tensornetwork
+from tensornetwork import network
 from tensornetwork.contractors import stochastic_contractor
 
 
-class SimpleStochasticTest(tf.test.TestCase):
+class StochasticTest(tf.test.TestCase):
   # TODO: More contraction cases
 
-  def test_edge_cost(self):
-    net = tensornetwork.TensorNetwork()
-    a = net.add_node(np.ones([4, 5, 2]))
-    b = net.add_node(np.ones([3, 2, 3]))
-    e = net.connect(a[2], b[1])
-    cost, shared_edges = stochastic_contractor.edge_cost(e)
-    self.assertEqual(140, cost)
-    self.assertSetEqual({e}, shared_edges)
+  def test_find_parallel(self):
+    net = network.TensorNetwork()
+    a = net.add_node(np.ones([4, 5, 2, 3]))
+    b = net.add_node(np.ones([3, 2, 3, 5]))
+    net.connect(a[2], b[1])
+    net.connect(a[1], b[3])
+    net.connect(a[3], b[0])
+    parallel_edges, parallel_dim = stochastic_contractor.find_parallel(a[2])
+    self.assertSetEqual(parallel_edges, {a[1], a[2], a[3]})
+    self.assertEqual(parallel_dim, 30)
+
+  def test_contract_trace_edges(self):
+    net = network.TensorNetwork()
+    a = net.add_node(np.ones([4, 5, 2, 3, 4]))
+    b = net.add_node(np.ones([3, 2, 3, 5]))
+    net.connect(a[2], b[1])
+    net.connect(a[1], b[3])
+    net.connect(b[0], b[2])
+    net.connect(a[0], a[4])
+    e = a[1]
+    net, node_sizes, node_sizes_none = stochastic_contractor.contract_trace_edges(net)
+    self.assertDictEqual(node_sizes, {e.node1: 30, e.node2: 10})
+    self.assertDictEqual(node_sizes_none, dict())
 
   def test_contraction_sanity(self):
-    net = tensornetwork.TensorNetwork()
+    net = network.TensorNetwork()
     a = net.add_node(np.ones([4, 5, 2]))
     b = net.add_node(np.ones([3, 2, 3]))
     net.connect(a[2], b[1])
     net.connect(b[0], b[2])
     net = stochastic_contractor.stochastic(net, 2)
+    net.check_correct()
     res = net.get_final_node()
     self.assertAllClose(res.get_tensor(), 6 * np.ones([4, 5]))
 
-  def test_contraction_multiple_edges(self):
-    net = tensornetwork.TensorNetwork()
+  def test_contraction_parallel_edges(self):
+    net = network.TensorNetwork()
     a = net.add_node(np.ones([4, 5, 2]))
     b = net.add_node(np.ones([3, 2, 3, 5]))
     c = net.add_node(np.ones([4,]))
@@ -58,11 +74,12 @@ class SimpleStochasticTest(tf.test.TestCase):
     net.connect(a[1], b[3])
     net.connect(a[0], c[0])
     net = stochastic_contractor.stochastic(net, 2)
+    net.check_correct()
     res = net.get_final_node()
     self.assertAllClose(res.get_tensor(), 120)
 
   def test_contraction_disconnected(self):
-    net = tensornetwork.TensorNetwork()
+    net = network.TensorNetwork()
     a = net.add_node(np.ones([4, 5, 2]))
     b = net.add_node(np.ones([3, 2, 3]))
     edge1 = a[0]
@@ -72,12 +89,11 @@ class SimpleStochasticTest(tf.test.TestCase):
     d = net.add_node(np.ones([4, 3]))
     edge2 = c[0]
     net.connect(c[1], d[0])
-
     net = stochastic_contractor.stochastic(net, 2)
+    net.check_correct(check_connected=False)
     node1, node2 = edge1.node1, edge2.node1
     self.assertAllClose(node1.get_tensor(), 6 * np.ones([4, 5]))
     self.assertAllClose(node2.get_tensor(), 4 * np.ones([3, 3]))
-
 
 if __name__ == '__main__':
   tf.test.main()
