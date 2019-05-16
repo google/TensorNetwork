16e6b4dcce594717f95fe8ebe213024ce47c5059
stavros11 <35475381+stavros11@users.noreply.github.com>
2019-05-15 18:41:39 -0700

    Add test_draw_tree with patch

--- tensornetwork/nxwrapper_test.py
+++ tensornetwork/nxwrapper_test.py
@@ -17,15 +17,16 @@ from __future__ import division
 from __future__ import print_function
 
 import numpy as np
+from unittest import mock
 import tensorflow as tf
 # pylint: disable=g-import-not-at-top
-from tensornetwork import tensornetwork, nxwrapper
+from tensornetwork import network, nxwrapper
 
 
 class TreePlotWrapperTest(tf.test.TestCase):
 
   def test_wrapping_mps(self):
-    net = tensornetwork.TensorNetwork()
+    net = network.TensorNetwork()
     mps_nodes = [net.add_node(np.ones([2, 3]), name="W0")]
     for i in range(1, 5):
       mps_nodes.append(net.add_node(np.ones([3, 2, 3]), name="W{}".format(i)))
@@ -40,7 +41,7 @@ class TreePlotWrapperTest(tf.test.TestCase):
     self.assertSetEqual(nodes_set, plotter.node_set)
 
   def test_wrapping_bintree(self):
-    net = tensornetwork.TensorNetwork()
+    net = network.TensorNetwork()
     tree_levels = []
     root = net.add_node(np.eye(4), name="R")
     leaves = [net.add_node(np.ones([4,]), name="W1{}".format(i))
@@ -56,7 +57,7 @@ class TreePlotWrapperTest(tf.test.TestCase):
     self.assertDictEqual(pos, plotter.pos)
 
   def test_set_options(self):
-    net = tensornetwork.TensorNetwork()
+    net = network.TensorNetwork()
     node = net.add_node(np.ones(5))
     plotter = nxwrapper.TreePlotWrapper(node)
     new_options = {"y_margin": 2.0, "sizes": 50, "top_to_bottom": False}
@@ -67,13 +68,25 @@ class TreePlotWrapperTest(tf.test.TestCase):
     self.assertDictEqual(plotter.options, full_options)
 
   def test_colorsizemaps(self):
-    net = tensornetwork.TensorNetwork()
+    net = network.TensorNetwork()
     node = net.add_node(np.ones(5))
     node2 = net.add_node(np.ones(5))
     net.connect(node[0], node2[0])
     plotter = nxwrapper.TreePlotWrapper(node)
     self.assertListEqual(plotter.colormap, ["red", "red"])
     self.assertListEqual(plotter.sizemap, [1000, 1000])
+
+  @mock.patch("tensornetwork.nxwrapper.nx")
+  def test_draw_tree(self, mock_nx):
+    net = network.TensorNetwork()
+    tree_levels = []
+    root = net.add_node(np.eye(4), name="R")
+    leaves = [net.add_node(np.ones([4,]), name="W1{}".format(i))
+              for i in range(2)]
+    net.connect(root[0], leaves[0][0])
+    net.connect(root[1], leaves[1][0])
+    nxwrapper.draw_tree(root)
+    self.assertTrue(mock_nx.draw_networkx.called)
 
 
 if __name__ == "__main__":
