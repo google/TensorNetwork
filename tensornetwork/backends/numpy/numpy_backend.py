5ce934a221809075fd55f4c584e3a6fbe585d892
stavros11 <35475381+stavros11@users.noreply.github.com>
2019-05-18 18:25:46 -0700

    Fix typo in directory

--- tensornetwork/backends/numpy/numpy_backend.py
+++ tensornetwork/backends/numpy/numpy_backend.py
@@ -17,6 +17,7 @@ from __future__ import division
 from __future__ import print_function
 from typing import Optional, Any, Sequence, Tuple
 from tensornetwork.backends import base_backend
+from 
 
 Tensor = Any
 
