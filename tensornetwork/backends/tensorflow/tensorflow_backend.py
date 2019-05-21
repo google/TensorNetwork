0ddd90aceb911d3d648af041f621a05a1488a5c1
Chase Roberts <chaseriley@google.com>
2019-05-20 21:07:11 -0700

    Moved tensordot2 import

--- tensornetwork/backends/tensorflow/tensorflow_backend.py
+++ tensornetwork/backends/tensorflow/tensorflow_backend.py
@@ -17,7 +17,6 @@ from __future__ import division
 from __future__ import print_function
 from typing import Optional, Any, Sequence, Tuple
 from tensornetwork.backends import base_backend
-from tensornetwork.backends.tensorflow import tensordot2
 
 # This might seem bad, but pytype treats tf.Tensor as Any anyway, so
 # we don't actually lose anything by doing this.
@@ -34,12 +33,14 @@ class TensorFlowBackend(base_backend.BaseBackend):
     except ImportError:
       raise AssertionError("tensorflow is not installed.")
     from tensornetwork.backends.tensorflow import decompositions
+    from tensornetwork.backends.tensorflow import tensordot2
+    self.tensordot2 = tensordot2
     self.tf = tensorflow
     self.decompositions = decompositions
     self.name = "tensorflow"
 
   def tensordot(self, a: Tensor, b: Tensor, axes: Sequence[Sequence[int]]):
-    return tensordot2.tensordot(a, b, axes)
+    return self.tensordot2.tensordot(a, b, axes)
 
   def reshape(self, tensor: Tensor, shape: Tensor):
     return self.tf.reshape(tensor, shape)
@@ -78,7 +79,7 @@ class TensorFlowBackend(base_backend.BaseBackend):
     return self.tf.trace(tensor)
 
   def outer_product(self, tensor1: Tensor, tensor2: Tensor) -> Tensor:
-    return tensordot2.tensordot(tensor1, tensor2, 0)
+    return self.tensordot2.tensordot(tensor1, tensor2, 0)
 
   def einsum(self, expression: str, *tensors: Tensor) -> Tensor:
     return self.tf.einsum(expression, *tensors)
