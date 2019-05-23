50ca3ce6d4ad44fdce421898fbf69ee652480d13
Chase Roberts <chaseriley@google.com>
2019-05-23 13:31:45 -0700

    0.0.2 release

--- tensornetwork/__init__.py
+++ tensornetwork/__init__.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 from tensornetwork.network import TensorNetwork
 from tensornetwork.network_components import Node, Edge
 from tensornetwork.ncon_interface import ncon, ncon_network
+from tensornetwork.version import __version__
 from tensornetwork import contractors
 from tensornetwork import config
 from typing import Text
