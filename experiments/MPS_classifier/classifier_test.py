5324f3fd82dace26b75c3baf4f083f3beccdf8c1
stavros11 <35475381+stavros11@users.noreply.github.com>
2019-06-13 19:11:54 +0200

    Remove unused import

--- experiments/MPS_classifier/classifier_test.py
+++ experiments/MPS_classifier/classifier_test.py
@@ -16,7 +16,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import pytest
 import numpy as np
 import tensorflow as tf
 from experiments.MPS_classifier import classifier
