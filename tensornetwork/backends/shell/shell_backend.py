11313b61dd7962efe3739f1be4a75f7b42322d64
stavros11 <35475381+stavros11@users.noreply.github.com>
2019-06-15 21:26:40 +0300

    Add shell backend

---
+++ tensornetwork/backends/shell/shell_backend.py
@@ -0,0 +1,108 @@
+# Copyright 2019 The TensorNetwork Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from typing import Optional, Sequence, Tuple, List
+from tensornetwork.backends import base_backend
+
+# Treat tensors as tuples that carry the real tensor's shape.
+# Conversion from tensor to tuple will happen somewhere else?
+Tensor = Tuple
+
+
+class ShellBackend(base_backend.BaseBackend):
+  """See base_backend.BaseBackend for documentation."""
+
+  def __init__(self):
+    self.name = "shell"
+
+  def tensordot(self, a: Tensor, b: Tensor, axes: Sequence[Sequence[int]]):
+    # Does not work when axis < 0
+    gen_a = (x for i, x in enumerate(a) if i not in axes[0])
+    gen_b = (x for i, x in enumerate(b) if i not in axes[1])
+    return tuple(self._concat_generators(gen_a, gen_b))
+
+  def _concat_generators(self, *gen):
+    """Concatenates Python generators."""
+    for g in gen:
+      yield from g
+
+  def reshape(self, tensor: Tensor, shape: Tensor):
+    return shape
+
+  def transpose(self, tensor, perm):
+    return tuple(tensor[i] for i in perm)
+
+  def svd_decomposition(self,
+                        tensor: Tensor,
+                        split_axis: int,
+                        max_singular_values: Optional[int] = None,
+                        max_truncation_error: Optional[float] = None
+                       ) -> Tuple[Tensor, Tensor, Tensor, Tensor]:
+    raise NotImplementedError("SVD shape cannot be calculated without"
+                              "explicit tensor values.")
+
+  def concat(self, values: Sequence[Tensor], axis: int) -> Tensor:
+    # Does not work when axis < 0
+    concat_dim = len(values) * values[0][axis]
+    return values[0][:axis] + (concat_dim,) + values[0][axis + 1:]
+
+  def shape(self, tensor: Tensor) -> Tensor:
+    return tensor
+
+  def prod(self, values: Tensor) -> Tensor:
+    return values
+
+  def sqrt(self, tensor: Tensor) -> Tensor:
+    return tensor
+
+  def diag(self, tensor: Tensor) -> Tensor:
+    return (3 - len(tensor)) * tensor
+
+  def convert_to_tensor(self, tensor: Tensor) -> Tensor:
+    return tensor
+
+  def trace(self, tensor: Tensor) -> Tensor:
+    return tensor[:-2]
+
+  def outer_product(self, tensor1: Tensor, tensor2: Tensor) -> Tensor:
+    return tensor1 + tensor2
+
+  def einsum(self, expression: str, *tensors: Tensor) -> Tensor:
+    expr_list = expression.split(",")
+    expr_list[-1], res = expr_list[-1].split("->")
+    shape = []
+    for char in res:
+      i, ind = self._find_char(expr_list, char)
+      shape.append(tensors[i][ind])
+    return tuple(shape)
+
+  def _find_char(self, expr_list: List[str], char: str):
+    """Finds character in einsum tensor expression.
+
+    Args:
+      expr_list: List with expression for input tensors in einsum.
+      char: One character string (letter) that corresponds to a specific
+        einsum component.
+
+    Returns:
+      i: Index of the tensor that has `char` components.
+      ind: Index of `char` in the i-th expression string.
+    """
+    for i, expr in enumerate(expr_list):
+      ind = expr.find(char)
+      if ind != -1:
+        return i, ind
---
+++ tensornetwork/backends/shell/shell_backend_test.py
@@ -0,0 +1,87 @@
+# Copyright 2019 The TensorNetwork Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+import numpy as np
+from tensornetwork.backends.shell import shell_backend
+
+
+def test_tensordot_matmul():
+  bkd = shell_backend.ShellBackend()
+  a = np.ones((6, 4, 3, 10))
+  b = np.ones((6, 4, 10, 5))
+  axes = [[3], [2]]
+  c = bkd.tensordot(a.shape, b.shape, axes=axes)
+  assert c == np.tensordot(a, b, axes=axes).shape
+
+def test_tensordot():
+  bkd = shell_backend.ShellBackend()
+  a = np.ones((6, 4, 3, 10))
+  b = np.ones((4, 6, 10, 5))
+  axes = [[0, 1, 3], [1, 0, 2]]
+  c = bkd.tensordot(a.shape, b.shape, axes=axes)
+  assert c == np.tensordot(a, b, axes=axes).shape
+
+def test_reshape():
+  bkd = shell_backend.ShellBackend()
+  shape = bkd.reshape((5, 6), (5, 3, 2))
+  assert shape == (5, 3, 2)
+
+def test_transpose():
+  bkd = shell_backend.ShellBackend()
+  shape = bkd.transpose((5, 3, 2), [0, 2, 1])
+  assert shape == (5, 2, 3)
+
+def test_concat():
+  bkd = shell_backend.ShellBackend()
+  values = [(5, 3, 2), (5, 3, 2), (5, 3, 2)]
+  shape = bkd.concat(values, axis=2)
+  assert shape == (5, 3, 6)
+
+def test_trace():
+  bkd = shell_backend.ShellBackend()
+  shape = bkd.trace((5, 3, 6, 2, 2))
+  assert shape == (5, 3, 6)
+
+def test_outer_product():
+  bkd = shell_backend.ShellBackend()
+  shape = bkd.outer_product((5, 2, 3), (6, 2, 4))
+  assert shape == (5, 2, 3, 6, 2, 4)
+
+def test_einsum_batch():
+  bkd = shell_backend.ShellBackend()
+  a = np.ones((6, 4, 3, 10))
+  b = np.ones((6, 4, 10, 5))
+  expr = "abij,abjk->abik"
+  c = bkd.einsum(expr, a.shape, b.shape)
+  assert c == np.einsum(expr, a, b).shape
+
+def test_einsum():
+  bkd = shell_backend.ShellBackend()
+  a = np.ones((6, 4, 3, 10, 8))
+  b = np.ones((6, 7, 4, 10, 5))
+  expr = "abicj,akbcl->kilj"
+  c = bkd.einsum(expr, a.shape, b.shape)
+  assert c == np.einsum(expr, a, b).shape
+
+def test_einsum_three_tensors():
+  bkd = shell_backend.ShellBackend()
+  a = np.ones((6, 4, 3, 10, 8))
+  b = np.ones((6, 7, 4, 10, 5))
+  c = np.ones((5, 3))
+  expr = "abicj,akbcl,li->jk"
+  s = bkd.einsum(expr, a.shape, b.shape, c.shape)
+  assert s == np.einsum(expr, a, b, c).shape
