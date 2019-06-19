b903203d817ce229f2c7766a4e5865c89c4f5a5d
Martin <martin.ganahl@gmail.com>
2019-06-19 18:39:19 -0400

    removed unneccessary imports

--- experiments/MPS/matrixproductstates.py
+++ experiments/MPS/matrixproductstates.py
@@ -16,16 +16,10 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-import sys
-import warnings
-import copy
 import numpy as np
-import scipy as sp
 import tensorflow as tf
-
 from experiments.MPS import misc_mps
 from experiments.MPS import tensornetwork_tools as tnt
-
 
 def is_mps_tensor(tensor):
     return tf.equal(tf.rank(tensor), 3)
