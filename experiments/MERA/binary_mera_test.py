3205e83270d694e1bfb83235593589aafbdca3c0
Martin <martin.ganahl@gmail.com>
2019-06-19 12:16:19 -0400

    removed comment

--- experiments/MERA/binary_mera_test.py
+++ experiments/MERA/binary_mera_test.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-functions for binary MERA optimization
 """
 import tensorflow as tf
 import numpy as np
