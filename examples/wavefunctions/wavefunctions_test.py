348133c640ee43ac6bc7a3e3ff27572eb70a355d
Ashley Milsted <ashmilsted@gmail.com>
2019-06-25 15:04:56 -0400

    Add euclidean tests and test defun versions.

--- examples/wavefunctions/wavefunctions_test.py
+++ examples/wavefunctions/wavefunctions_test.py
@@ -55,8 +55,10 @@ def test_apply_op(num_sites):
     np.testing.assert_allclose(res, 1.0)
 
 
-@pytest.mark.parametrize("num_sites,phys_dim", [(2, 3), (5, 2)])
-def test_evolve_trotter(num_sites, phys_dim):
+@pytest.mark.parametrize(
+    "num_sites,phys_dim,graph",
+    [(2, 3, False), (2, 3, True), (5, 2, False)])
+def test_evolve_trotter(num_sites, phys_dim, graph):
     psi = tf.complex(
         tf.random_normal([phys_dim] * num_sites, dtype=tf.float64),
         tf.random_normal([phys_dim] * num_sites, dtype=tf.float64))
@@ -70,7 +72,10 @@ def test_evolve_trotter(num_sites, phys_dim):
     norm1 = wavefunctions.inner(psi, psi)
     en1 = sum(wavefunctions.expval(psi, H[i], i) for i in range(num_sites - 1))
 
-    psi, t = wavefunctions.evolve_trotter(psi, H, 0.001, 10)
+    if graph:
+        psi, t = wavefunctions.evolve_trotter_defun(psi, H, 0.001, 10)
+    else:
+        psi, t = wavefunctions.evolve_trotter(psi, H, 0.001, 10)
 
     norm2 = wavefunctions.inner(psi, psi)
     en2 = sum(wavefunctions.expval(psi, H[i], i) for i in range(num_sites - 1))
@@ -78,3 +83,34 @@ def test_evolve_trotter(num_sites, phys_dim):
     np.testing.assert_allclose(t, 0.01)
     np.testing.assert_almost_equal(norm1/norm2, 1.0)
     np.testing.assert_almost_equal(en1/en2, 1.0, decimal=2)
+
+
+@pytest.mark.parametrize(
+    "num_sites,phys_dim,graph",
+    [(2, 3, False), (2, 3, True), (5, 2, False)])
+def test_evolve_trotter_euclidean(num_sites, phys_dim, graph):
+    psi = tf.complex(
+        tf.random_normal([phys_dim] * num_sites, dtype=tf.float64),
+        tf.random_normal([phys_dim] * num_sites, dtype=tf.float64))
+    h = tf.complex(
+        tf.random_normal((phys_dim**2, phys_dim**2), dtype=tf.float64),
+        tf.random_normal((phys_dim**2, phys_dim**2), dtype=tf.float64))
+    h = 0.5 * (h + tf.linalg.adjoint(h))
+    h = tf.reshape(h, (phys_dim, phys_dim, phys_dim, phys_dim))
+    H = [h] * (num_sites - 1)
+
+    norm1 = wavefunctions.inner(psi, psi)
+    en1 = sum(wavefunctions.expval(psi, H[i], i) for i in range(num_sites - 1))
+
+    if graph:
+        psi, t = wavefunctions.evolve_trotter_defun(
+            psi, H, 0.1, 10, euclidean=True)
+    else:
+        psi, t = wavefunctions.evolve_trotter(psi, H, 0.1, 10, euclidean=True)
+
+    norm2 = wavefunctions.inner(psi, psi)
+    en2 = sum(wavefunctions.expval(psi, H[i], i) for i in range(num_sites - 1))
+
+    np.testing.assert_allclose(t, 1.0)
+    assert norm2.numpy() < norm1.numpy()
+    assert en2.numpy()/norm2.numpy() < en1.numpy()/norm1.numpy()
