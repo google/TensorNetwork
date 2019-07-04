5df2a90dc5902ea3db0ebd4d19c0f3d8a6f9c928
Martin <martin.ganahl@gmail.com>
2019-07-04 12:56:27 -0400

    a few changes

--- experiments/MPS_classifier/train_classifier.py
+++ experiments/MPS_classifier/train_classifier.py
@@ -4,14 +4,15 @@ import tensornetwork as tn
 import experiments.MPS_classifier.MPSMNIST as mm
 import datetime
 import pickle
+import time
 if __name__ == "__main__":
-    numpy_svd  = True
+    numpy_svd  = False
     factor = 1.5
     n_stpcnt = 20
     lower_bound = 1E-9
     max_local_steps =  2
     trunc_thresh = 1E-8
-    n_batches = 1
+    n_batches = 50
     n_sweeps = 10
     dtype = tf.float64
     train,valid,test = mm.load_MNIST('data')
@@ -25,11 +26,30 @@ if __name__ == "__main__":
     mps.position(0, trunc_thresh=trunc_thresh)
     mps.compute_data_environments(samples)    
     lr = 1E-8
-
+    loss_thresh=1E100
     today = str(datetime.date.today())
-    #losses, accuracies, walltimes = mps.optimize(samples, labels, learning_rate=lr, num_sweeps=n_sweeps, n0=n0, numpy_svd=numpy_svd,
-    #                                             factor=factor, n_stpcnt=n_stpcnt, lower_bound=lower_bound, max_local_steps=max_local_steps)
-    losses, accuracies = mps.optimize_simple(samples, labels, learning_rate=lr, num_sweeps=n_sweeps, numpy_svd=numpy_svd, trunc_thresh=trunc_thresh)
+    losses,  accuracies = [], []
+    t0 = time.time()    
+    l, a = mps.left_right_sweep_simple(samples,labels,learning_rate=1E-6,numpy_svd=numpy_svd,
+                                       trunc_thresh=1E-8,t0=t0)
+    losses.extend(l)
+    accuracies.extend(a)
+    
+    l, a = mps.right_left_sweep_simple(samples,labels,learning_rate=1E-8,numpy_svd=numpy_svd,
+                                       trunc_thresh=1E-8, loss_thresh=loss_thresh,t0=t0)
+    losses.extend(l)
+    accuracies.extend(a)
+
+    for n in range(n_sweeps -1):
+        l, a = mps.left_right_sweep_simple(samples,labels,learning_rate=1E-8,numpy_svd=numpy_svd,
+                                           trunc_thresh=trunc_thresh, loss_thresh=loss_thresh,t0=t0)
+        losses.estend(l)
+        accuracies.extend(a)
+        
+        l, a = mps.right_left_sweep_simple(samples,labels,learning_rate=1E-8,numpy_svd=numpy_svd,
+                                           trunc_thresh=trunc_thresh, loss_thresh=loss_thresh, t0=t0)    
+        losses.extend(l)
+        accuracies.extend(a)
                                                         
     
     with open(today + '_MPS_MNIST_classifier_D{}.pickle'.format(D), 'wb') as f:
