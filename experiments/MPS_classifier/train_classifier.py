bd13b69fefd0f3e87d7b9b4b2d8224ddf0a5ca61
mganahl <martin.ganahl@gmail.com>
2019-07-04 18:33:18 +0000

    added maximal D value

--- experiments/MPS_classifier/train_classifier.py
+++ experiments/MPS_classifier/train_classifier.py
@@ -11,8 +11,8 @@ if __name__ == "__main__":
     n_stpcnt = 20
     lower_bound = 1E-9
     max_local_steps =  2
-    trunc_thresh = 1E-8
-    n_batches = 50
+
+    n_batches = 1
     n_sweeps = 10
     dtype = tf.float64
     train,valid,test = mm.load_MNIST('data')
@@ -21,33 +21,35 @@ if __name__ == "__main__":
     labels = tf.convert_to_tensor(labs[0].astype(dtype.as_numpy_dtype))
     valid_data, valid_labels = mm.generate_mapped_MNIST_batches(valid[0], valid[1],n_batches=1)
     N = samples.shape[2]
+    lr = 1E-8
+    trunc_thresh = 1E-7
+    D = 30
+    loss_thresh=1E100
     
-    mps = mm.MPSClassifier.eye(d = [2]*N, D=[2]*N, num_labels=10, scaling=1.0, noise=1E-6, dtype=dtype)
+    mps = mm.MPSClassifier.eye(d = [2]*N, D=[4]*N, num_labels=10, scaling=1.0, noise=1E-8, dtype=dtype)
     mps.position(0, trunc_thresh=trunc_thresh)
     mps.compute_data_environments(samples)    
-    lr = 1E-8
-    loss_thresh=1E100
     today = str(datetime.date.today())
     losses,  accuracies = [], []
     t0 = time.time()    
-    l, a = mps.left_right_sweep_simple(samples,labels,learning_rate=1E-6,numpy_svd=numpy_svd,
-                                       trunc_thresh=1E-8,t0=t0)
+    l, a = mps.left_right_sweep_simple(samples,labels,learning_rate=1E-7,numpy_svd=numpy_svd,
+                                       trunc_thresh=1E-7,t0=t0, D=D)
     losses.extend(l)
     accuracies.extend(a)
     
     l, a = mps.right_left_sweep_simple(samples,labels,learning_rate=1E-8,numpy_svd=numpy_svd,
-                                       trunc_thresh=1E-8, loss_thresh=loss_thresh,t0=t0)
+                                       trunc_thresh=1E-7, loss_thresh=loss_thresh,t0=t0, D=D)
     losses.extend(l)
     accuracies.extend(a)
 
     for n in range(n_sweeps -1):
-        l, a = mps.left_right_sweep_simple(samples,labels,learning_rate=1E-8,numpy_svd=numpy_svd,
-                                           trunc_thresh=trunc_thresh, loss_thresh=loss_thresh,t0=t0)
+        l, a = mps.left_right_sweep_simple(samples,labels,learning_rate=lr,numpy_svd=numpy_svd,
+                                           trunc_thresh=trunc_thresh, loss_thresh=loss_thresh,t0=t0, D=D)
         losses.estend(l)
         accuracies.extend(a)
         
-        l, a = mps.right_left_sweep_simple(samples,labels,learning_rate=1E-8,numpy_svd=numpy_svd,
-                                           trunc_thresh=trunc_thresh, loss_thresh=loss_thresh, t0=t0)    
+        l, a = mps.right_left_sweep_simple(samples,labels,learning_rate=lr,numpy_svd=numpy_svd,
+                                           trunc_thresh=trunc_thresh, loss_thresh=loss_thresh, t0=t0, D=D)    
         losses.extend(l)
         accuracies.extend(a)
                                                         
