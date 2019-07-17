797976cd30605465433cf36c679cb3fdf134349c
Martin <martin.ganahl@gmail.com>
2019-07-17 00:03:36 -0400

    renmoved some bugs and typos

--- experiments/MPS_classifier/train_classifier.py
+++ experiments/MPS_classifier/train_classifier.py
@@ -15,24 +15,27 @@ if __name__ == "__main__":
     test[0], _ = mm.shuffle(test[0],new_order)
     N = 784
     lr = 1E-4
-    trunc_thresh = 1E-8
-    D = 100
+    D = 10
     loss_thresh=0.1
     today = str(datetime.date.today())
-    mps = mm.MPSClassifier.eye(d = [2]*N, D=[4]*N, num_labels=10, scaling=1.0, noise=1E-4, dtype=dtype)
-    mps.position(0, trunc_thresh=trunc_thresh, D=4)
+    mps = mm.MPSClassifier.eye(ds = [2]*N, D=D, num_labels=10, label_position=0,scaling=1.0, noise=1E-4, dtype=dtype)
+    mps.position(0,  D=D)
     losses,  accuracies = [], []    
     for nb1, nb2 in [(100,50),(20, 10), (10,10)]:
         data, labs = mm.generate_mapped_MNIST_batches(train[0],train[1],n_batches=nb1)
         samples = tf.convert_to_tensor(data[0].astype(dtype.as_numpy_dtype))
         labels = tf.convert_to_tensor(labs[0].astype(dtype.as_numpy_dtype))
-        mps.compute_data_environments(samples)    
-    
-        
         t0 = time.time()
-        l, a = mps.left_right_sweep_simple(samples,labels,learning_rate=lr,numpy_svd=numpy_svd,
-                                           trunc_thresh=trunc_thresh,D=D,t0=t0,
-                                           max_local_steps=4, loss_thresh=loss_thresh,lower_lr_bound=1E-8)
+        
+        mps.label_position(0,D=D)
+        mps.position(0)
+        mps.compute_data_environments(samples)        
+        l, a = mps.left_right_sweep_label(samples,labels,learning_rate=1E-3,D=D)
+
+        mps.label_position(N//2, D=D)        
+        mps.position(0)
+        mps.compute_data_environments(samples)        
+        l, a = mps.left_right_sweep_simple(samples,labels,learning_rate=lr, t0=t0)
         losses.extend(l)
         accuracies.extend(a)
         data, labs = mm.generate_mapped_MNIST_batches(train[0],train[1],n_batches=nb2)
@@ -40,22 +43,20 @@ if __name__ == "__main__":
         labels = tf.convert_to_tensor(labs[0].astype(dtype.as_numpy_dtype))
         mps.compute_data_environments(samples)    
         
-        l, a = mps.right_left_sweep_simple(samples,labels,learning_rate=lr,numpy_svd=numpy_svd,
-                                           trunc_thresh=trunc_thresh, t0=t0, D=D,
-                                           max_local_steps=4, loss_thresh=loss_thresh,lower_lr_bound=1E-8)        
+        l, a = mps.right_left_sweep_simple(samples,labels,learning_rate=lr,
+                                           t0=t0)
+                                           
         losses.extend(l)
         accuracies.extend(a)
     
-        # for n in range(n_sweeps -1):
-        #     l, a = mps.left_right_sweep_simple(samples,labels,learning_rate=lr,numpy_svd=numpy_svd,
-        #                                        trunc_thresh=trunc_thresh, loss_thresh=loss_thresh,t0=t0, D=D)
-        #     losses.estend(l)
-        #     accuracies.extend(a)
+        for n in range(n_sweeps -1):
+            l, a = mps.left_right_sweep_simple(samples,labels,learning_rate=lr,t0=t0)
+            losses.extend(l)
+            accuracies.extend(a)
             
-        #     l, a = mps.right_left_sweep_simple(samples,labels,learning_rate=lr,numpy_svd=numpy_svd,
-        #                                        trunc_thresh=trunc_thresh, loss_thresh=loss_thresh, t0=t0, D=D)    
-        #     losses.extend(l)
-        #     accuracies.extend(a)
+            l, a = mps.right_left_sweep_simple(samples,labels,learning_rate=lr,t0=t0)
+            losses.extend(l)
+            accuracies.extend(a)
                                                             
     
         with open(today + '_MPS_MNIST_classifier_D{}.pickle'.format(D), 'wb') as f:
