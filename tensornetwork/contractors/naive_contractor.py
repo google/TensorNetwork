558e379aa8536a7e4936b0f606b8231a33c48d82
Martin <martin.ganahl@gmail.com>
2019-08-09 17:34:34 -0400

    added output_edge_order argument to naive contractor

--- tensornetwork/contractors/naive_contractor.py
+++ tensornetwork/contractors/naive_contractor.py
@@ -23,15 +23,18 @@ from tensornetwork import network_components
 
 
 def naive(net: network.TensorNetwork,
-          edge_order: Optional[Sequence[network_components.Edge]] = None
+          output_edge_order: Sequence[Sequence[network_components.Edge]],
+          edge_order: Optional[Sequence[network_components.Edge]] = None,
          ) -> network.TensorNetwork:
   """Contract a TensorNetwork in the order the edges were created.
+  if `edge_order` is passed, contract the network in the order of `edge_order`.
 
   This contraction method will usually be very suboptimal unless the edges were
   created in a deliberate way.
 
   Args:
     net: A TensorNetwork.
+    output_edge_order: A list of lists of edges. Edges of the final nodes in `nodes_set` are reordered into `output_edge_order`
     edge_order: An optional list of edges. Must be equal to all non-dangling
       edges in the net.
   Returns:
@@ -42,11 +45,27 @@ def naive(net: network.TensorNetwork,
   """
   if edge_order is None:
     edge_order = sorted(net.get_all_nondangling())
+    
   if set(edge_order) != net.get_all_nondangling():
     raise ValueError("Set of passed edges does not match expected set."
                      "Given: {}\nExpected: {}".format(
                           edge_order, net.get_all_nondangling()))
+
+  if set([edge for edge_list in output_edge_order for edge in edge_list]) != (net.get_all_edges() - net.get_all_nondangling()):
+    raise ValueError("output edges are not all dangling.")
+  
   for edge in edge_order:
     if edge in net:
       net.contract_parallel(edge)
+      
+  if len(net.nodes_set) != len(output_edge_order):
+    raise ValueError("number of elements {0} in `output_edge_order` is not matching"
+                     " the number of remaining nodes {1} in `nodes_set`".format(len(output_edge_order), len(net.nodes_set)))
+  for order in output_edge_order:
+    if not any([set(order) == set(node.edges) for node in net.nodes_set]):
+      raise ValueError('elements {0} in `output_edge_order` do not match any node-edges in `node_set`'.format([o.name for o in order]))
+    
+  node_order_pairs = [(node, order) for order in output_edge_order for node in net.nodes_set  if set(node.edges) == set(order)]    
+  net.nodes_set = set([node.reorder_edges(order) for node, order in node_order_pairs])
   return net
+
