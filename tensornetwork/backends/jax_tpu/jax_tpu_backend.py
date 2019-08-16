a787a0941ebead043814b8a16b844a7f9a8a1214
Chase Roberts <chaseriley@google.com>
2019-08-16 11:14:41 -0700

    Pylint fixes

--- tensornetwork/backends/jax_tpu/jax_tpu_backend.py
+++ tensornetwork/backends/jax_tpu/jax_tpu_backend.py
@@ -24,8 +24,12 @@ import logging
 
 Tensor = Any
 
+
 def optimized_shape(shape):
   size = 1
+  # This is disabled since the return below will
+  # always run since we have a `or i == 0`.
+  #pylint: disable=inconsistent-return-statements
   for i, dim in reversed(list(enumerate(shape))):
     size *= dim
     if size >= 128 or i == 0:
@@ -33,16 +37,16 @@ def optimized_shape(shape):
       new_shape = (int(onp.prod(shape[:i])),) + (size,)
       return new_shape, first_expensive_axis
 
-class JaxTPUTensor(base_backend.BaseBackend):
+
+class JaxTPUTensor():
+
   def __init__(self, concrete_tensor, virtual_tensor):
-    assert isinstance(virtual_tensor , shell_backend.ShellTensor)
+    assert isinstance(virtual_tensor, shell_backend.ShellTensor)
     concrete_tensor = self.to_device_array(concrete_tensor)
     if onp.prod(concrete_tensor.shape) != onp.prod(virtual_tensor.shape):
-      raise ValueError(
-          "Virtual shapes and concrete shapes do not math."
-          "Virtual shape: {}, concrete shape: {}".format(
-              virtual_tensor.shape,
-              concrete_tensor.shape))
+      raise ValueError("Virtual shapes and concrete shapes do not math."
+                       "Virtual shape: {}, concrete shape: {}".format(
+                           virtual_tensor.shape, concrete_tensor.shape))
     self.virtual_tensor = virtual_tensor
     self.set_optimized_concrete_tensor(concrete_tensor)
 
@@ -50,10 +54,10 @@ class JaxTPUTensor(base_backend.BaseBackend):
   def shape(self):
     return self.virtual_tensor.shape
 
-  @ property
+  @property
   def real_tensor(self):
     return jax.numpy.reshape(self.concrete_tensor, self.virtual_tensor.shape)
-  
+
   def set_optimized_concrete_tensor(self, tensor):
     shape, self.first_expensive_axis = optimized_shape(tensor.shape)
     if shape != tensor.shape:
@@ -64,14 +68,14 @@ class JaxTPUTensor(base_backend.BaseBackend):
   def to_device_array(self, tensor):
     return jax.jit(lambda x: x)(tensor)
 
-
+# pylint: disable=abstract-method
 class JaxTPUBackend(base_backend.BaseBackend):
   """See base_backend.BaseBackend for documentation."""
 
   def __init__(self):
     super(JaxTPUBackend, self).__init__()
     self.shell_backend = shell_backend.ShellBackend()
-  
+
   def concat(self, values: Tensor, axis: int) -> Tensor:
     return onp.concatenate(values, axis)
 
@@ -111,8 +115,8 @@ class JaxTPUBackend(base_backend.BaseBackend):
       concrete_a = jax.numpy.reshape(a.concrete_tensor, a.shape)
       concrete_b = jax.numpy.reshape(b.concrete_tensor, b.shape)
       resulting_tensor = jax.numpy.tensordot(concrete_a, concrete_b, axes)
-    new_virtual = self.shell_backend.tensordot(
-        a.virtual_tensor, b.virtual_tensor, axes)
+    new_virtual = self.shell_backend.tensordot(a.virtual_tensor,
+                                               b.virtual_tensor, axes)
     return JaxTPUTensor(resulting_tensor, new_virtual)
 
 
@@ -122,18 +126,15 @@ def optimized_dot_general(a, b, contracting_axes):
     # These reshapes are much cheaper than the reshapes done in tensordot.
     axes = contracting_axes[i]
     new_dim = onp.prod([tensor.shape[index] for index in axes])
-    new_shape = list(tensor.shape[:tensor.first_expensive_axis]) + [tensor.concrete_tensor.shape[-1]]
+    new_shape = (
+        list(tensor.shape[:tensor.first_expensive_axis]) +
+        [tensor.concrete_tensor.shape[-1]])
     tmp_tensor = jax.numpy.reshape(tensor.concrete_tensor, new_shape)
     perm = axes + sorted(list(set(range(len(tmp_tensor.shape))) - set(axes)))
     tmp_tensor = jax.numpy.transpose(tmp_tensor, perm)
-    tmp_tensor = jax.numpy.reshape(tmp_tensor, [new_dim] + list(tmp_tensor.shape[len(axes):]))
+    tmp_tensor = jax.numpy.reshape(tmp_tensor, [new_dim] +
+                                   list(tmp_tensor.shape[len(axes):]))
     new_tensors.append(tmp_tensor)
 
-  return jax.lax.dot_general(new_tensors[0], new_tensors[1], 
-    (((0,), (0,)), (tuple(), tuple())))
-
-
-
-
-
-
+  return jax.lax.dot_general(new_tensors[0], new_tensors[1],
+                             (((0,), (0,)), (tuple(), tuple())))
