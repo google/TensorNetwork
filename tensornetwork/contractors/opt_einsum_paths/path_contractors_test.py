42afafdfd2a904828eade253cdd08629f2870fbd
Martin <martin.ganahl@gmail.com>
2019-08-17 22:43:14 -0400

    another indentation problem fixed

--- tensornetwork/contractors/opt_einsum_paths/path_contractors_test.py
+++ tensornetwork/contractors/opt_einsum_paths/path_contractors_test.py
@@ -27,19 +27,19 @@ def path_algorithm_fixture(request):
 
 
 def test_sanity_check(backend, path_algorithm):
- net = tensornetwork.TensorNetwork(backend=backend)
- a = net.add_node(np.eye(2))
- b = net.add_node(np.ones((2, 7, 11)))
- c = net.add_node(np.ones((7, 11, 13, 2)))
- d = net.add_node(np.eye(13))
- # pylint: disable=pointless-statement
- a[0] ^ b[0]
- b[1] ^ c[0]
- b[2] ^ c[1]
- c[2] ^ d[1]
- c[3] ^ a[1]
- final_node = path_algorithm(net).get_final_node()
- assert final_node.shape == (13,)
+  net = tensornetwork.TensorNetwork(backend=backend)
+  a = net.add_node(np.eye(2))
+  b = net.add_node(np.ones((2, 7, 11)))
+  c = net.add_node(np.ones((7, 11, 13, 2)))
+  d = net.add_node(np.eye(13))
+  # pylint: disable=pointless-statement
+  a[0] ^ b[0]
+  b[1] ^ c[0]
+  b[2] ^ c[1]
+  c[2] ^ d[1]
+  c[3] ^ a[1]
+  final_node = path_algorithm(net).get_final_node()
+  assert final_node.shape == (13,)
 
 
 def test_trace_edge(backend, path_algorithm):
