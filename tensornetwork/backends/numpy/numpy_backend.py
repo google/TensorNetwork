94ae0bbf9d77cdb40e058e26b8bb46cd0c2e1874
Martin <martin.ganahl@gmail.com>
2019-08-27 16:09:31 -0400

    removed Union

--- tensornetwork/backends/numpy/numpy_backend.py
+++ tensornetwork/backends/numpy/numpy_backend.py
@@ -15,7 +15,7 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-from typing import Optional, Any, Sequence, Tuple, Union
+from typing import Optional, Any, Sequence, Tuple
 from tensornetwork.backends import base_backend
 
 Tensor = Any
