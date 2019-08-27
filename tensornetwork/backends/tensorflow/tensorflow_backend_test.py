7089f3e13e13dc19572ac4483c850e972da6644a
Martin <martin.ganahl@gmail.com>
2019-08-27 18:26:29 -0400

    tests updated

--- tensornetwork/backends/tensorflow/tensorflow_backend_test.py
+++ tensornetwork/backends/tensorflow/tensorflow_backend_test.py
@@ -124,51 +124,51 @@ def test_norm():
 
 def test_eye():
   backend = tensorflow_backend.TensorFlowBackend()
-  a = backend.eye(4)
-  np.testing.assert_allclose(np.eye(4), a)
+  a = backend.eye(N=4, M=5)
+  np.testing.assert_allclose(np.eye(4, 5), a)
 
 
 def test_ones():
   backend = tensorflow_backend.TensorFlowBackend()
-  a = backend.ones(4)
+  a = backend.ones((4, 4))
   np.testing.assert_allclose(np.ones((4, 4)), a)
 
 
 def test_zeros():
   backend = tensorflow_backend.TensorFlowBackend()
-  a = backend.zeros(4)
+  a = backend.zeros((4, 4))
   np.testing.assert_allclose(np.zeros((4, 4)), a)
 
 
 def test_randn():
   backend = tensorflow_backend.TensorFlowBackend()
-  a = backend.randn(4)
+  a = backend.randn((4, 4))
   assert a.shape == (4, 4)
 
 
 def test_eye_dtype():
   backend = tensorflow_backend.TensorFlowBackend()
   dtype = tf.float32
-  a = backend.eye(4, dtype=dtype)
+  a = backend.eye(N=4, M=4, dtype=dtype)
   assert a.dtype == dtype
 
 
 def test_ones_dtype():
   backend = tensorflow_backend.TensorFlowBackend()
   dtype = tf.float32
-  a = backend.ones(4, dtype=dtype)
+  a = backend.ones((4, 4), dtype=dtype)
   assert a.dtype == dtype
 
 
 def test_zeros_dtype():
   backend = tensorflow_backend.TensorFlowBackend()
   dtype = tf.float32
-  a = backend.zeros(4, dtype=dtype)
+  a = backend.zeros((4, 4), dtype=dtype)
   assert a.dtype == dtype
 
 
 def test_randn_dtype():
   backend = tensorflow_backend.TensorFlowBackend()
   dtype = tf.float32
-  a = backend.randn(4, dtype=dtype)
+  a = backend.randn((4, 4), dtype=dtype)
   assert a.dtype == dtype
