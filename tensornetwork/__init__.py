49dd384833db44b6d67f13579b1c87348e9bff19
michaelmarien <marien.mich@gmail.com>
2019-08-27 21:59:54 +0200

    added load function to tensornetwork init

--- tensornetwork/__init__.py
+++ tensornetwork/__init__.py
@@ -1,5 +1,5 @@
 from __future__ import absolute_import
-from tensornetwork.network import TensorNetwork
+from tensornetwork.network import TensorNetwork, load
 from tensornetwork.network_components import Node, Edge
 from tensornetwork.ncon_interface import ncon, ncon_network
 from tensornetwork.version import __version__
