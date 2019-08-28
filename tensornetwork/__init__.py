f9f6522954d48bd8c8d615e2917df8e4e025a824
michaelmarien <marien.mich@gmail.com>
2019-08-28 12:40:48 +0200

    cleanup double imports

--- tensornetwork/__init__.py
+++ tensornetwork/__init__.py
@@ -1,7 +1,5 @@
 from __future__ import absolute_import
 from tensornetwork.network import TensorNetwork, load
-from tensornetwork.network_components import Node, Edge
-from tensornetwork.network import TensorNetwork
 from tensornetwork.network_components import Node, Edge, CopyNode
 from tensornetwork.ncon_interface import ncon, ncon_network
 from tensornetwork.version import __version__
