8e61df935186cb29e56e0d9d79129badedd129df
Martin <martin.ganahl@gmail.com>
2019-08-28 21:32:09 -0400

    dtuype defaults and supported dtypesc

--- tensornetwork/config.py
+++ tensornetwork/config.py
@@ -25,16 +25,17 @@ default_dtypes = {
 
 supported_dtypes = {
     'tensorflow': (tf.int8, tf.int16, tf.int32, tf.int64, tf.float32,
-                   tf.float64, tf.complex64, tf.complex128, tf.bool),
-    'numpy': (np.int8, np.int16, np.int32, np.int64, np.float32, np.float64,
-              np.complex64, np.complex128, np.bool),
+                   tf.float16, tf.float64, tf.complex64, tf.complex128,
+                   tf.bool),
+    'numpy': (np.int8, np.int16, np.int32, np.int64, np.float16, np.float32,
+              np.float64, np.complex64, np.complex128, np.bool),
     'pytorch': (torch.int8, torch.int16, torch.int32, torch.int64,
-                torch.float32, torch.float64, torch.complex64, torch.complex128,
-                torch.bool),
+                torch.float16, torch.float32, torch.float64, torch.complex64,
+                torch.complex128, torch.bool),
     'jax': (np.int8, np.int16, np.int32, np.int64, np.float32, np.float64,
             np.complex64, np.complex128, np.bool),
 }
-
+#shell supports everything
 supported_dtypes['shell'] =\
     supported_dtypes['numpy'] + \
     supported_dtypes['pytorch'] + \
