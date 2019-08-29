07e805f1284858eb8c420e622a2043c0895dcbc6
Martin <martin.ganahl@gmail.com>
2019-08-29 13:16:57 -0400

    more types added to supported numpy dtypes
    due top numpy's ndarray returning np.dtype and not type

--- tensornetwork/config.py
+++ tensornetwork/config.py
@@ -15,7 +15,11 @@ import tensorflow as tf
 import numpy as np
 import torch
 default_backend = "tensorflow"
-#for backwards compatibility default dtypes have to be None
+#for backwards compatibility default dtypes have to be `None`
+#changing this will cause tests to fail due backend.convert_to_tensor(tensor)
+#raising TypeErrors when incoming `tensor` has a dtype different from
+#backend.dtype.
+
 default_dtypes = {
     'tensorflow': None,
     'numpy': None,
@@ -24,17 +28,32 @@ default_dtypes = {
     'shell': None
 }
 
+numpy_dtypes = [
+    np.int8, np.int16, np.int32, np.int64, np.float16, np.float32, np.float64,
+    np.complex64, np.complex128, np.bool
+]
+#np.ndarray.dtype doesn't return a `type`, but a `dtype` object
+#backend_factory compares object identities, and we want to catch both
+#the case of `dtype` being a `type` (like np.float64 given by a user) AND
+#the case of `dtype` being obtained from `np.ndarray.dtype`
+numpy_dtypes += [np.dtype(d) for d in numpy_dtypes]
+numpy_dtypes.append(None)
 supported_dtypes = {
-    'tensorflow': (tf.int8, tf.int16, tf.int32, tf.int64, tf.float32,
-                   tf.float16, tf.float64, tf.complex64, tf.complex128, tf.bool,
-                   None),
-    'numpy': (np.int8, np.int16, np.int32, np.int64, np.float16, np.float32,
-              np.float64, np.complex64, np.complex128, np.bool, None),
-    'pytorch': (torch.int8, torch.int16, torch.int32, torch.int64,
-                torch.float16, torch.float32, torch.float64, torch.complex64,
-                torch.complex128, torch.bool, None),
-    'jax': (np.int8, np.int16, np.int32, np.int64, np.float16, np.float32,
-            np.float64, np.complex64, np.complex128, np.bool, None),
+    'tensorflow': [
+        tf.int8, tf.int16, tf.int32, tf.int64, tf.float32, tf.float16,
+        tf.float64, tf.complex64, tf.complex128, tf.bool, None
+    ],
+    'numpy':
+        numpy_dtypes,
+    'pytorch': [
+        torch.int8, torch.int16, torch.int32, torch.int64, torch.float16,
+        torch.float32, torch.float64, torch.complex64, torch.complex128,
+        torch.bool, None
+    ],
+    'jax': [
+        np.int8, np.int16, np.int32, np.int64, np.float16, np.float32,
+        np.float64, np.complex64, np.complex128, np.bool, None
+    ]
 }
 #shell supports everything
 supported_dtypes['shell'] =\
