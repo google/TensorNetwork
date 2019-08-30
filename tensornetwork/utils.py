094a3b704aac74fc992d74b5a3bba79bbb85069c
michaelmarien <marien.mich@gmail.com>
2019-08-30 21:35:50 +0200

    fix pylint cyclic dep

--- tensornetwork/utils.py
+++ tensornetwork/utils.py
@@ -1,7 +1,5 @@
 import h5py
-from tensornetwork import TensorNetwork
-from tensornetwork import component_factory
-from tensornetwork import network_components
+import tensornetwork
 
 
 def load(path: str):
@@ -11,18 +9,19 @@ def load(path: str):
     path: path to folder where network is saved.
   """
   with h5py.File(path, 'r') as net_file:
-    net = TensorNetwork(backend=net_file["backend"][()])
+    net = tensornetwork.TensorNetwork(backend=net_file["backend"][()])
     nodes = list(net_file["nodes"].keys())
     edges = list(net_file["edges"].keys())
 
     for node_name in nodes:
       node_data = net_file["nodes/" + node_name]
-      node_type = component_factory.get_component(node_data['type'][()])
+      node_type = (tensornetwork.component_factory
+                                .get_component(node_data['type'][()]))
       node_type._load_node(net, node_data)
 
     nodes_dict = {node.name: node for node in net.nodes_set}
 
     for edge in edges:
       edge_data = net_file["edges/" + edge]
-      network_components.Edge._load_edge(edge_data, nodes_dict)
+      tensornetwork.network_components.Edge._load_edge(edge_data, nodes_dict)
   return net
