51a3182cdeb35d74c7938e1ff88def63c0330a26
michaelmarien <marien.mich@gmail.com>
2019-08-30 21:54:17 +0200

    found and fixed root cause of pylint cyclic

--- tensornetwork/utils.py
+++ tensornetwork/utils.py
@@ -1,5 +1,7 @@
 import h5py
-import tensornetwork
+from tensornetwork.network import TensorNetwork
+from tensornetwork.component_factory import get_component
+from tensornetwork.network_components import Edge
 
 
 def load(path: str):
@@ -9,19 +11,18 @@ def load(path: str):
     path: path to folder where network is saved.
   """
   with h5py.File(path, 'r') as net_file:
-    net = tensornetwork.TensorNetwork(backend=net_file["backend"][()])
+    net = TensorNetwork(backend=net_file["backend"][()])
     nodes = list(net_file["nodes"].keys())
     edges = list(net_file["edges"].keys())
 
     for node_name in nodes:
       node_data = net_file["nodes/" + node_name]
-      class_name = node_data['type'][()]
-      node_type = (tensornetwork.component_factory.get_component(class_name))
+      node_type = get_component(node_data['type'][()])
       node_type._load_node(net, node_data)
 
     nodes_dict = {node.name: node for node in net.nodes_set}
 
     for edge in edges:
       edge_data = net_file["edges/" + edge]
-      tensornetwork.network_components.Edge._load_edge(edge_data, nodes_dict)
+      Edge._load_edge(edge_data, nodes_dict)
   return net
