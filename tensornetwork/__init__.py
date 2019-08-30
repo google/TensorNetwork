cc73cf128150d2a999a099d5de7d8078d65c180c
michaelmarien <marien.mich@gmail.com>
2019-08-30 20:08:08 +0200

    import load from utils

--- tensornetwork/__init__.py
+++ tensornetwork/__init__.py
@@ -1,11 +1,12 @@
 from __future__ import absolute_import
-from tensornetwork.network import TensorNetwork, load
+from tensornetwork.network import TensorNetwork
 from tensornetwork.network_components import Node, Edge, CopyNode
 from tensornetwork.ncon_interface import ncon, ncon_network
 from tensornetwork.version import __version__
 from tensornetwork.visualization.graphviz import to_graphviz
 from tensornetwork import contractors
 from tensornetwork import config
+from tensornetwork.utils import load
 from typing import Text
 
 
