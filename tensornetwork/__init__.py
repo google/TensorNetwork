6eb95b17b5239f6658a6c19b5ea0afeea48ec561
Martin <martin.ganahl@gmail.com>
2019-09-03 13:15:36 -0400

    created mops folder, moved mps stuff into mps folder

--- tensornetwork/__init__.py
+++ tensornetwork/__init__.py
@@ -6,7 +6,7 @@ from tensornetwork.version import __version__
 from tensornetwork.visualization.graphviz import to_graphviz
 from tensornetwork import contractors
 from tensornetwork import config
-from tensornetwork import mps
+from mps import mps
 from typing import Text, Optional, Type
 from tensornetwork.utils import load
 
