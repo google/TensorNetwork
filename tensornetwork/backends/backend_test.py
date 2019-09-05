5fcc0bb8cbb202d7173e18ec57ff4d9d25cdc4aa
Martin <martin.ganahl@gmail.com>
2019-09-05 12:47:38 -0400

    added some pylint: disable lines

--- tensornetwork/backends/backend_test.py
+++ tensornetwork/backends/backend_test.py
@@ -10,18 +10,20 @@ def clean_backend_import():
   sys.modules.pop('tensornetwork.backends.pytorch.pytorch_backend', None)
   sys.modules.pop('tensornetwork.backends.jax.jax_backend', None)
   sys.modules.pop('tensornetwork.backends.tensorflow.tensorflow_backend', None)
-  yield # use as teardown
+  yield  # use as teardown
   sys.modules.pop('tensornetwork', None)
 
 
 @pytest.fixture
 def no_backend_dependency(monkeypatch):
   import_orig = builtins.__import__
+
   # pylint: disable=redefined-builtin
   def mocked_import(name, globals, locals, fromlist, level):
     if name in ['torch', 'tensorflow', 'jax']:
       raise ImportError()
     return import_orig(name, globals, locals, fromlist, level)
+
   monkeypatch.setattr(builtins, '__import__', mocked_import)
 
 
@@ -31,6 +33,7 @@ def test_backend_pytorch_missing_cannot_initialize_backend():
     from tensornetwork.backends.pytorch.pytorch_backend import PyTorchBackend
     PyTorchBackend()
 
+
 @pytest.mark.usefixtures('no_backend_dependency')
 def test_backend_tensorflow_missing_cannot_initialize_backend():
   with pytest.raises(ImportError):
@@ -38,14 +41,17 @@ def test_backend_tensorflow_missing_cannot_initialize_backend():
       import TensorFlowBackend
     TensorFlowBackend()
 
+
 @pytest.mark.usefixtures('no_backend_dependency')
 def test_backend_jax_missing_cannot_initialize_backend():
   with pytest.raises(ImportError):
     from tensornetwork.backends.jax.jax_backend import JaxBackend
     JaxBackend()
 
+
 @pytest.mark.usefixtures('no_backend_dependency')
 def test_config_pytorch_missing_can_import_config():
+  # pylint: disable=unused-variable
   import tensornetwork.config
   with pytest.raises(ImportError):
     import torch
@@ -53,6 +59,7 @@ def test_config_pytorch_missing_can_import_config():
 
 @pytest.mark.usefixtures('no_backend_dependency')
 def test_config_tensorflow_missing_can_import_config():
+  # pylint: disable=unused-variable
   import tensornetwork.config
   with pytest.raises(ImportError):
     import tensorflow as tf
@@ -61,11 +68,15 @@ def test_config_tensorflow_missing_can_import_config():
 @pytest.mark.usefixtures('no_backend_dependency')
 def test_import_tensornetwork_without_backends():
   with pytest.raises(ImportError):
+    # pylint: disable=unused-variable
     import torch
   with pytest.raises(ImportError):
+    # pylint: disable=unused-variable
     import tensorflow as tf
   with pytest.raises(ImportError):
+    # pylint: disable=unused-variable
     import jax
+  # pylint: disable=unused-variable
   import tensornetwork
   import tensornetwork.backends.pytorch.pytorch_backend
   import tensornetwork.backends.tensorflow.tensorflow_backend
