9bb1898fcf03aa484ae39adab49c48fe343c10fc
stavros11 <35475381+stavros11@users.noreply.github.com>
2019-09-06 14:07:22 +0200

    Refactor `base`

--- tensornetwork/contractors/opt_einsum_paths/utils.py
+++ tensornetwork/contractors/opt_einsum_paths/utils.py
@@ -1,5 +1,9 @@
 from tensornetwork import network
-from typing import Any, Dict, List, Set
+from typing import Any, Callable, Dict, List, Set, Tuple
+
+
+Algorithm = Callable[[List[Set[int]], Set[int], Dict[int, int]],
+                     List[Tuple[int]]]
 
 
 def multi_remove(elems: List[Any], indices: List[int]) -> List[Any]:
@@ -21,3 +25,11 @@ def get_output_set(net: network.TensorNetwork) -> Set[int]:
 
 def get_size_dict(net: network.TensorNetwork) -> Dict[int, int]:
   return {edge: edge.dimension for edge in net.get_all_edges()}
+
+
+def get_path(net: network.TensorNetwork, algorithm: Algorithm
+             ) -> List[Tuple[int]]:
+  input_sets = get_input_sets(net)
+  output_set = get_output_set(net)
+  size_dict = get_size_dict(net)
+  return algorithm(input_sets, output_set, size_dict)
