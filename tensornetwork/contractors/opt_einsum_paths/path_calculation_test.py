a1b02e6110e4b5e61dad5195070d8f7d7fd13e8a
stavros11 <35475381+stavros11@users.noreply.github.com>
2019-09-06 17:43:23 +0200

    Create test list

--- tensornetwork/contractors/opt_einsum_paths/path_calculation_test.py
+++ tensornetwork/contractors/opt_einsum_paths/path_calculation_test.py
@@ -23,13 +23,7 @@ import tensornetwork
 from tensornetwork.contractors.opt_einsum_paths import utils
 
 
-@pytest.fixture(name="path_algorithm",
-                params=["optimal", "branch", "greedy"])
-def path_algorithm_fixture(request):
-  return getattr(opt_einsum.paths, request.param)
-
-
-def check_path(calculated_path, correct_path, bypass=False):
+def check_path(calculated_path, correct_path):
   if not isinstance(calculated_path, list):
     return False
 
@@ -43,11 +37,11 @@ def check_path(calculated_path, correct_path, bypass=False):
   return ret
 
 
-def create_tensor_network():
-  """Creates 'GEMM1' contraction from `opt_einsum` tests in `TensorNetowrk`.
-
-  Note that the optimal contraction order is [(0, 2), (0, 1)].
-  """
+# We do not use the backend fixture as this file tests only contraction paths
+# that `opt_einsum` returns and not the actual contractions performed by
+# `TensorNetwork`.
+def gemm_network():
+  """Creates 'GEMM1' contraction from `opt_einsum` tests."""
   net = tensornetwork.TensorNetwork()
   x = net.add_node(np.ones([1, 2, 4]))
   y = net.add_node(np.ones([1, 3]))
@@ -57,15 +51,58 @@ def create_tensor_network():
   x[1] ^ z[0]
   x[2] ^ z[1]
   y[1] ^ z[2]
-
-  # This ordering is compatible with `TensorNetwork` only if we sort
-  # according to `node.signature`!
-  optimal_order = [(0, 2), (0, 1)]
-  return net, optimal_order
+  return net
 
 
-def test_path_optimal(path_algorithm):
-  net, optimal_order = create_tensor_network()
+def inner_network():
+  """Creates a (modified) `Inner1` contraction from `opt_einsum` tests."""
+  net = tensornetwork.TensorNetwork()
+  x = net.add_node(np.ones([5, 2, 3, 4]))
+  y = net.add_node(np.ones([5, 3]))
+  z = net.add_node(np.ones([2, 4]))
+  # pylint: disable=pointless-statement
+  x[0] ^ y[0]
+  x[1] ^ z[0]
+  x[2] ^ y[1]
+  x[3] ^ z[1]
+  return net
+
+
+def matrix_chain():
+  """Creates a contraction of chain of matrices.
+
+  The `greedy` algorithm does not find the optimal path in this case!
+  """
+  net = tensornetwork.TensorNetwork()
+  d = [10, 8, 6, 4, 2]
+  nodes = [net.add_node(np.ones([d1, d2])) for d1, d2 in zip(d[:-1], d[1:])]
+  for i in range(len(d) - 2):
+    nodes[i][1] ^ nodes[i + 1][0]
+  return net
+
+
+# Parametrize tests by giving:
+# (contraction algorithm, network, correct path that is expected)
+test_list = [
+    ("optimal", "gemm_network", [(0, 2), (0, 1)]),
+    ("branch", "gemm_network", [(0, 2), (0, 1)]),
+    ("greedy", "gemm_network", [(0, 2), (0, 1)]),
+    ("optimal", "inner_network", [(0, 1), (0, 1)]),
+    ("branch", "inner_network", [(0, 1), (0, 1)]),
+    ("greedy", "inner_network", [(0, 1), (0, 1)]),
+    ("optimal", "matrix_chain", [(2, 3), (1, 2), (0, 1)]),
+    ("branch", "matrix_chain", [(2, 3), (1, 2), (0, 1)]),
+    ("greedy", "matrix_chain", [(0, 1), (0, 2), (0, 1)]),
+    ]
+
+
+@pytest.mark.parametrize("params", test_list)
+def test_path_optimal(params):
+  algorithm_name, network_name, correct_path = params
+
+  net = globals()[network_name]()
+  path_algorithm = getattr(opt_einsum.paths, algorithm_name)
+
   calculated_path, sorted_nodes = utils.get_path(net, path_algorithm)
   assert sorted_nodes == sorted(net.nodes_set, key = lambda n: n.signature)
-  assert check_path(calculated_path, optimal_order)
+  assert check_path(calculated_path, correct_path)
