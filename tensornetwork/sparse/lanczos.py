bb36cf66106a3c43ca533ec74e2d09a191f72924
Martin <martin.ganahl@gmail.com>
2019-09-11 15:23:22 -0400

    added documentation

--- tensornetwork/sparse/lanczos.py
+++ tensornetwork/sparse/lanczos.py
@@ -19,20 +19,42 @@ from __future__ import print_function
 from tensornetwork.backends import backend_factory
 import inspect
 import warnings
-from typing import Any, Sequence, List, Set, Optional, Union, Text, Tuple, Type, Dict, Callable
+from typing import Any, List, Optional, Text, Tuple, Type, Callable
 import numpy as np
 Tensor = Any
 
 
 class LinearOperator:
   """
-  A linear operator class
+  A linear operator class. `LinearOperator` is used to provide a 
+  common interface to `eigsh` and other sparse solvers across 
+  all supported backends.
   """
 
   def __init__(self, matvec: Callable, shape: Tuple[Tuple[int]],
-               dtype: Type[np.number], backend: str) -> None:
+               dtype: Type[np.number], backend: Text) -> None:
     """
     Initialize a `LinearOperator`
+    Args:
+      matvec: A function which implements a matrix-vector multiplication.
+        Allowed signatures are: `matvec(x)`, `matvec(x, backend)`
+          and `matvec(x, backend=default)` with `default` 'numpy', 'tensorflow'
+          'pytorch', 'jax' or 'shell'.
+         `result = `matvec` should return `result` of the same type as `x` (i.e.
+          a `Tensor` object)
+          Note that any default value for argument `backend` of `matvec` is overridden
+          by the value of the `backend` argument of `LinearOperator.__init__`
+      shape: A tuple of outgoing and incoming shapes of the `LinearOperator`.
+        For example, if `matvec(x)` takes as input a tensor `x` of shape `x.shape = (2,3,4,5)`
+        and returns `result` with `result.shape = (4,5,2)`, then `shape` has to be 
+        `shape = ((4,5,2), (2,3,4,5))`.
+      dtype: The data-type of the `LinearOperator`.
+      backend: The backend to be used with this `LinearOperator`. 
+        `backend` can be 'numpy', 'tensorflow', 'pytorch', 'jax' or 'shell'
+    Returns: 
+      None
+    Raises:
+      ValueError if `matvec` signature is other than the above mentioned.
     """
     self.shape = shape
     self.backend = backend_factory.get_backend(backend, dtype)
@@ -84,55 +106,75 @@ class LinearOperator:
 
 class ScalarProduct:
 
-  def __init__(self, vecvec: Callable, dtype: Type[np.number],
-               backend: str) -> None:
+  def __init__(self, dotprod: Callable, dtype: Type[np.number],
+               backend: Text) -> None:
+    """
+    Initialize a `ScalarProduct`
+    Args:
+      dotprod: A function which implements a dot-product between two vectors
+        Allowed signatures are: `dotprod(x, y)`, `dotprod(x, y, backend)`
+          and `dotprod(x, y, backend=default)` with `default` 'numpy', 'tensorflow'
+          'pytorch', 'jax' or 'shell'.
+         `result = `dotprod` should return `result` of the same type as `x` 
+          (i.e. a `Tensor` object)
+          Note that any default value for argument `backend` of `dotprod` is overridden
+          by the value of the `backend` argument of `ScalarProduct.__init__`
+      dtype: The data-type of the `ScalarProduct`.
+      backend: The backend to be used with this `ScalarProduct`. 
+        `backend` can be 'numpy', 'tensorflow', 'pytorch', 'jax' or 'shell'
+    Returns: 
+      None
+    Raises:
+      ValueError if `dotprod` signature is other than the above mentioned.
+    """
 
     self.backend = backend_factory.get_backend(backend, dtype)
-    args = inspect.getargspec(vecvec)
+    args = inspect.getargspec(dotprod)
     if len(args[0]) > 3:
       raise ValueError(
-          '`vecvec` can have at most 3 arguments; found args = {}'.format(
+          '`dotprod` can have at most 3 arguments; found args = {}'.format(
               args[0]))
     if (args[0][0] == 'backend') or (args[0][1] == 'backend'):
       raise ValueError(
-          "Found `backend` as first or second argument of `vecvec`. "
+          "Found `backend` as first or second argument of `dotprod`. "
           "It can only be the third argument!")
     if args[3] and (len(args[3]) != 1):
       N = len(args[0])
       params = [args[0][N - 1 - n] for n in range(len(args[3]))]
       defaults = [args[3][n] for n in reversed(range(len(args[3])))]
       raise ValueError(
-          "The only allowed argument to `vecvec` with defaults is `backend`. "
+          "The only allowed argument to `dotprod` with defaults is `backend`. "
           "Found arguments {} with default values {}!".format(
               params[::-1], defaults[::-1]))
 
     if args[3] and (len(args[3]) == 1):
       if args[3][0] not in ('tensorflow', 'numpy', 'jax', 'pytorch', 'shell'):
         raise ValueError(
-            "wrong default '{}' for argument `{}` of `vecvec`. "
+            "wrong default '{}' for argument `{}` of `dotprod`. "
             "Only allowed values are 'numpy', 'tensorflow', 'pytorch', 'jax' and 'shell'"
             .format(args[3][0], args[0][1]))
       if args[3][0] != self.backend.name:
-        warnings.warn("default value of parameter `{0}` = '{1}' of `vecvec` is "
-                      "different from LinearOperator.backend.name='{2}'."
-                      " Overriding the default to `{0}` = '{2}'".format(
-                          args[0][1], args[3][0], self.backend.name))
+        warnings.warn(
+            "default value of parameter `{0}` = '{1}' of `dotprod` is "
+            "different from ScalarProduct.backend.name='{2}'."
+            " Overriding the default to `{0}` = '{2}'".format(
+                args[0][1], args[3][0], self.backend.name))
 
-    if len(args[0]) == 2:  #vecvec takes only one argument
-      self.vecvec = vecvec
-    else:  #vecvec takes two arguments (x, backend)
+    if len(args[0]) == 2:  #dotprod takes only one argument
+      self.dotprod = dotprod
+    else:  #dotprod takes two arguments (x, backend)
 
-      def _vecvec(x, y):
-        return vecvec(x, y, self.backend.name)
+      def _dotprod(x, y):
+        return dotprod(x, y, self.backend.name)
 
-      self.vecvec = _vecvec
+      self.dotprod = _dotprod
 
   @property
   def dtype(self):
     return self.backend.dtype
 
   def __call__(self, x, y):
-    return self.vecvec(x, y)
+    return self.dotprod(x, y)
 
 
 def eigsh(A: LinearOperator,
@@ -143,11 +185,40 @@ def eigsh(A: LinearOperator,
           tol: Optional[float] = 1E-8,
           delta: Optional[float] = 1E-8,
           ndiag: Optional[int] = 20,
-          reortho: Optional[bool] = False):
-
+          reortho: Optional[bool] = False) -> Tuple[List]:
+  """
+  Lanczos method for finding the lowest eigenvector-eigenvalue pairs
+  of a `LinearOperator` `A`.
+  Args:
+    A: A (sparse) implementation of a linear operator
+    vv: A (sparse) implementation of a scalar product
+    v0: An initial vector for the Lanczos algorithm
+    ncv: The number of iterations (number of krylov vectors).
+    numeig: The nummber of eigenvector-eigenvalue pairs to be computed.
+      If `numeig > 1`, `reortho` has to be `True`.
+    tol: The desired precision of the eigenvalus. Currently we use 
+      `np.linalg.norm(eigvalsnew[0:numeig] - eigvalsold[0:numeig]) < tol`
+      as stopping criterion between two diagonalization steps of the
+      tridiagonal operator.
+    delta: Stopping criterion for Lanczos iteration.
+      If two successive Krylov vectors `x_m` and `x_n`
+      have an overlap abs(<x_m|x_n>) < delta, the iteration is stopped.
+    ndiag: The tridiagonal Operator is diagonalized every `ndiag` iterations to
+      check convergence.
+    reortho: If `True`, Krylov vectors are kept orthogonal by explicit orthogonalization
+      (this is more costly than `reortho=False`)
+  Returns:
+    (eigvals, eigvecs)
+     eigvals: A list of `numeig` lowest eigenvalues
+     eigvecs: A list of `numeig` lowest eigenvectors
+    
+  """
   if ncv < numeig:
     raise ValueError('`ncv` >= `numeig` required!')
-
+  if numeig > 1 and not reortho:
+    raise ValueError(
+        'Got numeig = {} > 1 and `reortho = False`. Use `reortho=True` for `numeig > 1`'
+        .format(numeig))
   if A.backend.name != vv.backend.name:
     raise ValueError("A.backend={} is different from vv.backend={}".format(
         A.backend.name, vv.backend.name))
@@ -222,9 +293,9 @@ def eigsh(A: LinearOperator,
       backend.convert_to_tensor(np.array(eta[n]))
       for n in range(min(numeig, len(eta)))
   ]
-  eig_states = [
+  eigvecs = [
       backend.convert_to_tensor(states[n])
       for n in range(min(numeig, len(eta)))
   ]
 
-  return eigvals, eig_states
+  return eigvals, eigvecs
