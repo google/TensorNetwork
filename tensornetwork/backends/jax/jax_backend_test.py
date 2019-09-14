e195746db83ae3273e747f98c70d22b5eb135cff
Martin <martin.ganahl@gmail.com>
2019-09-14 00:11:46 -0400

    dtypes

--- tensornetwork/backends/jax/jax_backend_test.py
+++ tensornetwork/backends/jax/jax_backend_test.py
@@ -9,10 +9,8 @@ import pytest
 from tensornetwork.backends.jax import jax_backend
 import tensornetwork.config as config_file
 
-np_dtypes = config_file.supported_numpy_dtypes
-np_randn_dtypes = [
-    np.float32, np.float16, np.float64, np.complex64, np.complex128
-]
+np_randn_dtypes = [np.float32, np.float16, np.float64]
+np_dtypes = np_randn_dtypes + [np.complex64, np.complex128]
 
 
 def test_tensordot():
