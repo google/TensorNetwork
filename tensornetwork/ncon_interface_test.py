931aca3a92ec2f0c3c592419a0c27eb1c5ac48fc
Martin <martin.ganahl@gmail.com>
2019-10-01 19:01:46 -0400

    typo

--- tensornetwork/ncon_interface_test.py
+++ tensornetwork/ncon_interface_test.py
@@ -1,4 +1,4 @@
-x  # Copyright 2019 The TensorNetwork Authors
+# Copyright 2019 The TensorNetwork Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
