3f519245f95329c4fefbf56fc586699d0e3a3690
Martin <martin.ganahl@gmail.com>
2019-10-01 11:31:30 -0400

    fix typing of Algorithm

--- tensornetwork/contractors/opt_einsum_paths/utils.py
+++ tensornetwork/contractors/opt_einsum_paths/utils.py
@@ -17,9 +17,7 @@ from tensornetwork.network import TensorNetwork
 from tensornetwork.network_components import BaseNode, Edge
 from typing import Any, Callable, Dict, List, Set, Tuple, Iterable, Union
 # `opt_einsum` algorithm method typing
-#Algorithm = Callable[[List[Set[Edge]], Set[Edge], Dict[Edge, Any]], List[
-#    Tuple[int, int]]]
-Algorithm = Callable[[List[Set[int]], Set[int], Dict[int, int]], List[
+Algorithm = Callable[[List[Set[Edge]], Set[Edge], Dict[Edge, Any]], List[
     Tuple[int, int]]]
 
 
