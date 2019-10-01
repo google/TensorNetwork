326c3a4f5fb4f1707aefe3099c4d4310590b2658
Martin <martin.ganahl@gmail.com>
2019-10-01 19:04:10 -0400

    typo

--- tensornetwork/ncon_interface.py
+++ tensornetwork/ncon_interface.py
@@ -20,7 +20,7 @@ import warnings
 from typing import Any, Sequence, List, Optional, Union, Text, Tuple, Dict, Set
 from tensornetwork import network
 from tensornetwork import network_components
-from tensornetwor import config
+from tensornetwork import config
 from tensornetwork.backends import backend_factory
 Tensor = Any
 
