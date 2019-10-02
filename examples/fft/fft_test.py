d55538b6448508a402e4f1e88edd22d7444c0c29
Chase Roberts <chaseriley@google.com>
2019-10-02 14:56:40 -0700

    Fixed bad spacing

--- examples/fft/fft_test.py
+++ examples/fft/fft_test.py
@@ -10,15 +10,15 @@ import tensornetwork as tn
 
 
 def test_fft(self):
-n = 3
-initial_state = [complex(0)] * (1 << n)
-initial_state[1] = 1j
-initial_state[5] = -1
-initial_node = tn.Node(np.array(initial_state).reshape((2,) * n))
+  n = 3
+  initial_state = [complex(0)] * (1 << n)
+  initial_state[1] = 1j
+  initial_state[5] = -1
+  initial_node = tn.Node(np.array(initial_state).reshape((2,) * n))
 
-fft_out = fft.add_fft([initial_node[k] for k in range(n)])
-result = tn.contractors.greedy(tn.reachable(fft_out[0].node1), fft_out)
-tn.flatten_edges(fft_out)
-actual = result.tensor
-expected = np.fft.fft(initial_state, norm="ortho")
-self.assertAllClose(expected, actual)
+  fft_out = fft.add_fft([initial_node[k] for k in range(n)])
+  result = tn.contractors.greedy(tn.reachable(fft_out[0].node1), fft_out)
+  tn.flatten_edges(fft_out)
+  actual = result.tensor
+  expected = np.fft.fft(initial_state, norm="ortho")
+  self.assertAllClose(expected, actual)
