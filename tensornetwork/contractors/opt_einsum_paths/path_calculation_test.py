34385db5ad0f9bb9ac0e0bbd25b615a362874d54
michaelmarien <marien.mich@gmail.com>
2019-10-18 22:13:49 +0200

    removed TensorNetwork from path calculation tests

--- tensornetwork/contractors/opt_einsum_paths/path_calculation_test.py
+++ tensornetwork/contractors/opt_einsum_paths/path_calculation_test.py
@@ -19,7 +19,7 @@ github.com/dgasmith/opt_einsum/blob/master/opt_einsum/tests/test_paths.py
 import numpy as np
 import opt_einsum
 import pytest
-import tensornetwork
+import tensornetwork as tn
 from tensornetwork.contractors.opt_einsum_paths import utils
 
 
@@ -42,30 +42,28 @@ def check_path(calculated_path, correct_path):
 # `TensorNetwork`.
 def gemm_network():
   """Creates 'GEMM1' contraction from `opt_einsum` tests."""
-  net = tensornetwork.TensorNetwork()
-  x = net.add_node(np.ones([1, 2, 4]))
-  y = net.add_node(np.ones([1, 3]))
-  z = net.add_node(np.ones([2, 4, 3]))
+  x = tn.Node(np.ones([1, 2, 4]))
+  y = tn.Node(np.ones([1, 3]))
+  z = tn.Node(np.ones([2, 4, 3]))
   # pylint: disable=pointless-statement
   x[0] ^ y[0]
   x[1] ^ z[0]
   x[2] ^ z[1]
   y[1] ^ z[2]
-  return net
+  return [x, y, z]
 
 
 def inner_network():
   """Creates a (modified) `Inner1` contraction from `opt_einsum` tests."""
-  net = tensornetwork.TensorNetwork()
-  x = net.add_node(np.ones([5, 2, 3, 4]))
-  y = net.add_node(np.ones([5, 3]))
-  z = net.add_node(np.ones([2, 4]))
+  x = tn.Node(np.ones([5, 2, 3, 4]))
+  y = tn.Node(np.ones([5, 3]))
+  z = tn.Node(np.ones([2, 4]))
   # pylint: disable=pointless-statement
   x[0] ^ y[0]
   x[1] ^ z[0]
   x[2] ^ y[1]
   x[3] ^ z[1]
-  return net
+  return [x, y, z]
 
 
 def matrix_chain():
@@ -73,13 +71,12 @@ def matrix_chain():
 
   The `greedy` algorithm does not find the optimal path in this case!
   """
-  net = tensornetwork.TensorNetwork()
   d = [10, 8, 6, 4, 2]
-  nodes = [net.add_node(np.ones([d1, d2])) for d1, d2 in zip(d[:-1], d[1:])]
+  nodes = [tn.Node(np.ones([d1, d2])) for d1, d2 in zip(d[:-1], d[1:])]
   for a, b in zip(nodes[:-1], nodes[1:]):
     # pylint: disable=pointless-statement
     a[1] ^ b[0]
-  return net
+  return nodes
 
 
 # Parametrize tests by giving:
@@ -101,8 +98,8 @@ test_list = [
 def test_path_optimal(params):
   algorithm_name, network_name, correct_path = params
 
-  net = globals()[network_name]()
+  nodes = globals()[network_name]()
   path_algorithm = getattr(opt_einsum.paths, algorithm_name)
 
-  calculated_path, _ = utils.get_path(net, path_algorithm)
+  calculated_path, _ = utils.get_path(nodes, path_algorithm)
   assert check_path(calculated_path, correct_path)
