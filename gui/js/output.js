5a2471f0f3673f1346545dca6471f065f0597557
Cutter Coryell <coryell>
2019-10-22 13:46:53 -0700

    GUI: add axis and edge names to TensorNetwork output code
    - also changed ... to np.zeros of appropriate shape so code can be
    executed without modification

--- gui/js/output.js
+++ gui/js/output.js
@@ -20,24 +20,57 @@ Vue.component(
         },
         computed: {
             outputCode: function() {
-                let code = `import tensornetwork as tn\n`;
+                let code = `import numpy as np\nimport tensornetwork as tn\n`;
 
                 code += `\n# Node definitions\n`;
-                code += `# TODO: fill in ... values\n\n`;
+                code += `# TODO: replace np.zeros with actual values\n\n`;
 
                 for (let i = 0; i < this.state.nodes.length; i++) {
                     let node = this.state.nodes[i];
-                    code += `${node.name} = tn.Node(..., name="${node.name}")\n`;
+                    let values = this.placeholderValues(node);
+                    let axes = this.axisNames(node);
+                    code += `${node.name} = tn.Node(${values}, name="${node.name}"${axes})\n`;
                 }
 
                 code += `\n# Edge definitions\n\n`;
 
                 for (let i = 0; i < this.state.edges.length; i++) {
                     let edge = this.state.edges[i];
-                    code += `${edge[0][0]}[${edge[0][1]}] ^ ${edge[1][0]}[${edge[1][1]}]\n`;
+                    let name = this.edgeName(edge);
+                    code += `tn.connect(${edge[0][0]}[${edge[0][1]}], ${edge[1][0]}[${edge[1][1]}]${name})\n`;
                 }
 
                 return code;
+            }
+        },
+        methods: {
+            placeholderValues: function(node) {
+                let code = `np.zeros((`;
+                for (let i = 0; i < node.axes.length; i++) {
+                    code += `0, `;
+                }
+                code += `))`;
+                return code;
+            },
+            axisNames: function(node) {
+                let code = `, axis_names=[`;
+                let willOutput = false;
+                for (let i = 0; i < node.axes.length; i++) {
+                    let axis = node.axes[i];
+                    if (axis) {
+                        willOutput = true;
+                        code += `"${axis}", `
+                    }
+                    else {
+                        code += `None, `
+                    }
+                }
+                code += `]`;
+                return willOutput ? code : ``;
+            },
+            edgeName: function(edge) {
+                let name = edge[2];
+                return name ? `, name="${name}"` : ``;
             }
         },
 		template: `
