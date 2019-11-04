4a9e4edf04c03b5ac93be214fb97caaabadb0d6b
maga <martin.ganahl@gmail.com>
2019-11-04 15:49:29 -0500

    Revert "fixed docstrings of eigsh_lanczos"
    This reverts commit d2afc924baf9f956406e7ac9cee957bc6d4d388e.

--- tensornetwork/backends/pytorch/pytorch_backend.py
+++ tensornetwork/backends/pytorch/pytorch_backend.py
@@ -142,16 +142,16 @@ class PyTorchBackend(base_backend.BaseBackend):
   def conj(self, tensor: Tensor) -> Tensor:
     return tensor  #pytorch does not support complex dtypes
 
-  def eigsh_lanczos(self,
-                    A: Callable,
-                    initial_state: Optional[Tensor] = None,
-                    ncv: Optional[int] = 200,
-                    numeig: Optional[int] = 1,
-                    tol: Optional[float] = 1E-8,
-                    delta: Optional[float] = 1E-8,
-                    ndiag: Optional[int] = 20,
-                    reorthogonalize: Optional[bool] = False
-                   ) -> Tuple[List, List]:
+  def eigsh_lanczos(
+      self,
+      A: Callable,
+      initial_state: Optional[Tensor] = None,
+      ncv: Optional[int] = 200,
+      numeig: Optional[int] = 1,
+      tol: Optional[float] = 1E-8,
+      delta: Optional[float] = 1E-8,
+      ndiag: Optional[int] = 20,
+      reorthogonalize: Optional[bool] = False) -> Tuple[List, List]:
     """
     Lanczos method for finding the lowest eigenvector-eigenvalue pairs
     of a `LinearOperator` `A`.
@@ -167,9 +167,15 @@ class PyTorchBackend(base_backend.BaseBackend):
         as stopping criterion between two diagonalization steps of the
         tridiagonal operator.
       delta: Stopping criterion for Lanczos iteration.
+<<<<<<< HEAD
         If a Krylov vector `x_n` has an L2 norm ||x_n|| < delta, the iteration 
         is stopped. It means that an (approximate) invariant subspace has been 
         found.
+=======
+        If two successive Krylov vectors `x_m` and `x_n`
+        have an overlap abs(<x_m|x_n>) < delta, the iteration is stopped.
+        It means that an (approximate) invariant subspace has been found.
+>>>>>>> parent of d2afc92... fixed docstrings of eigsh_lanczos
       ndiag: The tridiagonal Operator is diagonalized every `ndiag` 
         iterations to check convergence.
       reorthogonalize: If `True`, Krylov vectors are kept orthogonal by 
