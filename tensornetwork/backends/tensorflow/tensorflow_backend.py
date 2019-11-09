48d60a853e9dc205db2b7c08f9b218aa0296eddc
Martin <martin.ganahl@gmail.com>
2019-11-09 13:13:37 -0500

    added eigh

--- tensornetwork/backends/tensorflow/tensorflow_backend.py
+++ tensornetwork/backends/tensorflow/tensorflow_backend.py
@@ -52,9 +52,8 @@ class TensorFlowBackend(base_backend.BaseBackend):
                         max_singular_values: Optional[int] = None,
                         max_truncation_error: Optional[float] = None
                        ) -> Tuple[Tensor, Tensor, Tensor, Tensor]:
-    return decompositions.svd_decomposition(self.tf, tensor, split_axis,
-                                            max_singular_values,
-                                            max_truncation_error)
+    return decompositions.svd_decomposition(
+        self.tf, tensor, split_axis, max_singular_values, max_truncation_error)
 
   def qr_decomposition(self, tensor: Tensor,
                        split_axis: int) -> Tuple[Tensor, Tensor]:
@@ -134,6 +133,9 @@ class TensorFlowBackend(base_backend.BaseBackend):
   def conj(self, tensor: Tensor) -> Tensor:
     return self.tf.math.conj(tensor)
 
+  def eigh(self, matrix: Tensor):
+    return self.tf.linalg.eigh(matrix)
+
   def eigs(self,
            A: Callable,
            initial_state: Optional[Tensor] = None,
@@ -145,16 +147,16 @@ class TensorFlowBackend(base_backend.BaseBackend):
            dtype: Optional[Type] = None) -> Tuple[List, List]:
     raise NotImplementedError()
 
-  def eigsh_lanczos(self,
-                    A: Callable,
-                    initial_state: Optional[Tensor] = None,
-                    ncv: Optional[int] = 200,
-                    numeig: Optional[int] = 1,
-                    tol: Optional[float] = 1E-8,
-                    delta: Optional[float] = 1E-8,
-                    ndiag: Optional[int] = 20,
-                    reorthogonalize: Optional[bool] = False
-                   ) -> Tuple[List, List]:
+  def eigsh_lanczos(
+      self,
+      A: Callable,
+      initial_state: Optional[Tensor] = None,
+      ncv: Optional[int] = 200,
+      numeig: Optional[int] = 1,
+      tol: Optional[float] = 1E-8,
+      delta: Optional[float] = 1E-8,
+      ndiag: Optional[int] = 20,
+      reorthogonalize: Optional[bool] = False) -> Tuple[List, List]:
     raise NotImplementedError(
         "Backend '{}' has not implemented eighs_lanczos.".format(self.name))
 
