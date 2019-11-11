3998fdc5ba48c8aa2f8e3441d74656e5bf8ebf3b
Martin <martin.ganahl@gmail.com>
2019-11-11 15:13:48 -0500

    remove test

--- tensornetwork/backends/numpy/numpy_backend_test.py
+++ tensornetwork/backends/numpy/numpy_backend_test.py
@@ -344,20 +344,9 @@ def test_eigh(dtype):
 
 
 @pytest.mark.parametrize("dtype", np_dtypes)
-def index_update_value(dtype):
+def test_index_update(dtype):
   backend = numpy_backend.NumPyBackend()
   tensor = backend.randn((4, 2, 3), dtype=dtype, seed=10)
   out = backend.index_update(tensor, tensor > 0.1, 0)
-  tensor[tensor > 0.1] = 0.0
-  np.testing.assert_allclose(tensor, out)
-
-
-@pytest.mark.parametrize("dtype", np_dtypes)
-def index_update_tensor(dtype):
-  backend = numpy_backend.NumPyBackend()
-  tensor = backend.randn((4, 2, 3), dtype=dtype, seed=10)
-  assignee = backend.randn((4, 2, 3), dtype=dtype, seed=12)
-  out = backend.index_update(tensor, tensor > 0.1, assignee)
-  out = bla
   tensor[tensor > 0.1] = 0.0
   np.testing.assert_allclose(tensor, out)
