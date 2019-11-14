a2fac8c89bdd25af149c273faefc91a591b1ec9c
martin.ganahl@gmail.com <martin.ganahl@gmail.com>
2019-11-14 15:07:13 -0500

    fix merge issues

--- tensornetwork/matrixproductstates/infinite_mps.py
+++ tensornetwork/matrixproductstates/infinite_mps.py
@@ -24,6 +24,7 @@ from tensornetwork.backends import backend_factory
 from tensornetwork.network_operations import split_node_qr, split_node_rq, split_node_full_svd, norm, conj
 from typing import Any, List, Optional, Text, Type, Union, Dict, Sequence
 from tensornetwork.matrixproductstates.base_mps import BaseMPS
+from tensornetwork.ncon_interface import ncon
 Tensor = Any
 
 
@@ -174,3 +175,147 @@ class InfiniteMPS(BaseMPS):
 
   def save(self, path: str):
     raise NotImplementedError()
+
+  # pylint: disable=arguments-differ
+  def canonicalize(
+      self,
+      left_initial_state: Optional[Union[BaseNode, Tensor]] = None,
+      right_initial_state: Optional[Union[BaseNode, Tensor]] = None,
+      precision: Optional[float] = 1E-10,
+      truncation_threshold: Optional[float] = 1E-15,
+      D: Optional[int] = None,
+      num_krylov_vecs: Optional[int] = 50,
+      maxiter: Optional[int] = 1000,
+      pseudo_inverse_cutoff: Optional[float] = None):
+    """
+    Canonicalize an InfiniteMPS (i.e. bring it into Schmidt-canonical form).
+
+    Args:
+      left_initial_state: An initial guess for the left eigenvector of
+        the unit-cell mps transfer matrix
+      right_initial_state: An initial guess for the right eigenvector of
+        the unit-cell transfer matrix
+      precision: The desired precision of the dominant eigenvalues (passed
+        to InfiniteMPS.transfer_matrix_eigs)
+      truncation_threshold: Truncation threshold for Schmidt-values at the 
+        boundaries of the mps.
+      D: The maximum number of Schmidt values to be kept at the boundaries 
+        of the mps.
+      num_krylov_vecs: Number of Krylov vectors to diagonalize transfer_matrix
+      maxiter: Maximum number of iterations in `eigs`
+      pseudo_inverse_cutoff: A cutoff for taking the Moore-Penrose pseudo-inverse
+        of a matrix. Given the SVD of a matrix :math:`M=U S V`, the inverse is 
+        is computed as :math:`V^* S^{-1}_+ U^*`, where :math:`S^{-1}_+` equals
+        `S^{-1}` for all values in `S` which are larger than `pseudo_inverse_cutoff`,
+         and is 0 for all others.
+    Returns:
+      None
+    """
+    # bring center_position to 0
+    self.position(0)
+    # dtype of eta is the same as InfiniteMPS.dtype
+    # this is assured in the backend.
+    eta, l = self.transfer_matrix_eigs(
+        direction='left',
+        initial_state=left_initial_state,
+        precision=precision,
+        num_krylov_vecs=num_krylov_vecs,
+        maxiter=maxiter)
+    sqrteta = self.backend.sqrt(eta)
+    self.nodes[0].tensor /= sqrteta
+
+    # TODO: would be nice to do the algebra directly on the nodes here
+    l.tensor /= self.backend.trace(l.tensor)
+    l.tensor = (l.tensor +
+                self.backend.transpose(self.backend.conj(l.tensor),
+                                       (1, 0))) / 2.0
+
+    # eigvals_left and u_left are both `Tensor` objects
+    eigvals_left, u_left = self.backend.eigh(l.tensor)
+    eigvals_left /= self.backend.norm(eigvals_left)
+    if pseudo_inverse_cutoff:
+      mask = eigvals_left <= pseudo_inverse_cutoff
+
+    inveigvals_left = 1.0 / eigvals_left
+    if pseudo_inverse_cutoff:
+      inveigvals_left = self.backend.index_update(inveigvals_left, mask, 0.0)
+
+    y = Node(
+        ncon(
+            [u_left, self.backend.diag(self.backend.sqrt(eigvals_left))],
+            [[-2, 1], [1, -1]],
+            backend=self.backend.name),
+        backend=self.backend)
+    invy = Node(
+        ncon([
+            self.backend.diag(self.backend.sqrt(inveigvals_left)),
+            self.backend.conj(u_left)
+        ], [[-2, 1], [-1, 1]],
+             backend=self.backend.name),
+        backend=self.backend)
+
+    eta, r = self.transfer_matrix_eigs(
+        direction='right',
+        initial_state=right_initial_state,
+        precision=precision,
+        num_krylov_vecs=num_krylov_vecs,
+        maxiter=maxiter)
+
+    r.tensor /= self.backend.trace(r.tensor)
+    r.tensor = (r.tensor +
+                self.backend.transpose(self.backend.conj(r.tensor),
+                                       (1, 0))) / 2.0
+    # eigvals_left and u_left are both `Tensor` objects
+    eigvals_right, u_right = self.backend.eigh(r.tensor)
+    eigvals_right /= self.backend.norm(eigvals_right)
+    if pseudo_inverse_cutoff:
+      mask = eigvals_right <= pseudo_inverse_cutoff
+
+    inveigvals_right = 1.0 / eigvals_right
+    if pseudo_inverse_cutoff:
+      inveigvals_right = self.backend.index_update(inveigvals_right, mask, 0.0)
+
+    x = Node(
+        ncon([u_right,
+              self.backend.diag(self.backend.sqrt(eigvals_right))],
+             [[-1, 1], [1, -2]],
+             backend=self.backend.name),
+        backend=self.backend)
+
+    invx = Node(
+        ncon([
+            self.backend.diag(self.backend.sqrt(inveigvals_right)),
+            self.backend.conj(u_right)
+        ], [[-1, 1], [-2, 1]],
+             backend=self.backend.name),
+        backend=self.backend)
+
+    tmp = Node(
+        ncon([y, x], [[-1, 1], [1, -2]], backend=self.backend.name),
+        backend=self.backend)
+    U, lam, V, _ = split_node_full_svd(
+        tmp, [tmp[0]], [tmp[1]],
+        max_singular_values=D,
+        max_truncation_err=truncation_threshold)
+    # absorb lam*V*invx into the left-most mps tensor
+    self.nodes[0] = ncon([lam, V, invx, self.nodes[0]],
+                         [[-1, 1], [1, 2], [2, 3], [3, -2, -3]])
+
+    # absorb connector * invy * U * lam into the right-most tensor
+    # Note that lam is absorbed here, which means that the state
+    # is in the parallel decomposition
+    # Note that we absorb connector_matrix here
+    self.nodes[-1] = ncon([self.get_node(len(self) - 1), invy, U, lam],
+                          [[-1, -2, 1], [1, 2], [2, 3], [3, -3]])
+    # now do a sweep of QR decompositions to bring the mps tensors into
+    # left canonical form (except the last one)
+    self.position(len(self) - 1)
+    # TODO: lam is a diagonal matrix, but we're not making
+    # use of it the moment
+    lam_norm = self.backend.norm(lam.tensor)
+    lam.tensor /= lam_norm
+    self.center_position = len(self) - 1
+    self.connector_matrix = Node(
+        self.backend.inv(lam.tensor), backend=self.backend)
+
+    return lam_norm
