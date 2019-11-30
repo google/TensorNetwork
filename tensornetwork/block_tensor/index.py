307f2dc4ed005eaf318661a166553b8ec1cb5d3f
Martin <martin.ganahl@gmail.com>
2019-11-29 22:08:41 -0500

    added Index, an index type for symmetric tensors

---
+++ tensornetwork/block_tensor/index.py
@@ -0,0 +1,177 @@
+# Copyright 2019 The TensorNetwork Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+import numpy as np
+from tensornetwork.network_components import Node, contract, contract_between
+# pylint: disable=line-too-long
+from tensornetwork.backends import backend_factory
+
+import numpy as np
+import copy
+from typing import List, Union, Any, Optional, Tuple, Text
+
+
+class Index:
+  """
+  An index class to store indices of a symmetric tensor.
+  An index keeps track of all its childs by storing references
+  to them (i.e. it is a binary tree).
+  """
+
+  def __init__(self,
+               charges: Union[List, np.ndarray],
+               flow: int,
+               name: Optional[Text] = None,
+               left_child: Optional["Index"] = None,
+               right_child: Optional["Index"] = None):
+    self.charges = np.asarray(charges)
+    self.flow = flow
+    self.left_child = left_child
+    self.right_child = right_child
+    self.name = name if name else 'index'
+
+  @property
+  def dimension(self):
+    return len(self.charges)
+
+  def _copy_helper(self, index: "Index", copied_index: "Index") -> None:
+    """
+    Helper function for copy
+    """
+    if index.left_child != None:
+      left_copy = Index(
+          charges=index.left_child.charges.copy(),
+          flow=copy.copy(index.left_child.flow),
+          name=index.left_child.name)
+      copied_index.left_child = left_copy
+      self._copy_helper(index.left_child, left_copy)
+    if index.right_child != None:
+      right_copy = Index(
+          charges=index.right_child.charges.copy(),
+          flow=copy.copy(index.right_child.flow),
+          name=index.right_child.name)
+      copied_index.right_child = right_copy
+      self._copy_helper(index.right_child, right_copy)
+
+  def copy(self):
+    """
+    Returns:
+      Index: A deep copy of `Index`. Note that all children of
+        `Index` are copied as well.
+    """
+    index_copy = Index(
+        charges=self.charges.copy(), flow=copy.copy(self.flow), name=self.name)
+
+    self._copy_helper(self, index_copy)
+    return index_copy
+
+  def _leave_helper(self, index: "Index", leave_list: List) -> None:
+    if index.left_child:
+      self._leave_helper(index.left_child, leave_list)
+    if index.right_child:
+      self._leave_helper(index.right_child, leave_list)
+    if (index.left_child is None) and (index.right_child is None):
+      leave_list.append(index)
+
+  def get_elementary_indices(self) -> List:
+    """
+    Returns:
+    List: A list containing the elementary indices (the leaves) 
+      of `Index`.
+    """
+    leave_list = []
+    self._leave_helper(self, leave_list)
+    return leave_list
+
+
+def fuse_charges(q1: Union[List, np.ndarray], flow1: int,
+                 q2: Union[List, np.ndarray], flow2: int) -> np.ndarray:
+  """
+  Fuse charges `q1` with charges `q2` by simple addition (valid
+  for U(1) charges). `q1` and `q2` typically belong to two consecutive
+  legs of `BlockSparseTensor`.
+  Given `q1 = [0,1,2]` and `q2 = [10,100]`, this returns
+  `[10, 11, 12, 100, 101, 102]`.
+  When using column-major ordering of indices in `BlockSparseTensor`, 
+  the position of q1 should be "to the left" of the position of q2.
+  Args:
+    q1: Iterable of integers
+    flow1: Flow direction of charge `q1`.
+    q2: Iterable of integers
+    flow2: Flow direction of charge `q2`.
+  Returns:
+    np.ndarray: The result of fusing `q1` with `q2`.
+  """
+  return np.reshape(
+      flow2 * np.asarray(q2)[:, None] + flow1 * np.asarray(q1)[None, :],
+      len(q1) * len(q2))
+
+
+def fuse_index_pair(left_index: Index,
+                    right_index: Index,
+                    flow: Optional[int] = 1) -> Index:
+  """
+  Fuse two consecutive indices (legs) of a symmetric tensor.
+  Args:
+    left_index: A tensor Index.
+    right_index: A tensor Index.
+    flow: An optional flow of the resulting `Index` object.
+  Returns:
+    Index: The result of fusing `index1` and `index2`.
+  """
+  #Fuse the charges of the two indices
+  if left_index is right_index:
+    raise ValueError(
+        "index1 and index2 are the same object. Can only fuse distinct objects")
+
+  fused_charges = fuse_charges(left_index.charges, left_index.flow,
+                               right_index.charges, right_index.flow)
+  return Index(
+      charges=fused_charges,
+      flow=flow,
+      left_child=left_index,
+      right_child=right_index)
+
+
+def fuse_indices(indices: List[Index], flow: Optional[int] = 1) -> Index:
+  """
+  Fuse a list of indices (legs) of a symmetric tensor.
+  Args:
+    indices: A list of tensor Index objects
+    flow: An optional flow of the resulting `Index` object.
+  Returns:
+    Index: The result of fusing `indices`.
+  """
+
+  index = indices[0]
+  for n in range(1, len(indices)):
+    index = fuse_index_pair(index, indices[n], flow=flow)
+  return index
+
+
+def split_index(index: Index) -> Tuple[Index, Index]:
+  """
+  Split an index (leg) of a symmetric tensor into two legs.
+  Args:
+    index: A tensor Index.
+  Returns:
+    Tuple[Index, Index]: The result of splitting `index`.
+  """
+  if (not index.left_child) or (not index.right_child):
+    raise ValueError("cannot split an elementary index")
+
+  return index.left_child, index.right_child
