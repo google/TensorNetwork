04c40283b98d8b31e1e0dac13a0cca6a3a3def84
mganahl <martin.ganahl@gmail.com>
2019-12-18 08:38:51 -0500

    added tests

--- tensornetwork/block_tensor/index_test.py
+++ tensornetwork/block_tensor/index_test.py
@@ -50,15 +50,12 @@ def test_elementary_indices():
   D = 10
   B = 4
   dtype = np.int16
-  q1 = np.random.randint(-B // 2, B // 2 + 1,
-                         D).astype(dtype)  #quantum numbers on leg 1
-  q2 = np.random.randint(-B // 2, B // 2 + 1,
-                         D).astype(dtype)  #quantum numbers on leg 2
-  i1 = Index(charges=q1, flow=1, name='index1')  #index on leg 1
-  i2 = Index(charges=q2, flow=1, name='index2')  #index on leg 2
-
-  i3 = Index(charges=q1, flow=1, name='index1')  #index on leg 1
-  i4 = Index(charges=q2, flow=1, name='index2')  #index on leg 2
+  q1 = np.random.randint(-B // 2, B // 2 + 1, D).astype(dtype)
+  q2 = np.random.randint(-B // 2, B // 2 + 1, D).astype(dtype)
+  i1 = Index(charges=q1, flow=1, name='index1')
+  i2 = Index(charges=q2, flow=1, name='index2')
+  i3 = Index(charges=q1, flow=1, name='index3')
+  i4 = Index(charges=q2, flow=1, name='index4')
 
   i12 = i1 * i2
   i34 = i3 * i4
@@ -72,3 +69,26 @@ def test_elementary_indices():
   assert elmt1234[1] is i2
   assert elmt1234[2] is i3
   assert elmt1234[3] is i4
+
+
+def test_copy():
+  D = 10
+  B = 4
+  dtype = np.int16
+  q1 = np.random.randint(-B // 2, B // 2 + 1, D).astype(dtype)
+  q2 = np.random.randint(-B // 2, B // 2 + 1, D).astype(dtype)
+  i1 = Index(charges=q1, flow=1, name='index1')
+  i2 = Index(charges=q2, flow=1, name='index2')
+  i3 = Index(charges=q1, flow=-1, name='index3')
+  i4 = Index(charges=q2, flow=-1, name='index4')
+
+  i12 = i1 * i2
+  i34 = i3 * i4
+  i1234 = i12 * i34
+  i1234_copy = i1234.copy()
+
+  elmt1234 = i1234_copy.get_elementary_indices()
+  assert elmt1234[0] is not i1
+  assert elmt1234[1] is not i2
+  assert elmt1234[2] is not i3
+  assert elmt1234[3] is not i4
