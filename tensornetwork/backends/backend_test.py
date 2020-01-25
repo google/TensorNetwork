631477f9747d8bedfb1cfabbbaa96df19a743b69
michaelmarien <marien.mich@gmail.com>
2020-01-25 10:05:54 +0100

    notimplemented tests for base backend

--- tensornetwork/backends/backend_test.py
+++ tensornetwork/backends/backend_test.py
@@ -4,6 +4,7 @@ import sys
 import pytest
 import numpy as np
 from tensornetwork import connect, contract, Node
+from tensornetwork.backends.base_backend import BaseBackend
 
 
 def clean_tensornetwork_modules():
@@ -146,3 +147,194 @@ def test_basic_network_without_backends_raises_error():
     Node(np.ones((2, 2)), backend="tensorflow")
   with pytest.raises(ImportError):
     Node(np.ones((2, 2)), backend="pytorch")
+[]
+
+def test_base_backend_name():
+  backend = BaseBackend()
+  assert backend.name == "base backend"
+
+
+def test_base_backend_tensordot_not_implemented():
+  backend = BaseBackend()
+  with pytest.raises(NotImplementedError):
+    backend.tensordot(np.ones((2, 2)), np.ones((2, 2)), axes=[0, 0])
+
+
+def test_base_backend_reshape_not_implemented():
+  backend = BaseBackend()
+  with pytest.raises(NotImplementedError):
+    backend.reshape(np.ones((2, 2)), (4, 1))
+
+
+def test_base_backend_transpose_not_implemented():
+  backend = BaseBackend()
+  with pytest.raises(NotImplementedError):
+    backend.transpose(np.ones((2, 2)), [0, 1])
+
+
+def test_base_backend_svd_decompositon_not_implemented():
+  backend = BaseBackend()
+  with pytest.raises(NotImplementedError):
+    backend.svd_decomposition(np.ones((2, 2)), 0)
+
+
+def test_base_backend_qr_decompositon_not_implemented():
+  backend = BaseBackend()
+  with pytest.raises(NotImplementedError):
+    backend.qr_decomposition(np.ones((2, 2)), 0)
+
+
+def test_base_backend_rq_decompositon_not_implemented():
+  backend = BaseBackend()
+  with pytest.raises(NotImplementedError):
+    backend.rq_decomposition(np.ones((2, 2)), 0)
+
+
+def test_base_backend_shape_concat_not_implemented():
+  backend = BaseBackend()
+  with pytest.raises(NotImplementedError):
+    backend.shape_concat([np.ones((2, 2)), np.ones((2, 2))], 0)
+
+
+def test_base_backend_shape_tensor_not_implemented():
+  backend = BaseBackend()
+  with pytest.raises(NotImplementedError):
+    backend.shape_tensor(np.ones((2, 2)))
+
+
+def test_base_backend_shape_tuple_not_implemented():
+  backend = BaseBackend()
+  with pytest.raises(NotImplementedError):
+    backend.shape_tuple(np.ones((2, 2)))
+
+
+def test_base_backend_shape_prod_not_implemented():
+  backend = BaseBackend()
+  with pytest.raises(NotImplementedError):
+    backend.shape_prod(np.ones((2, 2)))
+
+
+def test_base_backend_sqrt_not_implemented():
+  backend = BaseBackend()
+  with pytest.raises(NotImplementedError):
+    backend.sqrt(np.ones((2, 2)))
+
+
+def test_base_backend_diag_not_implemented():
+  backend = BaseBackend()
+  with pytest.raises(NotImplementedError):
+    backend.diag(np.ones((2, 2)))
+
+
+def test_base_backend_convert_to_tensor_not_implemented():
+  backend = BaseBackend()
+  with pytest.raises(NotImplementedError):
+    backend.convert_to_tensor(np.ones((2, 2)))
+
+
+def test_base_backend_trace_not_implemented():
+  backend = BaseBackend()
+  with pytest.raises(NotImplementedError):
+    backend.trace(np.ones((2, 2)))
+
+
+def test_base_backend_outer_product_not_implemented():
+  backend = BaseBackend()
+  with pytest.raises(NotImplementedError):
+    backend.outer_product(np.ones((2, 2)), np.ones((2, 2)))
+
+
+def test_base_backend_einsul_not_implemented():
+  backend = BaseBackend()
+  with pytest.raises(NotImplementedError):
+    backend.einsum("ii", np.ones((2, 2)))
+
+
+def test_base_backend_norm_not_implemented():
+  backend = BaseBackend()
+  with pytest.raises(NotImplementedError):
+    backend.norm(np.ones((2, 2)))
+
+
+def test_base_backend_eye_not_implemented():
+  backend = BaseBackend()
+  with pytest.raises(NotImplementedError):
+    backend.eye(2, dtype=np.float64)
+
+
+def test_base_backend_ones_not_implemented():
+  backend = BaseBackend()
+  with pytest.raises(NotImplementedError):
+    backend.ones((2, 2), dtype=np.float64)
+
+
+def test_base_backend_zeros_not_implemented():
+  backend = BaseBackend()
+  with pytest.raises(NotImplementedError):
+    backend.zeros((2, 2), dtype=np.float64)
+
+
+def test_base_backend_randn_not_implemented():
+  backend = BaseBackend()
+  with pytest.raises(NotImplementedError):
+    backend.randn((2, 2))
+
+
+def test_base_backend_random_uniforl_not_implemented():
+  backend = BaseBackend()
+  with pytest.raises(NotImplementedError):
+    backend.random_uniform((2, 2))
+
+
+def test_base_backend_conj_not_implemented():
+  backend = BaseBackend()
+  with pytest.raises(NotImplementedError):
+    backend.conj(np.ones((2, 2)))
+
+
+def test_base_backend_eigh_not_implemented():
+  backend = BaseBackend()
+  with pytest.raises(NotImplementedError):
+    backend.eigh(np.ones((2, 2)))
+
+
+def test_base_backend_eigs_not_implemented():
+  backend = BaseBackend()
+  with pytest.raises(NotImplementedError):
+    backend.eigs(np.ones((2, 2)))
+
+
+def test_base_backend_eigs_lanczos_not_implemented():
+  backend = BaseBackend()
+  with pytest.raises(NotImplementedError):
+    backend.eigsh_lanczos(np.ones((2, 2)))
+
+
+def test_base_backend_addition_not_implemented():
+  backend = BaseBackend()
+  with pytest.raises(NotImplementedError):
+    backend.addition(np.ones((2, 2)), np.ones((2, 2)))
+
+
+def test_base_backend_multiply_not_implemented():
+  backend = BaseBackend()
+  with pytest.raises(NotImplementedError):
+    backend.multiply(np.ones((2, 2)), np.ones((2, 2)))
+
+
+def test_base_backend_divide_not_implemented():
+  backend = BaseBackend()
+  with pytest.raises(NotImplementedError):
+    backend.divide(np.ones((2, 2)), np.ones((2, 2)))
+
+
+def test_base_backend_index_update_not_implemented():
+  backend = BaseBackend()
+  with pytest.raises(NotImplementedError):
+    backend.index_update(np.ones((2, 2)), np.ones((2, 2)), np.ones((2, 2)))
+
+
+def test_base_backend_inv_not_implemented():
+  backend = BaseBackend()
+  with pytest.raises(NotImplementedError):
+    backend.inv(np.ones((2, 2)))
