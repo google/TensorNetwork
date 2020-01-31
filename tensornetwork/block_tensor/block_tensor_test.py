9615d105d512685883cf4cd2af91c59037886acd
mganahl <martin.ganahl@gmail.com>
2020-01-31 09:43:54 -0500

    added bunch of tests

--- tensornetwork/block_tensor/block_tensor_test.py
+++ tensornetwork/block_tensor/block_tensor_test.py
@@ -8,6 +8,52 @@ from tensornetwork.block_tensor.block_tensor import compute_num_nonzero, reduce_
 np_dtypes = [np.float32, np.float16, np.float64, np.complex64, np.complex128]
 
 
+def get_contractable_tensors(R1, R2, cont):
+  DsA = np.random.randint(5, 10, R1)
+  DsB = np.random.randint(5, 10, R2)
+  assert R1 >= cont
+  assert R2 >= cont
+  chargesA = [
+      U1Charge(np.random.randint(-5, 5, DsA[n])) for n in range(R1 - cont)
+  ]
+  commoncharges = [
+      U1Charge(np.random.randint(-5, 5, DsA[n + R1 - cont]))
+      for n in range(cont)
+  ]
+  chargesB = [
+      U1Charge(np.random.randint(-5, 5, DsB[n])) for n in range(R2 - cont)
+  ]
+  #contracted indices
+  indsA = np.random.choice(np.arange(R1), cont, replace=False)
+  indsB = np.random.choice(np.arange(R2), cont, replace=False)
+
+  flowsA = np.full(R1, False, dtype=np.bool)
+  flowsB = np.full(R2, False, dtype=np.bool)
+  flowsB[indsB] = True
+
+  indicesA = [None for _ in range(R1)]
+  indicesB = [None for _ in range(R2)]
+  for n in range(len(indsA)):
+    indicesA[indsA[n]] = Index(commoncharges[n], flowsA[indsA[n]])
+    indicesB[indsB[n]] = Index(commoncharges[n], flowsB[indsB[n]])
+  compA = list(set(np.arange(R1)) - set(indsA))
+  compB = list(set(np.arange(R2)) - set(indsB))
+
+  for n in range(len(compA)):
+    indicesA[compA[n]] = Index(chargesA[n], flowsA[compA[n]])
+  for n in range(len(compB)):
+    indicesB[compB[n]] = Index(chargesB[n], flowsB[compB[n]])
+  indices_final = []
+  for n in sorted(compA):
+    indices_final.append(indicesA[n])
+  for n in sorted(compB):
+    indices_final.append(indicesB[n])
+  shapes = tuple([i.dim for i in indices_final])
+  A = BlockSparseTensor.random(indices=indicesA)
+  B = BlockSparseTensor.random(indices=indicesB)
+  return A, B, indsA, indsB
+
+
 @pytest.mark.parametrize("dtype", np_dtypes)
 def test_block_sparse_init(dtype):
   D = 10  #bond dimension
@@ -23,17 +69,16 @@ def test_block_sparse_init(dtype):
       Index(charges=charges[n], flow=flows[n], name='index{}'.format(n))
       for n in range(rank)
   ]
-  num_elements = compute_num_nonzero([i.charges for i in indices],
-                                     [i.flow for i in indices])
+  num_elements = compute_num_nonzero(charges, flows)
   A = BlockSparseTensor.random(indices=indices, dtype=dtype)
   assert A.dtype == dtype
   for r in range(rank):
-    assert A.indices[r].name == 'index{}'.format(r)
+    assert A.indices[r].name[0] == 'index{}'.format(r)
   assert A.dense_shape == tuple([D] * rank)
   assert len(A.data) == num_elements
 
 
-def test_find_dense_positions():
+def test_reduce_charges():
   left_charges = np.asarray([-2, 0, 1, 0, 0]).astype(np.int16)
   right_charges = np.asarray([-1, 0, 2, 1]).astype(np.int16)
   target_charge = np.zeros((1, 1), dtype=np.int16)
@@ -49,7 +94,7 @@ def test_find_dense_positions():
 
 def test_transpose():
   R = 4
-  Ds = [20, 3, 4, 5]
+  Ds = np.random.randint(10, 20, R)
   final_order = np.arange(R)
   np.random.shuffle(final_order)
   charges = [U1Charge(np.random.randint(-5, 5, Ds[n])) for n in range(R)]
@@ -58,253 +103,94 @@ def test_transpose():
   A = BlockSparseTensor.random(indices=indices)
   Adense = A.todense()
   dense_res = np.transpose(Adense, final_order)
-  A.transpose(final_order)
-  np.testing.assert_allclose(dense_res, A.todense())
+  B = A.transpose(final_order)
+  np.testing.assert_allclose(dense_res, B.todense())
 
 
-def test_tensordot():
+def test_reshape():
   R = 4
-  DsA = [10, 12, 14, 16]
-  DsB = [14, 16, 18, 20]
-  chargesA = [U1Charge(np.random.randint(-5, 5, DsA[n])) for n in range(R // 2)]
-  commoncharges = [
-      U1Charge(np.random.randint(-5, 5, DsA[n + R // 2])) for n in range(R // 2)
-  ]
-  chargesB = [
-      U1Charge(np.random.randint(-5, 5, DsB[n + R // 2])) for n in range(R // 2)
-  ]
-  indsA = np.random.choice(np.arange(R), R // 2, replace=False)
-  indsB = np.random.choice(np.arange(R), R // 2, replace=False)
-  flowsA = np.full(R, False, dtype=np.bool)
-  flowsB = np.full(R, False, dtype=np.bool)
-
-  flowsB[indsB] = True
-  indicesA = [None for _ in range(R)]
-  indicesB = [None for _ in range(R)]
-  for n in range(len(indsA)):
-    indicesA[indsA[n]] = Index(commoncharges[n], flowsA[indsA[n]])
-    indicesB[indsB[n]] = Index(commoncharges[n], flowsB[indsB[n]])
-  compA = list(set(np.arange(R)) - set(indsA))
-  compB = list(set(np.arange(R)) - set(indsB))
-
-  for n in range(len(compA)):
-    indicesA[compA[n]] = Index(chargesA[n], flowsA[compA[n]])
-    indicesB[compB[n]] = Index(chargesB[n], flowsB[compB[n]])
-  indices_final = []
-  for n in sorted(compA):
-    indices_final.append(indicesA[n])
-  for n in sorted(compB):
-    indices_final.append(indicesB[n])
-  shapes = tuple([i.dimension for i in indices_final])
-  A = BlockSparseTensor.random(indices=indicesA)
-  B = BlockSparseTensor.random(indices=indicesB)
-
-  final_order = np.arange(R)
-  np.random.shuffle(final_order)
+  Ds = [3, 4, 5, 6]
+  charges = [U1Charge(np.random.randint(-5, 5, Ds[n])) for n in range(R)]
+  flows = np.full(R, fill_value=False, dtype=np.bool)
+  indices = [Index(charges[n], flows[n]) for n in range(R)]
+  A = BlockSparseTensor.random(indices=indices)
+  B = A.reshape([Ds[0] * Ds[1], Ds[2], Ds[3]])
   Adense = A.todense()
-  Bdense = B.todense()
-  dense_res = np.transpose(
-      np.tensordot(Adense, Bdense, (indsA, indsB)), final_order)
+  Bdense = Adense.reshape([Ds[0] * Ds[1], Ds[2], Ds[3]])
+  np.testing.assert_allclose(Bdense, B.todense())
 
-  res = tensordot(A, B, (indsA, indsB), final_order=final_order)
+
+def test_reshape_transpose():
+  R = 4
+  Ds = [3, 4, 5, 6]
+  charges = [U1Charge(np.random.randint(-5, 5, Ds[n])) for n in range(R)]
+  flows = np.full(R, fill_value=False, dtype=np.bool)
+  indices = [Index(charges[n], flows[n]) for n in range(R)]
+  A = BlockSparseTensor.random(indices=indices)
+  B = A.reshape([Ds[0] * Ds[1], Ds[2], Ds[3]]).transpose([2, 0, 1])
+  dense = A.todense().reshape([Ds[0] * Ds[1], Ds[2],
+                               Ds[3]]).transpose([2, 0, 1])
+  np.testing.assert_allclose(dense, B.todense())
+
+
+@pytest.mark.parametrize("R1, R2, cont", [(4, 4, 2), (4, 3, 3), (3, 4, 3)])
+def test_tensordot(R1, R2, cont):
+  A, B, indsA, indsB = get_contractable_tensors(R1, R2, cont)
+  res = tensordot(A, B, (indsA, indsB))
+  dense_res = np.tensordot(A.todense(), B.todense(), (indsA, indsB))
   np.testing.assert_allclose(dense_res, res.todense())
 
 
-# def test_find_dense_positions_2():
-#   D = 40  #bond dimension
-#   B = 4  #number of blocks
-#   dtype = np.int16  #the dtype of the quantum numbers
-#   rank = 4
-#   flows = np.asarray([1 for _ in range(rank)])
-#   flows[-2::] = -1
-#   charges = [
-#       np.random.randint(-B // 2, B // 2 + 1, D).astype(dtype)
-#       for _ in range(rank)
-#   ]
-#   indices = [
-#       Index(
-#           charges=U1Charge(charges[n]), flow=flows[n], name='index{}'.format(n))
-#       for n in range(rank)
-#   ]
-#   n1 = compute_num_nonzero([i.charges for i in indices],
-#                            [i.flow for i in indices])
+def test_tensordot_reshape():
+  R1 = 4
+  R2 = 4
 
-#   i01 = indices[0] * indices[1]
-#   i23 = indices[2] * indices[3]
-#   positions = find_dense_positions([i01.charges, i23.charges], [1, 1],
-#                                    U1Charge(np.asarray([0])))
-#   assert len(positions[0]) == n1
+  q = np.random.randint(-5, 5, 10, dtype=np.int16)
+  charges1 = [U1Charge(q) for n in range(R1)]
+  charges2 = [U1Charge(q) for n in range(R2)]
+  flowsA = np.asarray([False] * R1)
+  flowsB = np.asarray([True] * R2)
+  A = BlockSparseTensor.random(indices=[
+      Index(charges1[n], flowsA[n], name='a{}'.format(n)) for n in range(R1)
+  ])
+  B = BlockSparseTensor.random(indices=[
+      Index(charges2[n], flowsB[n], name='b{}'.format(n)) for n in range(R2)
+  ])
 
-# def test_find_sparse_positions():
-#   D = 40  #bond dimension
-#   B = 4  #number of blocks
-#   dtype = np.int16  #the dtype of the quantum numbers
-#   rank = 4
-#   flows = np.asarray([1 for _ in range(rank)])
-#   flows[-2::] = -1
-#   charges = [
-#       np.random.randint(-B // 2, B // 2 + 1, D).astype(dtype)
-#       for _ in range(rank)
-#   ]
-#   indices = [
-#       Index(
-#           charges=U1Charge(charges[n]), flow=flows[n], name='index{}'.format(n))
-#       for n in range(rank)
-#   ]
-#   n1 = compute_num_nonzero([i.charges for i in indices],
-#                            [i.flow for i in indices])
-#   i01 = indices[0] * indices[1]
-#   i23 = indices[2] * indices[3]
-#   unique_row_charges = np.unique(i01.charges.charges)
-#   unique_column_charges = np.unique(i23.charges.charges)
-#   common_charges = np.intersect1d(
-#       unique_row_charges, -unique_column_charges, assume_unique=True)
-#   blocks = find_sparse_positions([i01.charges, i23.charges], [1, 1],
-#                                  target_charges=U1Charge(np.asarray([0])))
-#   assert sum([len(v) for v in blocks.values()]) == n1
-#   np.testing.assert_allclose(np.sort(blocks[0]), np.arange(n1))
+  Adense = A.todense().reshape((10, 10 * 10, 10))
+  Bdense = B.todense().reshape((10 * 10, 10, 10))
 
-# def test_find_sparse_positions_2():
-#   D = 1000  #bond dimension
-#   B = 4  #number of blocks
-#   dtype = np.int16  #the dtype of the quantum numbers
-#   charges = np.random.randint(-B // 2, B // 2 + 1, D).astype(dtype)
-#   index = Index(charges=U1Charge(charges), flow=1, name='index0')
-#   targets = np.asarray([-1, 0, 1])
-#   blocks = find_sparse_positions([index.charges], [index.flow],
-#                                  target_charges=U1Charge(targets))
+  A = A.reshape((10, 10 * 10, 10))
+  B = B.reshape((10 * 10, 10, 10))
 
-#   inds = np.isin(charges, targets)
-#   relevant_charges = charges[inds]
-#   blocks_ = {t: np.nonzero(relevant_charges == t)[0] for t in targets}
-#   assert np.all(
-#       np.asarray(list(blocks.keys())) == np.asarray(list(blocks_.keys())))
-#   for k in blocks.keys():
-#     assert np.all(blocks[k] == blocks_[k])
+  res = tensordot(A, B, ([0, 1], [2, 0]))
+  dense = np.tensordot(Adense, Bdense, ([0, 1], [2, 0]))
+  np.testing.assert_allclose(dense, res.todense())
 
-# def test_find_sparse_positions_3():
-#   D = 40  #bond dimension
-#   B = 4  #number of blocks
-#   dtype = np.int16  #the dtype of the quantum numbers
-#   flows = [1, -1]
 
-#   rank = len(flows)
-#   charges = [
-#       np.random.randint(-B // 2, B // 2 + 1, D).astype(dtype)
-#       for _ in range(rank)
-#   ]
-#   indices = [
-#       Index(
-#           charges=U1Charge(charges[n]), flow=flows[n], name='index{}'.format(n))
-#       for n in range(rank)
-#   ]
-#   i1, i2 = indices
-#   common_charges = np.intersect1d(i1.charges.charges, i2.charges.charges)
-#   row_locations = find_sparse_positions(
-#       charges=[i1.charges, i2.charges],
-#       flows=flows,
-#       target_charges=U1Charge(common_charges))
-#   fused = (i1 * i2).charges
-#   relevant = fused.charges[np.isin(fused.charges, common_charges)]
-#   for k, v in row_locations.items():
-#     np.testing.assert_allclose(np.nonzero(relevant == k)[0], np.sort(v))
+@pytest.mark.parametrize("R1, R2, cont", [(4, 4, 2), (4, 3, 3), (3, 4, 3)])
+def test_tensordot_final_order(R1, R2, cont):
+  A, B, indsA, indsB = get_contractable_tensors(R1, R2, cont)
+  final_order = np.arange(R1 + R2 - 2 * cont)
+  np.random.shuffle(final_order)
+  res = tensordot(A, B, (indsA, indsB), final_order=final_order)
+  dense_res = np.transpose(
+      np.tensordot(A.todense(), B.todense(), (indsA, indsB)), final_order)
+  np.testing.assert_allclose(dense_res, res.todense())
 
-# # def test_dense_transpose():
-# #   Ds = [10, 11, 12]  #bond dimension
-# #   rank = len(Ds)
-# #   flows = np.asarray([1 for _ in range(rank)])
-# #   flows[-2::] = -1
-# #   charges = [U1Charge(np.zeros(Ds[n], dtype=np.int16)) for n in range(rank)]
-# #   indices = [
-# #       Index(charges=charges[n], flow=flows[n], name='index{}'.format(n))
-# #       for n in range(rank)
-# #   ]
-# #   A = BlockSparseTensor.random(indices=indices, dtype=np.float64)
-# #   B = np.transpose(np.reshape(A.data.copy(), Ds), (1, 0, 2))
-# #   A.transpose((1, 0, 2))
-# #   np.testing.assert_allclose(A.data, B.flat)
 
-# #   B = np.transpose(np.reshape(A.data.copy(), [11, 10, 12]), (1, 0, 2))
-# #   A.transpose((1, 0, 2))
+@pytest.mark.parametrize("R1, R2", [(2, 2), (3, 3), (4, 4), (1, 1)])
+def test_tensordot_inner(R1, R2):
 
-# #   np.testing.assert_allclose(A.data, B.flat)
+  A, B, indsA, indsB = get_contractable_tensors(R1, R2, 0)
+  res = tensordot(A, B, (indsA, indsB))
+  dense_res = np.tensordot(A.todense(), B.todense(), (indsA, indsB))
+  np.testing.assert_allclose(dense_res, res.todense())
 
-# @pytest.mark.parametrize("R", [1, 2])
-# def test_find_diagonal_dense_blocks(R):
-#   rs = [U1Charge(np.random.randint(-4, 4, 50)) for _ in range(R)]
-#   cs = [U1Charge(np.random.randint(-4, 4, 50)) for _ in range(R)]
-#   charges = rs + cs
 
-#   left_fused = fuse_charges(charges[0:R], [1] * R)
-#   right_fused = fuse_charges(charges[R:], [1] * R)
-#   left_unique = left_fused.unique()
-#   right_unique = right_fused.unique()
-#   zero = left_unique.zero_charge
-#   blocks = {}
-#   rdim = len(right_fused)
-#   for lu in left_unique:
-#     linds = np.nonzero(left_fused == lu)[0]
-#     rinds = np.nonzero(right_fused == lu * (-1))[0]
-#     if (len(linds) > 0) and (len(rinds) > 0):
-#       blocks[lu] = fuse_ndarrays([linds * rdim, rinds])
-#   comm, blocks_ = _find_diagonal_dense_blocks(rs, cs, [1] * R, [1] * R)
-#   for n in range(len(comm)):
-#     assert np.all(blocks[comm.charges[n]] == blocks_[n][0])
-
-# # #@pytest.mark.parametrize("dtype", np_dtypes)
-# # def test_find_diagonal_dense_blocks_2():
-# #   R = 1
-# #   rs = [U1Charge(np.random.randint(-4, 4, 50)) for _ in range(R)]
-# #   cs = [U1Charge(np.random.randint(-4, 4, 50)) for _ in range(R)]
-# #   charges = rs + cs
-
-# #   left_fused = fuse_charges(charges[0:R], [1] * R)
-# #   right_fused = fuse_charges(charges[R:], [1] * R)
-# #   left_unique = left_fused.unique()
-# #   right_unique = right_fused.unique()
-# #   zero = left_unique.zero_charge
-# #   blocks = {}
-# #   rdim = len(right_fused)
-# #   for lu in left_unique:
-# #     linds = np.nonzero(left_fused == lu)[0]
-# #     rinds = np.nonzero(right_fused == lu * (-1))[0]
-# #     if (len(linds) > 0) and (len(rinds) > 0):
-# #       blocks[lu] = fuse_ndarrays([linds * rdim, rinds])
-# #   comm, blocks_ = _find_diagonal_dense_blocks(rs, cs, [1] * R, [1] * R)
-# #   for n in range(len(comm)):
-# #     assert np.all(blocks[comm.charges[n]] == blocks_[n][0])
-
-# @pytest.mark.parametrize("R", [1, 2])
-# def test_find_diagonal_dense_blocks_transposed(R):
-#   order = np.arange(2 * R)
-#   np.random.shuffle(order)
-#   rs = [U1Charge(np.random.randint(-4, 4, 50)) for _ in range(R)]
-#   cs = [U1Charge(np.random.randint(-4, 4, 40)) for _ in range(R)]
-#   charges = rs + cs
-#   dims = np.asarray([len(c) for c in charges])
-#   strides = np.flip(np.append(1, np.cumprod(np.flip(dims[1::]))))
-#   stride_arrays = [np.arange(dims[n]) * strides[n] for n in range(2 * R)]
-
-#   left_fused = fuse_charges([charges[n] for n in order[0:R]], [1] * R)
-#   right_fused = fuse_charges([charges[n] for n in order[R:]], [1] * R)
-#   lstrides = fuse_ndarrays([stride_arrays[n] for n in order[0:R]])
-#   rstrides = fuse_ndarrays([stride_arrays[n] for n in order[R:]])
-
-#   left_unique = left_fused.unique()
-#   right_unique = right_fused.unique()
-#   blocks = {}
-#   rdim = len(right_fused)
-#   for lu in left_unique:
-#     linds = np.nonzero(left_fused == lu)[0]
-#     rinds = np.nonzero(right_fused == lu * (-1))[0]
-#     if (len(linds) > 0) and (len(rinds) > 0):
-#       tmp = fuse_ndarrays([linds * rdim, rinds])
-#       blocks[lu] = _find_values_in_fused(tmp, lstrides, rstrides)
-
-#   comm, blocks_ = _find_diagonal_dense_blocks([charges[n] for n in order[0:R]],
-#                                               [charges[n] for n in order[R:]],
-#                                               [1] * R, [1] * R,
-#                                               row_strides=strides[order[0:R]],
-#                                               column_strides=strides[order[R:]])
-#   for n in range(len(comm)):
-#     assert np.all(blocks[comm.charges[n]] == blocks_[n][0])
+@pytest.mark.parametrize("R1, R2", [(2, 2), (2, 1), (1, 2), (1, 1)])
+def test_tensordot_outer(R1, R2):
+  A, B, indsA, indsB = get_contractable_tensors(R1, R2, 0)
+  res = tensordot(A, B, axes=0)
+  dense_res = np.tensordot(A.todense(), B.todense(), axes=0)
+  np.testing.assert_allclose(dense_res, res.todense())
