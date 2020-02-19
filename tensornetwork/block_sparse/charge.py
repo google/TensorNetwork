b7292ff4fcde3ff8c878f3da7e1b1773b7b00630
mganahl <martin.ganahl@gmail.com>
2020-02-19 10:57:06 -0500

    adding more to charge.py

--- tensornetwork/block_sparse/charge.py
+++ tensornetwork/block_sparse/charge.py
@@ -18,7 +18,7 @@ from __future__ import print_function
 import numpy as np
 
 # pylint: disable=line-too-long
-from typing import List, Optional, Type
+from typing import List, Optional, Type, Any, Union
 
 
 class BaseCharge:
@@ -114,3 +114,272 @@ class BaseCharge:
 
   def __len__(self):
     return len(self.charge_labels)
+
+  def __eq__(self,
+             target_charges: Union[np.ndarray, "BaseCharge"]) -> np.ndarray:
+
+    if isinstance(target_charges, type(self)):
+      targets = np.unique(
+          target_charges.unique_charges[:, target_charges.charge_labels],
+          axis=1)
+    else:
+      if target_charges.ndim == 1:
+        target_charges = np.expand_dims(target_charges, 0)
+      targets = np.unique(target_charges, axis=1)
+    #pylint: disable=no-member
+    inds = np.nonzero(
+        np.logical_and.reduce(
+            np.expand_dims(self.unique_charges,
+                           2) == np.expand_dims(targets, 1),
+            axis=0))[0]
+    return np.expand_dims(self.charge_labels, 1) == np.expand_dims(inds, 0)
+
+  @property
+  def identity_charges(self) -> "BaseCharge":
+    """
+    Returns the identity charge.
+    Returns:
+      BaseCharge: The identity charge.
+    """
+    unique_charges = np.expand_dims(
+        np.asarray([ct.identity_charge() for ct in self.charge_types],
+                   dtype=np.int16), 1)
+    charge_labels = np.zeros(1, dtype=np.int16)
+    obj = self.__new__(type(self))
+    obj.__init__(unique_charges, charge_labels, self.charge_types)
+    return obj
+
+  def __add__(self, other: "BaseCharge") -> "BaseCharge":
+    """
+    Fuse `self` with `other`.
+    Args:
+      other: A `BaseCharge` object.
+    Returns:
+      BaseCharge: The result of fusing `self` with `other`.
+    """
+
+    # fuse the unique charges from each index, then compute new unique charges
+    comb_charges = fuse_ndarray_charges(self.unique_charges,
+                                        other.unique_charges, self.charge_types)
+    [unique_charges, charge_labels] = np.unique(
+        comb_charges, return_inverse=True, axis=1)
+    charge_labels = charge_labels.reshape(self.unique_charges.shape[1],
+                                          other.unique_charges.shape[1]).astype(
+                                              np.int16)
+
+    # find new labels using broadcasting
+    charge_labels = charge_labels[(
+        self.charge_labels[:, None] +
+        np.zeros([1, len(other)], dtype=np.int16)).ravel(), (
+            other.charge_labels[None, :] +
+            np.zeros([len(self), 1], dtype=np.int16)).ravel()]
+
+    obj = self.__new__(type(self))
+    obj.__init__(unique_charges, charge_labels, self.charge_types)
+
+    return obj
+
+  def dual(self, take_dual: Optional[bool] = False) -> "BaseCharge":
+    """
+    Return the charges of `BaseCharge`, possibly conjugated.
+    Args:
+      take_dual: If `True` return the dual charges. If `False` return 
+        regular charges.
+    Returns:
+      BaseCharge
+    """
+    if take_dual:
+      unique_dual_charges = np.stack([
+          self.charge_types[n].dual_charges(self.unique_charges[n, :])
+          for n in range(len(self.charge_types))
+      ],
+                                     axis=0)
+
+      obj = self.__new__(type(self))
+      obj.__init__(unique_dual_charges, self.charge_labels, self.charge_types)
+      return obj
+    return self
+
+  def __matmul__(self, other):
+    #some checks
+    if len(self) != len(other):
+      raise ValueError(
+          '__matmul__ requires charges to have the same number of elements')
+    charges = np.concatenate([self.charges, other.charges], axis=0)
+    charge_types = self.charge_types + other.charge_types
+    return BaseCharge(
+        charges=charges, charge_labels=None, charge_types=charge_types)
+
+  def __mul__(self, number: bool) -> "BaseCharge":
+    if not isinstance(number, (bool, np.bool_)):
+      raise ValueError(
+          "can only multiply by `True` or `False`, found {}".format(number))
+    return self.dual(number)
+
+
+class U1Charge(BaseCharge):
+
+  def __init__(self,
+               charges: np.ndarray,
+               charge_labels: Optional[np.ndarray] = None,
+               charge_types: Optional[List[Type["BaseCharge"]]] = None) -> None:
+    super().__init__(charges, charge_labels, charge_types=[type(self)])
+
+  @staticmethod
+  def fuse(charge1, charge2) -> np.ndarray:
+    return np.add.outer(charge1, charge2).ravel()
+
+  @staticmethod
+  def dual_charges(charges) -> np.ndarray:
+    return charges * charges.dtype.type(-1)
+
+  @staticmethod
+  def identity_charge() -> np.ndarray:
+    return np.int16(0)
+
+  @classmethod
+  def random(cls, minval: int, maxval: int, dimension: int) -> np.ndarray:
+    charges = np.random.randint(minval, maxval, dimension, dtype=np.int16)
+    return cls(charges=charges)
+
+
+def fuse_ndarray_charges(charges_A: np.ndarray, charges_B: np.ndarray,
+                         charge_types: List[Type[BaseCharge]]) -> np.ndarray:
+  """
+  Fuse the quantum numbers of two indices under their kronecker addition.
+  Args:
+    charges_A (np.ndarray): n-by-D1 dimensional array integers encoding charges,
+      with n the number of symmetries and D1 the index dimension.
+    charges__B (np.ndarray): n-by-D2 dimensional array of charges.
+    charge_types: A list of types of the charges.
+  Returns:
+    np.ndarray: n-by-(D1 * D2) dimensional array of the fused charges.
+  """
+  comb_charges = [0] * len(charge_types)
+  for n, ct in enumerate(charge_types):
+    comb_charges[n] = ct.fuse(charges_A[n, :], charges_B[n, :])
+
+  return np.concatenate(
+      comb_charges, axis=0).reshape(
+          len(charge_types), charges_A.shape[1] * charges_B.shape[1])
+
+
+def intersect(A: np.ndarray,
+              B: np.ndarray,
+              axis=0,
+              assume_unique=False,
+              return_indices=False) -> Any:
+  """
+  Extends numpy's intersect1d to find the row or column-wise intersection of
+  two 2d arrays. Takes identical input to numpy intersect1d.
+  Args:
+    A, B (np.ndarray): arrays of matching widths and datatypes
+  Returns:
+    ndarray: sorted 1D array of common rows/cols between the input arrays
+    ndarray: the indices of the first occurrences of the common values in A.
+      Only provided if return_indices is True.
+    ndarray: the indices of the first occurrences of the common values in B.
+      Only provided if return_indices is True.
+  """
+  #see https://stackoverflow.com/questions/8317022/get-intersecting-rows-across-two-2d-numpy-arrays
+  #pylint: disable=no-else-return
+  if A.ndim == 1:
+    return np.intersect1d(
+        A, B, assume_unique=assume_unique, return_indices=return_indices)
+
+  elif A.ndim == 2:
+    if axis == 0:
+      ncols = A.shape[1]
+      if A.shape[1] != B.shape[1]:
+        raise ValueError("array widths must match to intersect")
+
+      dtype = {
+          'names': ['f{}'.format(i) for i in range(ncols)],
+          'formats': ncols * [A.dtype]
+      }
+      if return_indices:
+        C, A_locs, B_locs = np.intersect1d(
+            A.view(dtype),
+            B.view(dtype),
+            assume_unique=assume_unique,
+            return_indices=return_indices)
+        return C.view(A.dtype).reshape(-1, ncols), A_locs, B_locs
+      C = np.intersect1d(
+          A.view(dtype), B.view(dtype), assume_unique=assume_unique)
+      return C.view(A.dtype).reshape(-1, ncols)
+
+    elif axis == 1:
+      #@Glen: why the copy here?
+      out = intersect(
+          A.T.copy(),
+          B.T.copy(),
+          axis=0,
+          assume_unique=assume_unique,
+          return_indices=return_indices)
+      if return_indices:
+        return out[0].T, out[1], out[2]
+      return out.T
+
+    raise NotImplementedError(
+        "intersection can only be performed on first or second axis")
+
+  raise NotImplementedError("intersect is only implemented for 1d or 2d arrays")
+
+
+def fuse_charges(charges: List[BaseCharge], flows: List[bool]) -> BaseCharge:
+  """
+  Fuse all `charges` into a new charge.
+  Charges are fused from "right to left",
+  in accordance with row-major order.
+
+  Args:
+    charges: A list of charges to be fused.
+    flows: A list of flows, one for each element in `charges`.
+  Returns:
+    BaseCharge: The result of fusing `charges`.
+  """
+  if len(charges) != len(flows):
+    raise ValueError(
+        "`charges` and `flows` are of unequal lengths {} != {}".format(
+            len(charges), len(flows)))
+  fused_charges = charges[0] * flows[0]
+  for n in range(1, len(charges)):
+    fused_charges = fused_charges + charges[n] * flows[n]
+  return fused_charges
+
+
+def fuse_degeneracies(degen1: Union[List, np.ndarray],
+                      degen2: Union[List, np.ndarray]) -> np.ndarray:
+  """
+  Fuse degeneracies `degen1` and `degen2` of two leg-charges
+  by simple kronecker product. `degen1` and `degen2` typically belong to two
+  consecutive legs of `BlockSparseTensor`.
+  Given `degen1 = [1, 2, 3]` and `degen2 = [10, 100]`, this returns
+  `[10, 100, 20, 200, 30, 300]`.
+  When using row-major ordering of indices in `BlockSparseTensor`,
+  the position of `degen1` should be "to the left" of the position of `degen2`.
+  Args:
+    degen1: Iterable of integers
+    degen2: Iterable of integers
+  Returns:
+    np.ndarray: The result of fusing `dege1` with `degen2`.
+  """
+  return np.reshape(degen1[:, None] * degen2[None, :],
+                    len(degen1) * len(degen2))
+
+
+def fuse_ndarrays(arrays: List[Union[List, np.ndarray]]) -> np.ndarray:
+  """
+  Fuse all `arrays` by simple kronecker addition.
+  Arrays are fused from "right to left", 
+  Args:
+    arrays: A list of arrays to be fused.
+  Returns:
+    np.ndarray: The result of fusing `arrays`.
+  """
+  if len(arrays) == 1:
+    return arrays[0]
+  fused_arrays = arrays[0]
+  for n in range(1, len(arrays)):
+    fused_arrays = np.ravel(np.add.outer(fused_arrays, arrays[n]))
+  return fused_arrays
--- tensornetwork/block_sparse/charge_test.py
+++ tensornetwork/block_sparse/charge_test.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pytest
 # pylint: disable=line-too-long
-from tensornetwork.block_sparse.charge import BaseCharge
+from tensornetwork.block_sparse.charge import BaseCharge, intersect, fuse_ndarrays, U1Charge, fuse_degeneracies
 
 
 def test_BaseCharge_charges():
@@ -42,3 +42,238 @@ def test_BaseCharge_copy():
   assert Q.unique_charges is not Qcopy.unique_charges
   np.testing.assert_allclose(Q.charge_labels, Qcopy.charge_labels)
   np.testing.assert_allclose(Q.unique_charges, Qcopy.unique_charges)
+
+
+def test_intersect_1():
+  a = np.array([[0, 1, 2], [2, 3, 4]])
+  b = np.array([[0, -2, 6], [2, 3, 4]])
+  out = intersect(a, b, axis=1)
+  np.testing.assert_allclose(np.array([[0], [2]]), out)
+
+
+def test_intersect_2():
+  a = np.array([[0, 1, 2], [2, 3, 4]])
+  b = np.array([[0, -2, 6, 2], [2, 3, 4, 4]])
+  out, la, lb = intersect(a, b, axis=1, return_indices=True)
+  np.testing.assert_allclose(np.array([[0, 2], [2, 4]]), out)
+  np.testing.assert_allclose(la, [0, 2])
+  np.testing.assert_allclose(lb, [0, 3])
+
+
+def test_intersect_3():
+  a = np.array([0, 1, 2, 3, 4])
+  b = np.array([0, -1, 4])
+  out = intersect(a, b)
+  np.testing.assert_allclose([0, 4], out)
+
+
+def test_intersect_3():
+  a = np.array([0, 1, 2, 3, 4])
+  b = np.array([0, -1, 4])
+  out, la, lb = intersect(a, b, return_indices=True)
+  np.testing.assert_allclose([0, 4], out)
+  np.testing.assert_allclose(la, [0, 4])
+  np.testing.assert_allclose(lb, [0, 2])
+
+
+def test_fuse_degeneracies():
+  d1 = np.asarray([0, 1])
+  d2 = np.asarray([2, 3, 4])
+  fused_degeneracies = fuse_degeneracies(d1, d2)
+  np.testing.assert_allclose(fused_degeneracies, np.kron(d1, d2))
+
+
+def test_U1Charge_charges():
+  D = 100
+  B = 6
+  charges = np.random.randint(-B // 2, B // 2 + 1, D).astype(np.int16)
+
+  q1 = U1Charge(charges)
+  assert np.all(q1.charges == charges)
+
+
+def test_U1Charge_dual():
+  D = 100
+  B = 6
+  charges = np.random.randint(-B // 2, B // 2 + 1, D).astype(np.int16)
+
+  q1 = U1Charge(charges)
+  assert np.all(q1.dual(True).charges == -charges)
+
+
+def test_U1Charge_fusion():
+
+  def run_test():
+    D = 2000
+    B = 6
+    O1 = np.random.randint(-B // 2, B // 2 + 1, D).astype(np.int16)
+    O2 = np.random.randint(-B // 2, B // 2 + 1, D).astype(np.int16)
+    P1 = np.random.randint(0, B + 1, D).astype(np.int16)
+    P2 = np.random.randint(-B // 2, B // 2 + 1, D).astype(np.int16)
+    Q1 = np.random.randint(1, B + 1, D).astype(np.int16)
+    Q2 = np.random.randint(1, B + 1, D).astype(np.int16)
+
+    charges_1 = [O1, O2]
+    charges_2 = [P1, P2]
+    charges_3 = [Q1, Q2]
+
+    fused_1 = fuse_ndarrays(charges_1)
+    fused_2 = fuse_ndarrays(charges_2)
+    fused_3 = fuse_ndarrays(charges_3)
+    q1 = U1Charge(O1) @ U1Charge(P1) @ U1Charge(Q1)
+    q2 = U1Charge(O2) @ U1Charge(P2) @ U1Charge(Q2)
+
+    target = BaseCharge(
+        charges=np.random.randint(-B, B, (3, 1), dtype=np.int16),
+        charge_labels=None,
+        charge_types=[U1Charge, U1Charge, U1Charge])
+    q12 = q1 + q2
+
+    nz_1 = np.nonzero(q12 == target)[0]
+    i1 = fused_1 == target.charges[0, 0]
+    i2 = fused_2 == target.charges[1, 0]
+    i3 = fused_3 == target.charges[2, 0]
+    #pylint: disable=no-member
+    nz_2 = np.nonzero(np.logical_and.reduce([i1, i2, i3]))[0]
+    return nz_1, nz_2
+
+  nz_1, nz_2 = run_test()
+  while len(nz_1) == 0:
+    nz_1, nz_2 = run_test()
+    break
+  assert np.all(nz_1 == nz_2)
+
+
+def test_U1Charge_multiple_fusion():
+
+  def run_test():
+    D = 300
+    B = 4
+    O1 = np.random.randint(-B // 2, B // 2 + 1, D).astype(np.int16)
+    O2 = np.random.randint(-B // 2, B // 2 + 1, D).astype(np.int16)
+    O3 = np.random.randint(-B // 2, B // 2 + 1, D).astype(np.int16)
+    P1 = np.random.randint(0, B + 1, D).astype(np.int16)
+    P2 = np.random.randint(-B // 2, B // 2 + 1, D).astype(np.int16)
+    P3 = np.random.randint(-B // 2, B // 2 + 1, D).astype(np.int16)
+    Q1 = np.random.randint(1, B + 1, D).astype(np.int16)
+    Q2 = np.random.randint(0, B + 1, D).astype(np.int16)
+    Q3 = np.random.randint(-B // 2, B // 2 + 1, D).astype(np.int16)
+
+    charges_1 = [O1, O2, O3]
+    charges_2 = [P1, P2, P3]
+    charges_3 = [Q1, Q2, Q3]
+
+    fused_1 = fuse_ndarrays(charges_1)
+    fused_2 = fuse_ndarrays(charges_2)
+    fused_3 = fuse_ndarrays(charges_3)
+    q1 = U1Charge(O1) @ U1Charge(P1) @ U1Charge(Q1)
+    q2 = U1Charge(O2) @ U1Charge(P2) @ U1Charge(Q2)
+    q3 = U1Charge(O3) @ U1Charge(P3) @ U1Charge(Q3)
+
+    target = BaseCharge(
+        charges=np.random.randint(-B, B, (3, 1), dtype=np.int16),
+        charge_labels=None,
+        charge_types=[U1Charge, U1Charge, U1Charge])
+
+    q123 = q1 + q2 + q3
+
+    nz_1 = np.nonzero(q123 == target)[0]
+    i1 = fused_1 == target.charges[0, 0]
+    i2 = fused_2 == target.charges[1, 0]
+    i3 = fused_3 == target.charges[2, 0]
+    #pylint: disable=no-member
+    nz_2 = np.nonzero(np.logical_and.reduce([i1, i2, i3]))[0]
+    return nz_1, nz_2
+
+  nz_1, nz_2 = run_test()
+  while len(nz_1) == 0:
+    nz_1, nz_2 = run_test()
+  assert np.all(nz_1 == nz_2)
+
+
+def test_U1Charge_multiple_fusion_with_flow():
+
+  def run_test():
+    D = 300
+    B = 4
+    O1 = np.random.randint(-B // 2, B // 2 + 1, D).astype(np.int8)
+    O2 = np.random.randint(-B // 2, B // 2 + 1, D).astype(np.int8)
+    O3 = np.random.randint(-B // 2, B // 2 + 1, D).astype(np.int8)
+    P1 = np.random.randint(0, B + 1, D).astype(np.int16)
+    P2 = np.random.randint(-B // 2, B // 2 + 1, D).astype(np.int16)
+    P3 = np.random.randint(-B // 2, B // 2 + 1, D).astype(np.int16)
+    Q1 = np.random.randint(1, B + 1, D).astype(np.int8)
+    Q2 = np.random.randint(0, B + 1, D).astype(np.int8)
+    Q3 = np.random.randint(-B // 2, B // 2 + 1, D).astype(np.int8)
+
+    charges_1 = [O1, -O2, O3]
+    charges_2 = [P1, -P2, P3]
+    charges_3 = [Q1, -Q2, Q3]
+
+    fused_1 = fuse_ndarrays(charges_1)
+    fused_2 = fuse_ndarrays(charges_2)
+    fused_3 = fuse_ndarrays(charges_3)
+    q1 = U1Charge(O1) @ U1Charge(P1) @ U1Charge(Q1)
+    q2 = U1Charge(O2) @ U1Charge(P2) @ U1Charge(Q2)
+    q3 = U1Charge(O3) @ U1Charge(P3) @ U1Charge(Q3)
+
+    target = BaseCharge(
+        charges=np.random.randint(-B, B, (3, 1), dtype=np.int16),
+        charge_labels=None,
+        charge_types=[U1Charge, U1Charge, U1Charge])
+    q123 = q1 + q2 * True + q3
+    nz_1 = np.nonzero(q123 == target)[0]
+    i1 = fused_1 == target.charges[0, 0]
+    i2 = fused_2 == target.charges[1, 0]
+    i3 = fused_3 == target.charges[2, 0]
+    #pylint: disable=no-member
+    nz_2 = np.nonzero(np.logical_and.reduce([i1, i2, i3]))[0]
+    return nz_1, nz_2
+
+  nz_1, nz_2 = run_test()
+  while len(nz_1) == 0:
+    nz_1, nz_2 = run_test()
+  assert np.all(nz_1 == nz_2)
+
+
+def test_U1Charge_fusion_with_flow():
+
+  def run_test():
+    D = 2000
+    B = 6
+    O1 = np.random.randint(-B // 2, B // 2 + 1, D).astype(np.int8)
+    O2 = np.random.randint(-B // 2, B // 2 + 1, D).astype(np.int8)
+    P1 = np.random.randint(0, B + 1, D).astype(np.int16)
+    P2 = np.random.randint(-B // 2, B // 2 + 1, D).astype(np.int16)
+    Q1 = np.random.randint(1, B + 1, D).astype(np.int8)
+    Q2 = np.random.randint(1, B + 1, D).astype(np.int8)
+
+    charges_1 = [O1, -O2]
+    charges_2 = [P1, -P2]
+    charges_3 = [Q1, -Q2]
+
+    fused_1 = fuse_ndarrays(charges_1)
+    fused_2 = fuse_ndarrays(charges_2)
+    fused_3 = fuse_ndarrays(charges_3)
+
+    q1 = U1Charge(O1) @ U1Charge(P1) @ U1Charge(Q1)
+    q2 = U1Charge(O2) @ U1Charge(P2) @ U1Charge(Q2)
+
+    target = BaseCharge(
+        charges=np.random.randint(-B, B, (3, 1), dtype=np.int16),
+        charge_labels=None,
+        charge_types=[U1Charge, U1Charge, U1Charge])
+    q12 = q1 + q2 * True
+
+    nz_1 = np.nonzero(q12 == target)[0]
+    i1 = fused_1 == target.charges[0, 0]
+    i2 = fused_2 == target.charges[1, 0]
+    i3 = fused_3 == target.charges[2, 0]
+    #pylint: disable=no-member
+    nz_2 = np.nonzero(np.logical_and.reduce([i1, i2, i3]))[0]
+    return nz_1, nz_2
+
+  nz_1, nz_2 = run_test()
+  while len(nz_1) == 0:
+    nz_1, nz_2 = run_test()
+  assert np.all(nz_1 == nz_2)
