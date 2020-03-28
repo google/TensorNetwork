a06f6f881019ba29bb6afa35dd808ebe926593f1
mganahl <martin.ganahl@gmail.com>
2020-03-27 21:14:55 -0400

    fix imports

--- tensornetwork/backends/symmetric/symmetric_backend_test.py
+++ tensornetwork/backends/symmetric/symmetric_backend_test.py
@@ -6,7 +6,7 @@ from tensornetwork.backends.symmetric import symmetric_backend
 from tensornetwork.block_sparse.charge import U1Charge, charge_equal, BaseCharge
 from tensornetwork.block_sparse.index import Index
 # pylint: disable=line-too-long
-from tensornetwork.block_sparse.block_tensor import tensordot, BlockSparseTensor, transpose, sqrt, ChargeArray, diag, trace, norm, eye, ones, zeros, randn, random, eigh, inv
+from tensornetwork.block_sparse import tensordot, BlockSparseTensor, transpose, sqrt, ChargeArray, diag, trace, norm, eye, ones, zeros, randn, random, eigh, inv
 
 np_randn_dtypes = [np.float32, np.float16, np.float64]
 np_dtypes = np_randn_dtypes + [np.complex64, np.complex128]
