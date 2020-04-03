064f85a6cfa13934fc67eac902b2b09016dfaaa1
mganahl <martin.ganahl@gmail.com>
2020-04-03 10:40:21 -0400

    added test

--- tensornetwork/backends/symmetric/symmetric_backend_test.py
+++ tensornetwork/backends/symmetric/symmetric_backend_test.py
@@ -781,3 +781,22 @@ def test_broadcast_left_multiplication_raises():
   tensor2 = backend.randn(indices)
   with pytest.raises(ValueError):
     backend.broadcast_left_multiplication(tensor1, tensor2)
+
+
+@pytest.mark.parametrize("dtype", [np.float64, np.complex128])
+@pytest.mark.parametrize("num_charges", [1, 2])
+def test_sparse_shape(dtype, num_charges):
+  np.random.seed(10)
+  Ds = [11, 12, 13]
+  R = len(Ds)
+  charges = [
+      BaseCharge(
+          np.random.randint(-5, 6, (num_charges, Ds[n])),
+          charge_types=[U1Charge] * num_charges) for n in range(R)
+  ]
+  flows = list(np.full(R, fill_value=False, dtype=np.bool))
+  indices = [Index(charges[n], flows[n]) for n in range(R)]
+  a = BlockSparseTensor.random(indices=indices, dtype=dtype)
+  backend = symmetric_backend.SymmetricBackend()
+  for s1, s2 in zip(a.sparse_shape, backend.sparse_shape(a)):
+    assert s1 == s2
