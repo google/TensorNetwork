d4515642fa56787961e2ba12f88a1815ece36d33
mganahl <martin.ganahl@gmail.com>
2020-05-01 12:11:33 -0400

    added test for backend factory

---
+++ tensornetwork/tests/backend_factory_test.py
@@ -0,0 +1,21 @@
+# Copyright 2019 The TensorNetwork Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from tensornetwork.backends.backend_factory import get_backend
+
+
+def test_backend_instantiation(backend):
+  backend1 = get_backend(backend)
+  backend2 = get_backend(backend)
+  assert backend1 is backend2
