4752c398e0cb47df7c474f2f2b3557031016baae
Chase Roberts <chaseriley@google.com>
2020-05-01 06:25:39 -0700

    Fix backend issues with Jax and Numpy backends (#568)
    * Change import behavior on TF and PyTorch backends.
    
    * silence the linter
    
    * jax backend no long inherits numpy backend
    
    * Typo and test
    
    * whitespace
    
    * force numpy backend
    
    * change test to use pickle
    
    * better test
    
    * Pickle just isn't going to work wtih travis
    
    * remove pickle import
    
    * revert bad addition

--- tensornetwork/backends/jax/jax_backend.py
+++ tensornetwork/backends/jax/jax_backend.py
@@ -12,17 +12,22 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Any, Optional, Tuple, Callable, List, Text, Type
-from tensornetwork.backends.numpy import numpy_backend
+from typing import Any, Optional, Tuple, Callable, List, Text, Type, Sequence
+from tensornetwork.backends import base_backend
+from tensornetwork.backends.numpy import decompositions
 import numpy as np
 
 Tensor = Any
 
-
-class JaxBackend(numpy_backend.NumPyBackend):
+# pylint: disable=abstract-method
+class JaxBackend(base_backend.BaseBackend):
   """See base_backend.BaseBackend for documentation."""
 
   def __init__(self, dtype: Optional[np.dtype] = None):
+    # pylint: disable=global-variable-undefined
+    global libjax  # Jax module
+    global jnp  # jax.numpy module
+    global jsp  # jax.scipy module
     super(JaxBackend, self).__init__()
     try:
       #pylint: disable=import-outside-toplevel
@@ -30,14 +35,20 @@ class JaxBackend(numpy_backend.NumPyBackend):
     except ImportError:
       raise ImportError("Jax not installed, please switch to a different "
                         "backend or install Jax.")
-    self.jax = jax
-    self.np = self.jax.numpy
-    self.sp = self.jax.scipy
+    libjax = jax
+    jnp = libjax.numpy
+    jsp = libjax.scipy
     self.name = "jax"
     self._dtype = np.dtype(dtype) if dtype is not None else None
+  
+  def tensordot(self, a: Tensor, b: Tensor, axes: Sequence[Sequence[int]]):
+    return jnp.tensordot(a, b, axes)
+  
+  def reshape(self, tensor: Tensor, shape: Tensor):
+    return jnp.reshape(tensor, np.asarray(shape).astype(np.int32))
 
-  def convert_to_tensor(self, tensor: Tensor) -> Tensor:
-    return self.np.asarray(tensor)
+  def transpose(self, tensor, perm):
+    return jnp.transpose(tensor, perm)
 
   def shape_concat(self, values: Tensor, axis: int) -> Tensor:
     return np.concatenate(values, axis)
@@ -49,7 +60,92 @@ class JaxBackend(numpy_backend.NumPyBackend):
     if len(start_indices) != len(slice_sizes):
       raise ValueError("Lengths of start_indices and slice_sizes must be"
                        "identical.")
-    return self.jax.lax.dynamic_slice(tensor, start_indices, slice_sizes)
+    return libjax.lax.dynamic_slice(tensor, start_indices, slice_sizes)
+
+  def svd_decomposition(self,
+                        tensor: Tensor,
+                        split_axis: int,
+                        max_singular_values: Optional[int] = None,
+                        max_truncation_error: Optional[float] = None,
+                        relative: Optional[bool] = False
+                       ) -> Tuple[Tensor, Tensor, Tensor, Tensor]:
+    return decompositions.svd_decomposition(
+        jnp,
+        tensor,
+        split_axis,
+        max_singular_values,
+        max_truncation_error,
+        relative=relative)
+
+  def qr_decomposition(
+      self,
+      tensor: Tensor,
+      split_axis: int,
+  ) -> Tuple[Tensor, Tensor]:
+    return decompositions.qr_decomposition(jnp, tensor, split_axis)
+
+  def rq_decomposition(
+      self,
+      tensor: Tensor,
+      split_axis: int,
+  ) -> Tuple[Tensor, Tensor]:
+    return decompositions.rq_decomposition(jnp, tensor, split_axis)
+
+  def shape_tensor(self, tensor: Tensor) -> Tensor:
+    return tensor.shape
+
+  def shape_tuple(self, tensor: Tensor) -> Tuple[Optional[int], ...]:
+    return tensor.shape
+
+  def sparse_shape(self, tensor: Tensor) -> Tuple[Optional[int], ...]:
+    return self.shape_tuple(tensor)
+
+  def shape_prod(self, values: Tensor) -> Tensor:
+    return np.prod(values)
+
+  def sqrt(self, tensor: Tensor) -> Tensor:
+    return jnp.sqrt(tensor)
+
+  def diag(self, tensor: Tensor) -> Tensor:
+    if len(tensor.shape) != 1:
+      raise TypeError("Only one dimensional tensors are allowed as input")
+    return jnp.diag(tensor)
+
+  def convert_to_tensor(self, tensor: Tensor) -> Tensor:
+    if (not isinstance(tensor, jnp.ndarray) and
+        not jnp.isscalar(tensor)):
+      raise TypeError("Expected a `jnp.array` or scalar. Got {}".format(
+          type(tensor)))
+    result = jnp.asarray(tensor)
+    return result
+
+  def trace(self, tensor: Tensor) -> Tensor:
+    # Default np.trace uses first two axes.
+    return jnp.trace(tensor, axis1=-2, axis2=-1)
+
+  def outer_product(self, tensor1: Tensor, tensor2: Tensor) -> Tensor:
+    return jnp.tensordot(tensor1, tensor2, 0)
+
+  def einsum(self, expression: str, *tensors: Tensor) -> Tensor:
+    return jnp.einsum(expression, *tensors)
+
+  def norm(self, tensor: Tensor) -> Tensor:
+    return jnp.linalg.norm(tensor)
+
+  def eye(self, N, dtype: Optional[np.dtype] = None,
+          M: Optional[int] = None) -> Tensor:
+    dtype = dtype if dtype is not None else jnp.float64
+    return jnp.eye(N, M=M, dtype=dtype)
+
+  def ones(self, shape: Tuple[int, ...],
+           dtype: Optional[np.dtype] = None) -> Tensor:
+    dtype = dtype if dtype is not None else jnp.float64
+    return jnp.ones(shape, dtype=dtype)
+
+  def zeros(self, shape: Tuple[int, ...],
+            dtype: Optional[np.dtype] = None) -> Tensor:
+    dtype = dtype if dtype is not None else jnp.float64
+    return jnp.zeros(shape, dtype=dtype)
 
   def randn(self,
             shape: Tuple[int, ...],
@@ -57,7 +153,7 @@ class JaxBackend(numpy_backend.NumPyBackend):
             seed: Optional[int] = None) -> Tensor:
     if not seed:
       seed = np.random.randint(0, 2**63)
-    key = self.jax.random.PRNGKey(seed)
+    key = libjax.random.PRNGKey(seed)
 
     dtype = dtype if dtype is not None else np.dtype(np.float64)
 
@@ -65,21 +161,21 @@ class JaxBackend(numpy_backend.NumPyBackend):
       real_dtype = np.dtype(real_dtype)
       complex_dtype = np.dtype(complex_dtype)
 
-      key_2 = self.jax.random.PRNGKey(seed + 1)
+      key_2 = libjax.random.PRNGKey(seed + 1)
 
-      real_part = self.jax.random.normal(key, shape, dtype=real_dtype)
-      complex_part = self.jax.random.normal(key_2, shape, dtype=real_dtype)
+      real_part = libjax.random.normal(key, shape, dtype=real_dtype)
+      complex_part = libjax.random.normal(key_2, shape, dtype=real_dtype)
       unit = (
           np.complex64(1j)
           if complex_dtype == np.dtype(np.complex64) else np.complex128(1j))
       return real_part + unit * complex_part
 
-    if np.dtype(dtype) is np.dtype(self.np.complex128):
-      return cmplx_randn(dtype, self.np.float64)
-    if np.dtype(dtype) is np.dtype(self.np.complex64):
-      return cmplx_randn(dtype, self.np.float32)
+    if np.dtype(dtype) is np.dtype(jnp.complex128):
+      return cmplx_randn(dtype, jnp.float64)
+    if np.dtype(dtype) is np.dtype(jnp.complex64):
+      return cmplx_randn(dtype, jnp.float32)
 
-    return self.jax.random.normal(key, shape).astype(dtype)
+    return libjax.random.normal(key, shape).astype(dtype)
 
   def random_uniform(self,
                      shape: Tuple[int, ...],
@@ -88,7 +184,7 @@ class JaxBackend(numpy_backend.NumPyBackend):
                      seed: Optional[int] = None) -> Tensor:
     if not seed:
       seed = np.random.randint(0, 2**63)
-    key = self.jax.random.PRNGKey(seed)
+    key = libjax.random.PRNGKey(seed)
 
     dtype = dtype if dtype is not None else np.dtype(np.float64)
 
@@ -96,15 +192,15 @@ class JaxBackend(numpy_backend.NumPyBackend):
       real_dtype = np.dtype(real_dtype)
       complex_dtype = np.dtype(complex_dtype)
 
-      key_2 = self.jax.random.PRNGKey(seed + 1)
+      key_2 = libjax.random.PRNGKey(seed + 1)
 
-      real_part = self.jax.random.uniform(
+      real_part = libjax.random.uniform(
           key,
           shape,
           dtype=real_dtype,
           minval=boundaries[0],
           maxval=boundaries[1])
-      complex_part = self.jax.random.uniform(
+      complex_part = libjax.random.uniform(
           key_2,
           shape,
           dtype=real_dtype,
@@ -115,39 +211,76 @@ class JaxBackend(numpy_backend.NumPyBackend):
           if complex_dtype == np.dtype(np.complex64) else np.complex128(1j))
       return real_part + unit * complex_part
 
-    if np.dtype(dtype) is np.dtype(self.np.complex128):
-      return cmplx_random_uniform(dtype, self.np.float64)
-    if np.dtype(dtype) is np.dtype(self.np.complex64):
-      return cmplx_random_uniform(dtype, self.np.float32)
+    if np.dtype(dtype) is np.dtype(jnp.complex128):
+      return cmplx_random_uniform(dtype, jnp.float64)
+    if np.dtype(dtype) is np.dtype(jnp.complex64):
+      return cmplx_random_uniform(dtype, jnp.float32)
 
-    return self.jax.random.uniform(
+    return libjax.random.uniform(
         key, shape, minval=boundaries[0], maxval=boundaries[1]).astype(dtype)
 
-  def eigs(self,
-           A: Callable,
-           initial_state: Optional[Tensor] = None,
-           num_krylov_vecs: Optional[int] = 200,
-           numeig: Optional[int] = 1,
-           tol: Optional[float] = 1E-8,
-           which: Optional[Text] = 'LR',
-           maxiter: Optional[int] = None,
-           dtype: Optional[Type] = None) -> Tuple[List, List]:
-    raise NotImplementedError("Backend '{}' has not implemented eigs.".format(
-        self.name))
+  def conj(self, tensor: Tensor) -> Tensor:
+    return jnp.conj(tensor)
 
-  def eigsh_lanczos(
-      self,
-      A: Callable,
-      initial_state: Optional[Tensor] = None,
-      num_krylov_vecs: Optional[int] = 200,
-      numeig: Optional[int] = 1,
-      tol: Optional[float] = 1E-8,
-      delta: Optional[float] = 1E-8,
-      ndiag: Optional[int] = 20,
-      reorthogonalize: Optional[bool] = False) -> Tuple[List, List]:
-    raise NotImplementedError(
-        "Backend '{}' has not implemented eighs_lanczos.".format(self.name))
+  def eigh(self, matrix: Tensor) -> Tuple[Tensor, Tensor]:
+    return jnp.linalg.eigh(matrix)
+
+  def addition(self, tensor1: Tensor, tensor2: Tensor) -> Tensor:
+    return tensor1 + tensor2
+
+  def subtraction(self, tensor1: Tensor, tensor2: Tensor) -> Tensor:
+    return tensor1 - tensor2
+
+  def multiply(self, tensor1: Tensor, tensor2: Tensor) -> Tensor:
+    return tensor1 * tensor2
+
+  def divide(self, tensor1: Tensor, tensor2: Tensor) -> Tensor:
+    return tensor1 / tensor2
 
   def index_update(self, tensor: Tensor, mask: Tensor,
                    assignee: Tensor) -> Tensor:
-    return self.jax.ops.index_update(tensor, mask, assignee)
+    return libjax.ops.index_update(tensor, mask, assignee)
+
+  def inv(self, matrix: Tensor) -> Tensor:
+    if len(matrix.shape) > 2:
+      raise ValueError("input to numpy backend method `inv` has shape {}."
+                       " Only matrices are supported.".format(matrix.shape))
+    return jnp.linalg.inv(matrix)
+
+  def broadcast_right_multiplication(self, tensor1: Tensor, tensor2: Tensor):
+    if len(tensor2.shape) != 1:
+      raise ValueError("only order-1 tensors are allowed for `tensor2`,"
+                       " found `tensor2.shape = {}`".format(tensor2.shape))
+    return tensor1 * tensor2
+
+  def broadcast_left_multiplication(self, tensor1: Tensor, tensor2: Tensor):
+    if len(tensor1.shape) != 1:
+      raise ValueError("only order-1 tensors are allowed for `tensor1`,"
+                       " found `tensor1.shape = {}`".format(tensor1.shape))
+
+    t1_broadcast_shape = self.shape_concat(
+        [self.shape_tensor(tensor1), [1] * (len(tensor2.shape) - 1)], axis=-1)
+    return tensor2 * self.reshape(tensor1, t1_broadcast_shape)
+
+  def sin(self, tensor: Tensor):
+    return jnp.sin(tensor)
+
+  def cos(self, tensor: Tensor):
+    return jnp.cos(tensor)
+
+  def exp(self, tensor: Tensor):
+    return jnp.exp(tensor)
+
+  def log(self, tensor: Tensor):
+    return jnp.log(tensor)
+
+  def expm(self, matrix: Tensor):
+    if len(matrix.shape) != 2:
+      raise ValueError("input to numpy backend method `expm` has shape {}."
+                       " Only matrices are supported.".format(matrix.shape))
+    if matrix.shape[0] != matrix.shape[1]:
+      raise ValueError("input to numpy backend method `expm` only supports"
+                       " N*N matrix, {x}*{y} matrix is given".format(
+                           x=matrix.shape[0], y=matrix.shape[1]))
+    # pylint: disable=no-member
+    return jsp.linalg.expm(matrix)
