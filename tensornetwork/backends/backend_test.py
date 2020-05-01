c82faf47b44bb693558f124200b7ab39932e2fc5
mganahl <martin.ganahl@gmail.com>
2020-05-01 12:07:54 -0400

    fix test

--- tensornetwork/backends/backend_test.py
+++ tensornetwork/backends/backend_test.py
@@ -150,7 +150,10 @@ def test_basic_network_without_backends_raises_error():
     Node(np.ones((2, 2)), backend="tensorflow")
   with pytest.raises(ImportError):
     Node(np.ones((2, 2)), backend="pytorch")
+
+
 []
+
 
 def test_base_backend_name():
   backend = BaseBackend()
@@ -316,7 +319,7 @@ def test_base_backend_eigs_not_implemented():
 def test_base_backend_eigs_lanczos_not_implemented():
   backend = BaseBackend()
   with pytest.raises(NotImplementedError):
-    backend.eigsh_lanczos(np.ones((2, 2)))
+    backend.eigsh_lanczos(lambda x: x, [], np.ones((2)))
 
 
 def test_base_backend_addition_not_implemented():
