46a5a3e838e46abc9d611b4d64c0c114d5ba382a
mganahl <martin.ganahl@gmail.com>
2020-05-04 16:56:18 -0400

    added test

---
+++ tensornetwork/matrixproductstates/mpo_test.py
@@ -0,0 +1,8 @@
+import pytest
+import numpy as np
+import time
+from tensornetwork.backends import backend_factory
+from tensornetwork.matrixproductstates.finite_mps import FiniteMPS
+
+
+def test_base_mpo_init()
