1746f19070f7b1ee9c05be49b77c7fed2a103869
mganahl <martin.ganahl@gmail.com>
2020-05-04 17:29:45 -0400

    tests

--- tensornetwork/matrixproductstates/mpo_test.py
+++ tensornetwork/matrixproductstates/mpo_test.py
@@ -2,7 +2,25 @@ import pytest
 import numpy as np
 import time
 from tensornetwork.backends import backend_factory
-from tensornetwork.matrixproductstates.finite_mps import FiniteMPS
+from tensornetwork.matrixproductstates.mpo import FiniteMPO, BaseMPO
 
 
-def test_base_mpo_init()
+def test_base_mpo_init(backend):
+  mpo = BaseMPO(tensors=[], backend=backend)
+  assert mpo.backend.name == backend
+
+
+def test_finite_mpo_init(backend):
+  tensors = [np.random.rand(1, 5, 2, 2), np.random.rand(5, 1, 2, 2)]
+  mpo = FiniteMPO(tensors=tensors, backend=backend, name='test')
+  assert mpo.backend.name == backend
+  assert mpo.name == 'test'
+
+
+def test_finite_mpo_raises(backend):
+  tensors = [np.random.rand(2, 5, 2, 2), np.random.rand(5, 1, 2, 2)]
+  with pytest.raises(ValueError):
+    mpo = FiniteMPO(tensors=tensors, backend=backend)
+  tensors = [np.random.rand(1, 5, 2, 2), np.random.rand(5, 2, 2, 2)]
+  with pytest.raises(ValueError):
+    FiniteMPO(tensors=tensors, backend=backend)
