e5accb1c707065a180a85f1c56e95c53fff970b0
Ben Penchas <bpenchas@google.com>
2020-05-03 20:20:07 -0500

    Adding MPO layer

---
+++ tensornetwork/tn_keras/mpo.py
@@ -0,0 +1,168 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Layer # type: ignore
+from tensorflow.keras import activations
+from tensorflow.keras import initializers
+from typing import List, Optional, Text, Tuple
+import tensornetwork as tn
+from tensornetwork.network_components import Node
+import numpy as np
+import math
+
+
+class DenseMPO(Layer):
+  """Matrix Product Operator (MPO) TN layer.
+
+  Example:
+
+  ```python
+  # as first layer in a sequential model:
+  model = Sequential()
+  model.add(
+    DenseMPO(1024, num_nodes=4, bond_dim=8, activation='relu', input_dim=1024))
+  # now the model will take as input arrays of shape (*, 1024)
+  # and output arrays of shape (*, 1024)
+  # after the first layer, you don't need to specify
+  # the size of the input anymore:
+  model.add(DenseMPO(1024, num_nodes=4, bond_dim=8, activation='relu'))
+  ```
+
+  Args:
+    output_dim: Positive integer, dimensionality of the output space.
+    num_nodes: Positive integer, number of nodes in the MPO.
+      Note input_dim**(1. / num_nodes) and output_dim**(1. / num_nodes)
+      must both be round.
+    bond_dim: Positive integer, size of the intermediate dimension.
+    activation: Activation function to use.
+      If you don't specify anything, no activation is applied
+      (ie. "linear" activation: `a(x) = x`).
+    use_bias: Boolean, whether the layer uses a bias vector.
+    kernel_initializer: Initializer for the node weight matrices.
+    bias_initializer: Initializer for the bias vector.
+
+  Input shape:
+    2D tensor with shape: `(batch_size, input_dim)`.
+
+  Output shape:
+    2D tensor with shape: `(batch_size, output_dim)`.
+  """
+  def __init__(self,
+               output_dim: int,
+               num_nodes: int,
+               bond_dim: int,
+               use_bias: Optional[bool] = True,
+               activation: Optional[Text] = None,
+               kernel_initializer: Optional[Text] = 'glorot_uniform',
+               bias_initializer: Optional[Text] = 'zeros',
+               **kwargs) -> None:
+    if 'input_shape' not in kwargs and 'input_dim' in kwargs:
+      kwargs['input_shape'] = (kwargs.pop('input_dim'),)
+    self.output_dim = output_dim
+    self.num_nodes = num_nodes
+    self.bond_dim = bond_dim
+    self.nodes = []
+
+    self.use_bias = use_bias
+    self.activation = activations.get(activation)
+    self.kernel_initializer = initializers.get(kernel_initializer)
+    self.bias_initializer = initializers.get(bias_initializer)
+    super(DenseMPO, self).__init__(**kwargs)
+
+  def build(self, input_shape: List[int]) -> None:
+    # Disable the attribute-defined-outside-init violations in this function
+    # pylint: disable=attribute-defined-outside-init
+    if input_shape[-1] is None:
+      raise ValueError('The last dimension of the inputs to `Dense` '
+                       'should be defined. Found `None`.')
+
+    def is_perfect_root(n, n_nodes):
+      root = n**(1. / n_nodes)
+      return round(root)**n_nodes == n
+
+    # Ensure the MPO dimensions will work
+    assert is_perfect_root(input_shape[-1], self.num_nodes)
+    assert is_perfect_root(self.output_dim, self.num_nodes)
+
+    self.in_leg_dim = int(input_shape[-1]**(1. / self.num_nodes))
+    self.out_leg_dim = int(self.output_dim**(1. / self.num_nodes))
+
+    self.nodes.append(
+        self.add_weight(name='end_node_first',
+                        shape=(self.in_leg_dim, self.bond_dim,
+                               self.out_leg_dim),
+                        trainable=True,
+                        initializer=self.kernel_initializer))
+    for i in range(self.num_nodes - 2):
+      self.nodes.append(
+          self.add_weight(name=f'middle_node_{i}',
+                          shape=(self.in_leg_dim, self.bond_dim, self.bond_dim,
+                                 self.out_leg_dim),
+                          trainable=True,
+                          initializer=self.kernel_initializer))
+    self.nodes.append(
+        self.add_weight(name='end_node_last',
+                        shape=(self.in_leg_dim, self.bond_dim,
+                               self.out_leg_dim),
+                        trainable=True,
+                        initializer=self.kernel_initializer))
+
+    self.bias_var = self.add_weight(
+        name='bias',
+        shape=(self.output_dim,),
+        trainable=True,
+        initializer=self.bias_initializer) if self.use_bias else None
+    super(DenseMPO, self).build(input_shape)
+
+  def call(self, inputs: tf.Tensor) -> tf.Tensor:
+
+    def f(x: tf.Tensor, nodes: List[Node], num_nodes: int, in_leg_dim: int,
+          use_bias: bool, bias_var: tf.Tensor) -> tf.Tensor:
+      orig_shape = x.shape
+      l = [in_leg_dim] * num_nodes
+      input_reshaped = tf.reshape(x, tuple(l))
+      x_node = tn.Node(input_reshaped, name='xnode', backend="tensorflow")
+
+      tn_nodes = []
+      for i, v in enumerate(nodes):
+        tn_nodes.append(tn.Node(v, name=f'node_{i}', backend="tensorflow"))
+        # Connect every node to input node
+        x_node[i] ^ tn_nodes[i][0]
+
+      for i, _ in enumerate(tn_nodes):
+        try:
+          tn_nodes[i][2] ^ tn_nodes[i + 1][2]
+        except: # pylint: disable=W0702
+          pass
+        try:
+          tn_nodes[i][1] ^ tn_nodes[i + 1][1]
+        except: # pylint: disable=W0702
+          pass
+        try:
+          tn_nodes[i][2] ^ tn_nodes[i + 1][1]
+        except: # pylint: disable=W0702
+          pass
+
+      # The TN should now look like this
+      #   |     |    |
+      #   1 --- 2 --- ...
+      #    \   /    /
+      #      x
+
+      remaining_legs = [tn_nodes[i][-1] for i in range(len(tn_nodes))]
+      # Contract TN using optimal contraction order
+      res = tn.contractors.optimal(tn_nodes + [x_node], remaining_legs)
+
+      result = tf.reshape(res.tensor, orig_shape)
+      if use_bias:
+        result += bias_var
+
+      return result
+
+    result = tf.vectorized_map(
+        lambda vec: f(vec, self.nodes, self.num_nodes, self.in_leg_dim, self.
+                      use_bias, self.bias_var), inputs)
+    if self.activation is not None:
+      result = self.activation(result)
+    return tf.reshape(result, (-1, self.output_dim))
+
+  def compute_output_shape(self, input_shape: List[int]) -> Tuple[int, int]:
+    return (input_shape[0], self.output_dim)
