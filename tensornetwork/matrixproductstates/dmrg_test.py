7801e57fa2c21ab89b8eae195f8ac383e17ee818
mganahl <martin.ganahl@gmail.com>
2020-05-05 10:09:01 -0400

    dicstrings and checks

--- tensornetwork/matrixproductstates/dmrg_test.py
+++ tensornetwork/matrixproductstates/dmrg_test.py
@@ -1,5 +1,6 @@
 from tensornetwork import FiniteMPS
 from tensornetwork.matrixproductstates.dmrg import FiniteDMRG, BaseDMRG
+from tensornetwork.backends import backend_factory
 from tensornetwork.matrixproductstates.mpo import FiniteXXZ
 import pytest
 import numpy as np
@@ -33,7 +34,7 @@ def get_XXZ_Hamiltonian(N, Jx, Jy, Jz):
 
 
 def test_BaseDMRG_init(backend_dtype_values):
-  backend = backend_dtype_values[0]
+  backend = backend_factory.get_backend(backend_dtype_values[0])
   dtype = backend_dtype_values[1]
 
   N = 10
@@ -41,13 +42,96 @@ def test_BaseDMRG_init(backend_dtype_values):
   Jz = np.ones(N - 1)
   Jxy = np.ones(N - 1)
   Bz = np.zeros(N)
-  dtype = np.float64
   mpo = FiniteXXZ(Jz, Jxy, Bz, dtype=dtype, backend=backend)
   mps = FiniteMPS.random([2] * N, [D] * (N - 1), dtype=dtype, backend=backend)
   dmrg = BaseDMRG(mps, mpo, np.ones((1, 1, 1), dtype=dtype),
                   np.ones((1, 1, 1), dtype=dtype), 'name')
   assert dmrg.name == 'name'
-  assert dmrg.backend.name == backend
+  assert dmrg.backend is backend
+
+
+def test_BaseDMRG_raises():
+  numpy_backend = backend_factory.get_backend('numpy')
+  pytorch_backend = backend_factory.get_backend('pytorch')
+  dtype = np.float64
+  N = 10
+  D = 10
+  Jz = np.ones(N - 1)
+  Jxy = np.ones(N - 1)
+  Bz = np.zeros(N)
+  mpo = FiniteXXZ(Jz, Jxy, Bz, dtype=dtype, backend=numpy_backend)
+  mps = FiniteMPS.random(
+      [2] * (N - 1), [D] * (N - 2), dtype=dtype, backend=numpy_backend)
+  with pytest.raises(ValueError):
+    BaseDMRG(mps, mpo, numpy_backend.ones((1, 1, 1), dtype=dtype),
+             numpy_backend.ones((1, 1, 1), dtype=dtype), 'name')
+
+  mpo = FiniteXXZ(Jz, Jxy, Bz, dtype=np.float64, backend=numpy_backend)
+  mps = FiniteMPS.random(
+      [2] * N, [D] * (N - 1), dtype=np.float32, backend=numpy_backend)
+  with pytest.raises(
+      TypeError,
+      match="mps.dtype = {} is different from "
+      "mpo.dtype = {}".format(mps.dtype, mpo.dtype)):
+    BaseDMRG(mps, mpo, numpy_backend.ones((1, 1, 1), dtype=dtype),
+             numpy_backend.ones((1, 1, 1), dtype=dtype), 'name')
+
+  mpo = FiniteXXZ(Jz, Jxy, Bz, dtype=np.float64, backend=numpy_backend)
+  mps = FiniteMPS.random(
+      [2] * N, [D] * (N - 1), dtype=np.float64, backend=pytorch_backend)
+  with pytest.raises(TypeError, match="mps and mpo use different backends."):
+    BaseDMRG(mps, mpo, numpy_backend.ones((1, 1, 1), dtype=dtype),
+             numpy_backend.ones((1, 1, 1), dtype=dtype), 'name')
+
+
+def test_BaseDMRG_position(backend_dtype_values):
+  backend = backend_factory.get_backend(backend_dtype_values[0])
+  dtype = backend_dtype_values[1]
+
+  N = 10
+  D = 10
+  Jz = np.ones(N - 1)
+  Jxy = np.ones(N - 1)
+  Bz = np.zeros(N)
+  mpo = FiniteXXZ(Jz, Jxy, Bz, dtype=dtype, backend=backend)
+  mps = FiniteMPS.random([2] * N, [D] * (N - 1), dtype=dtype, backend=backend)
+  dmrg = BaseDMRG(mps, mpo, np.ones((1, 1, 1), dtype=dtype),
+                  np.ones((1, 1, 1), dtype=dtype), 'name')
+  dmrg.position(N - 1)
+  np.testing.assert_allclose(np.arange(N), sorted(list(dmrg.left_envs.keys())))
+  np.testing.assert_allclose([N - 1], list(dmrg.right_envs.keys()))
+  assert dmrg.mps.center_position == N - 1
+  dmrg.position(0)
+  np.testing.assert_allclose([0], list(dmrg.left_envs.keys()))
+  np.testing.assert_allclose(np.arange(N), sorted(list(dmrg.right_envs.keys())))
+  assert dmrg.mps.center_position == 0
+
+  with pytest.raises(IndexError, match="site > length of mps"):
+    dmrg.position(N)
+  with pytest.raises(IndexError, match="site < 0"):
+    dmrg.position(-1)
+
+
+def test_compute_envs(backend_dtype_values):
+  backend = backend_factory.get_backend(backend_dtype_values[0])
+  dtype = backend_dtype_values[1]
+
+  N = 10
+  D = 10
+  Jz = np.ones(N - 1)
+  Jxy = np.ones(N - 1)
+  Bz = np.zeros(N)
+  mpo = FiniteXXZ(Jz, Jxy, Bz, dtype=dtype, backend=backend)
+  mps = FiniteMPS.random([2] * N, [D] * (N - 1), dtype=dtype, backend=backend)
+  dmrg = BaseDMRG(mps, mpo, np.ones((1, 1, 1), dtype=dtype),
+                  np.ones((1, 1, 1), dtype=dtype), 'name')
+  dmrg.position(5)
+  dmrg.compute_left_envs()
+  dmrg.compute_right_envs()
+  np.testing.assert_allclose([0, 1, 2, 3, 4, 5],
+                             sorted(list(dmrg.left_envs.keys())))
+  np.testing.assert_allclose([5, 6, 7, 8, 9],
+                             sorted(list(dmrg.right_envs.keys())))
 
 
 @pytest.mark.parametrize("N", [4, 6, 7])
