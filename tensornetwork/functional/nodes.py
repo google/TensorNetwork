dedcc50317cea42b054d031bc64e1be2d861c19e
Chase Roberts <chaseriley@google.com>
2020-05-16 01:51:39 -0700

    Added better documentation and changed lazy behavior

--- tensornetwork/functional/nodes.py
+++ tensornetwork/functional/nodes.py
@@ -10,22 +10,30 @@ def no_duplicates(values):
       elements.add(i)
   return elements
 
-def merge_network_nodes(network1, network2):
-  """Merge two NetworkNodes together into a new NetworkNode."""
+def merge_lazy_networks(network1, network2):
+  """Merge two LazyNetworks together into a new LazyNetwork."""
   nodes = network1.nodes + network2.nodes
   axes_map = defaultdict(lambda: [])
   for axes, edges in network1.axes_map.items():
     axes_map[axes] += edges
   for axes, edges in network2.axes_map.items():
     axes_map[axes] += edges
-  return NetworkNode(nodes, axes_map)
+  return LazyNetwork(nodes, axes_map)
 
-class NetworkNode:
+class LazyNetwork:
+  """A lazy tensor network."""
   def __init__(self, nodes, axes_map):
     self.nodes = nodes
     self.axes_map = axes_map
 
   def contract(self, algo, axis_order):
+    """Contract the tensor network into a node.
+    Args:
+      algo: a contraction algorithm.
+      axis_order: Order of the output axes for this network.
+    Returns:
+      the tn.Node of the contracted network.
+    """
     node_dict, edge_dict = tn.copy(self.nodes)
     for edges in self.axes_map.values():
       if len(edges) != 2 and len(edges) != 1:
@@ -39,42 +47,54 @@ class NetworkNode:
     return algo(list(node_dict.values()), output_edge_order=edge_order) 
 
 class FunctionalNode:
+  """A functional Node. This Node is immutable unlike the standard tn.Node
+  """
   def __init__(self, tensor, axes=None):
+    """Create a FunctionalNode.
+
+    Args:
+      tensor: A tensor, tn.Node. or LazyNode object.
+      axes: An optional list of axes. Used to define
+        an initial axis order.
+    """
     if axes is None:
       self.node = tn.Node(tensor)
       self.axes_order = None
       return
-    if isinstance(tensor, NetworkNode):
-      self.network_node = tensor
+    if isinstance(tensor, LazyNetwork):
+      self.lazy_network = tensor
     else:
       node = tn.Node(tensor)
       axes_map = defaultdict(lambda: [])
       for axis, edge in zip(axes, node.edges):
         axes_map[axis] += [edge]
-      self.network_node = NetworkNode(
+      self.lazy_network = LazyNetwork(
           [node], 
           axes_map
       )
     self.axes_order = axes[:]
 
   def __call__(self, *axes_order):
+    """Generate a permutation of the given FuncationNode.
+
+    Args:
+      *axes_order: The new order of the axes.
+    Returns:
+      a new FunctionalNode with the given order.
+    """ 
     if self.axes_order is None:
       return FunctionalNode(self.node, axes_order)
-    assert set(axes_order) == set(self.axes_order)
-    node = self.network_node.contract(
-      tn.contractors.greedy,
-      axes_order)
-    return FunctionalNode(node, axes_order)
+    return FunctionalNode(self.lazy_network, axes_order)
 
   @property
   def tensor(self):
-    return self.network_node.contract(
+    return self.lazy_network.contract(
         tn.contractors.greedy, self.axes_order).tensor
 
   def __matmul__(self, other):
     left_axes = no_duplicates(self.axes_order)
     right_axes = no_duplicates(other.axes_order)
     new_axes_order = list(left_axes ^ right_axes)
-    new_network_node = merge_network_nodes(self.network_node, other.network_node)
-    return FunctionalNode(new_network_node, new_axes_order)
+    new_lazy_network = merge_lazy_networks(self.lazy_network, other.lazy_network)
+    return FunctionalNode(new_lazy_network, new_axes_order)
 
