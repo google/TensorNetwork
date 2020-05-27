1524f41b634c53d6b298a4b8e96237922ed5a0b5
Chase Roberts <chaseriley@google.com>
2020-05-26 20:11:51 -0700

    Better test

--- tensornetwork/tn_keras/mpo_test.py
+++ tensornetwork/tn_keras/mpo_test.py
@@ -13,11 +13,12 @@ import pytest
       [3, 4],
       [2, 3])
 )
-def test_shape_sanity_check(in_dim_base,dim1, dim2, num_nodes, bond_dim):
+def test_shape_sanity_check(in_dim_base, dim1, dim2, num_nodes, bond_dim):
   model = tf.keras.Sequential([
     tf.keras.Input(in_dim_base**num_nodes),
     mpo.DenseMPO(dim1**num_nodes, num_nodes=num_nodes, bond_dim=bond_dim), 
     mpo.DenseMPO(dim2**num_nodes, num_nodes=num_nodes, bond_dim=bond_dim),
   ])
-  
-  model.predict(np.ones((32, in_dim_base**num_nodes)))
+  # Hard code batch size.
+  result = model.predict(np.ones((32, in_dim_base**num_nodes)))
+  assert result.shape == (32, dim2 ** num_nodes)
