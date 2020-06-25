a57617d85fcc9d4f83456cbcb46df50cf446458f
mganahl <martin.ganahl@gmail.com>
2020-06-25 10:11:34 -0400

    wip

--- tensornetwork/backends/symmetric/symmetric_backend.py
+++ tensornetwork/backends/symmetric/symmetric_backend.py
@@ -19,6 +19,7 @@ from tensornetwork.block_sparse.index import Index
 from tensornetwork.block_sparse.blocksparsetensor import BlockSparseTensor
 import tensornetwork.block_sparse as bs
 import numpy
+
 Tensor = Any
 
 #TODO (mganahl): implement sparse solvers
@@ -102,7 +103,7 @@ class SymmetricBackend(abstract_backend.AbstractBackend):
     return tensor
 
   def trace(self, tensor: Tensor) -> Tensor:
-    # Default np.trace uses first two axes.
+    # Default numpy.trace uses first two axes.
     return self.bs.trace(tensor)
 
   def outer_product(self, tensor1: Tensor, tensor2: Tensor) -> Tensor:
@@ -162,6 +163,129 @@ class SymmetricBackend(abstract_backend.AbstractBackend):
 
   def eigh(self, matrix: Tensor) -> Tuple[Tensor, Tensor]:
     return self.bs.eigh(matrix)
+  
+  def eigsh_lanczos(self,
+                    A: Callable,
+                    args: Optional[List[Tensor]] = None,
+                    initial_state: Optional[Tensor] = None,
+                    shape: Optional[Tuple] = None,
+                    dtype: Optional[Type[numpy.number]] = None,
+                    num_krylov_vecs: int = 20,
+                    numeig: int = 1,
+                    tol: float = 1E-8,
+                    delta: float = 1E-8,
+                    ndiag: int = 20,
+                    reorthogonalize: bool = False) -> Tuple[List, List]:
+    """
+    Lanczos method for finding the lowest eigenvector-eigenvalue pairs
+    of a linear operator `A`.
+    Args:
+      A: A (sparse) implementation of a linear operator.
+         Call signature of `A` is `res = A(vector, *args)`, where `vector`
+         can be an arbitrary `Tensor`, and `res.shape` has to be `vector.shape`.
+      arsg: A list of arguments to `A`.  `A` will be called as
+        `res = A(initial_state, *args)`.
+      initial_state: An initial vector for the Lanczos algorithm. If `None`,
+        a random initial `Tensor` is created using the `backend.randn` method
+      shape: The shape of the input-dimension of `A`.
+      dtype: The dtype of the input `A`. If both no `initial_state` is provided,
+        a random initial state with shape `shape` and dtype `dtype` is created.
+      num_krylov_vecs: The number of iterations (number of krylov vectors).
+      numeig: The nummber of eigenvector-eigenvalue pairs to be computed.
+        If `numeig > 1`, `reorthogonalize` has to be `True`.
+      tol: The desired precision of the eigenvalus. Uses
+        `numpy.linalg.norm(eigvalsnew[0:numeig] - eigvalsold[0:numeig]) < tol`
+        as stopping criterion between two diagonalization steps of the
+        tridiagonal operator.
+      delta: Stopping criterion for Lanczos iteration.
+        If a Krylov vector :math: `x_n` has an L2 norm
+        :math:`\\lVert x_n\\rVert < delta`, the iteration
+        is stopped. It means that an (approximate) invariant subspace has
+        been found.
+      ndiag: The tridiagonal Operator is diagonalized every `ndiag` iterations
+        to check convergence.
+      reorthogonalize: If `True`, Krylov vectors are kept orthogonal by
+        explicit orthogonalization (more costly than `reorthogonalize=False`)
+    Returns:
+      (eigvals, eigvecs)
+       eigvals: A list of `numeig` lowest eigenvalues
+       eigvecs: A list of `numeig` lowest eigenvectors
+    """
+    if args is None:
+      args = []
+
+    if num_krylov_vecs < numeig:
+      raise ValueError('`num_krylov_vecs` >= `numeig` required!')
+
+    if numeig > 1 and not reorthogonalize:
+      raise ValueError(
+          "Got numeig = {} > 1 and `reorthogonalize = False`. "
+          "Use `reorthogonalize=True` for `numeig > 1`".format(numeig))
+    if initial_state is None:
+      if (shape is None) or (dtype is None):
+        raise ValueError("if no `initial_state` is passed, then `shape` and"
+                         "`dtype` have to be provided")
+      initial_state = self.randn(shape, dtype)
+
+    if not isinstance(initial_state, BlockSparseTensor):
+      raise TypeError("Expected a `BlockSparseTensor`. Got {}".format(
+          type(initial_state)))
+
+    vector_n = initial_state
+    Z = self.norm(vector_n)
+    vector_n /= Z
+    norms_vector_n = []
+    diag_elements = []
+    krylov_vecs = []
+    first = True
+    eigvalsold = []
+    for it in range(num_krylov_vecs):
+      #normalize the current vector:
+      norm_vector_n = self.norm(vector_n)
+      if abs(norm_vector_n) < delta:
+        break
+      norms_vector_n.append(norm_vector_n)
+      vector_n = vector_n / norms_vector_n[-1]
+      #store the Lanczos vector for later
+      if reorthogonalize:
+        for v in krylov_vecs:
+          v.tranpose_data() # make sure storage layouts are matching
+          vector_n -= numpy.dot(numpy.conj(v.data), vector_n.data) * v
+      krylov_vecs.append(vector_n)
+      A_vector_n = A(vector_n, *args)
+      diag_elements.append(
+          numpy.dot(numpy.ravel(numpy.conj(vector_n)), numpy.ravel(A_vector_n)))
+
+      if ((it > 0) and (it % ndiag) == 0) and (len(diag_elements) >= numeig):
+        #diagonalize the effective Hamiltonian
+        A_tridiag = numpy.diag(diag_elements) + numpy.diag(
+            norms_vector_n[1:], 1) + numpy.diag(numpy.conj(norms_vector_n[1:]), -1)
+        eigvals, u = numpy.linalg.eigh(A_tridiag)
+        if not first:
+          if numpy.linalg.norm(eigvals[0:numeig] - eigvalsold[0:numeig]) < tol:
+            break
+        first = False
+        eigvalsold = eigvals[0:numeig]
+      if it > 0:
+        A_vector_n -= (krylov_vecs[-1] * diag_elements[-1])
+        A_vector_n -= (krylov_vecs[-2] * norms_vector_n[-1])
+      else:
+        A_vector_n -= (krylov_vecs[-1] * diag_elements[-1])
+      vector_n = A_vector_n
+
+    A_tridiag = numpy.diag(diag_elements) + numpy.diag(
+        norms_vector_n[1:], 1) + numpy.diag(numpy.conj(norms_vector_n[1:]), -1)
+    eigvals, u = numpy.linalg.eigh(A_tridiag)
+    eigenvectors = []
+    if numpy.iscomplexobj(A_tridiag):
+      eigvals = numpy.array(eigvals).astype(A_tridiag.dtype)
+
+    for n2 in range(min(numeig, len(eigvals))):
+      state = self.zeros(initial_state.shape, initial_state.dtype)
+      for n1, vec in enumerate(krylov_vecs):
+        state += vec * u[n1, n2]
+      eigenvectors.append(state / numpy.linalg.norm(state))
+    return eigvals[0:numeig], eigenvectors
 
   def addition(self, tensor1: Tensor, tensor2: Tensor) -> Tensor:
     return tensor1 + tensor2
