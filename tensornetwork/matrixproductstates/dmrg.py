7e17bd98d5c7cf4d1d195a43a557a18a45d072b9
mganahl <martin.ganahl@gmail.com>
2020-06-26 14:46:26 -0400

    added compute_energy()

--- tensornetwork/matrixproductstates/dmrg.py
+++ tensornetwork/matrixproductstates/dmrg.py
@@ -277,17 +277,16 @@ class BaseDMRG:
     Returns:
       float: The energy upon termination of `run_one_site`.
     """
+    if num_sweeps == 0:
+      return self.compute_energy()
 
     converged = False
     final_energy = 1E100
     iteration = 1
     initial_site = 0
+
     self.mps.position(0)  #move center position to the left end
     self.compute_right_envs()
-    if num_sweeps == 0:
-      return ncon([self.add_right_layer(self.right_envs[0],
-                                        self.mps.tensors[0],
-                                        self.mpo.tensors[0])],[[1, 1, -1]])
 
     def print_msg(site):
       if verbose > 0:
@@ -347,6 +346,14 @@ class BaseDMRG:
                 "after {1} iterations".format(precision, num_sweeps))
         break
     return final_energy
+
+  def compute_energy(self):
+    self.mps.position(0)  #move center position to the left end
+    self.compute_right_envs()
+    return ncon([
+        self.add_right_layer(self.right_envs[0], self.mps.tensors[0],
+                             self.mpo.tensors[0])
+    ], [[1, 1, -1]])[0]
 
 
 class FiniteDMRG(BaseDMRG):
