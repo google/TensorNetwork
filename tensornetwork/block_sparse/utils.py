093c79443aab217c473b01006e3d74b665a0a20b
mganahl <martin.ganahl@gmail.com>
2020-07-09 09:10:04 -0400

    remove newlinw

--- tensornetwork/block_sparse/utils.py
+++ tensornetwork/block_sparse/utils.py
@@ -420,7 +420,6 @@ def _find_diagonal_sparse_blocks(
 
   # calculate number of non-zero elements in each row of the matrix
   row_ind = reduce_charges(charges[:partition], flows[:partition], block_qnums)
-
   row_num_nz = col_degen[col_to_block[row_ind.charge_labels]]
   cumulate_num_nz = np.insert(np.cumsum(row_num_nz[0:-1]), 0, 0).astype(SIZE_T)
   # calculate mappings for the position in datavector of each block
