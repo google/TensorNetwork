b9adeee74c250ac79df546f8af8aea7fcce3a4e6
Martin Ganahl <martin.ganahl@gmail.com>
2020-07-10 14:54:35 -0400

    fix bug in print_msg in dmrg (#691)
    * fix bug in print_msg in dmrg
    
    * linting
    
    * remove newline
    
    * docstring
    
    * remove jitting of ncon functions
    
    they are jitted internally now
    
    * add canonicalize to random
    
    * linting
    
    * wip
    
    * revert to old version
    
    * fix tests
    
    * linting
    
    * fix small bug with num_sweeps
    
    better print message
    
    * move code
    
    * linting
    
    * WIP
    
    * added compute_energy()
    
    * remove np.unique
    
    * shaving off a few more microns of constant overhead
    
    * fix typing
    
    * raise Error
    
    * remove future imports
    
    * remove future imports
    
    * remove future imports
    
    * remove future imports
    
    * shave off a few microns of constant overhead
    
    * remove time import
    
    * shorted code
    
    * typo
    
    * fix skipping of site bug
    
    * test added
    
    * test std output for dmrg
    
    * fix output stream
    
    * linting
    
    * add more Ns back
    
    * rename routine
    
    * revert code
    
    * linting
    
    * remove disable command
    
    * remove disable
    
    * remove disable
    
    * remove disable
    
    * linting
    
    * linting
    
    * linting
    
    * linting
    
    * linting
    
    * fix import
    
    * remove line
    
    Co-authored-by: Chase Roberts <chaseriley@google.com>

--- tensornetwork/matrixproductstates/dmrg_test.py
+++ tensornetwork/matrixproductstates/dmrg_test.py
@@ -159,6 +159,7 @@ def test_compute_envs(backend_dtype_values):
 
 @pytest.mark.parametrize("N", [4, 6, 7])
 def test_finite_DMRG_init(backend_dtype_values, N):
+  np.random.seed(16)
   backend = backend_dtype_values[0]
   dtype = backend_dtype_values[1]
   H = get_XXZ_Hamiltonian(N, 1, 1, 1)
@@ -175,3 +176,33 @@ def test_finite_DMRG_init(backend_dtype_values, N):
   dmrg = FiniteDMRG(mps, mpo)
   energy = dmrg.run_one_site(num_sweeps=4, num_krylov_vecs=10)
   np.testing.assert_allclose(energy, eta[0])
+
+
+def test_finite_DMRG_outstream(backend_dtype_values, capsys):
+  np.random.seed(16)
+  N = 6
+  backend = backend_dtype_values[0]
+  dtype = backend_dtype_values[1]
+  mpo = FiniteXXZ(
+      Jz=np.ones(N - 1),
+      Jxy=np.ones(N - 1),
+      Bz=np.zeros(N),
+      dtype=dtype,
+      backend=backend)
+  D = 32
+  mps = FiniteMPS.random([2] * N, [D] * (N - 1), dtype=dtype, backend=backend)
+  dmrg = FiniteDMRG(mps, mpo)
+  num_sweeps = 2
+  dmrg.run_one_site(
+      num_sweeps=num_sweeps, num_krylov_vecs=10, verbose=2, precision=1E-100)
+  out, _ = capsys.readouterr()
+  out = out.split('\n')
+  act = [o[:28] + '\n' for o in out]
+  act = ''.join(act[0:num_sweeps * (2 * N - 2)])
+
+  exp = ''.join([
+      f"SS-DMRG sweep={n}/{num_sweeps}, site={m}/{N}:\n"
+      for n in range(1, num_sweeps + 1)
+      for m in [0, 1, 2, 3, 4, 5, 4, 3, 2, 1]
+  ])
+  assert act == exp
