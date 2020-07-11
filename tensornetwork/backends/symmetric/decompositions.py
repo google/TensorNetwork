1ab68881b2dcb4c86e3c6155c18159e5ecec89d1
mganahl <martin.ganahl@gmail.com>
2020-07-11 09:25:20 -0400

    linting

--- tensornetwork/backends/symmetric/decompositions.py
+++ tensornetwork/backends/symmetric/decompositions.py
@@ -13,8 +13,10 @@
 # limitations under the License.
 """Tensor Decomposition Implementations."""
 from typing import Optional, Any, Tuple
-from tensornetwork.block_sparse.utils import (_find_transposed_diagonal_sparse_blocks, get_real_dtype, SIZE_T)
-from tensornetwork.block_sparse.blocksparsetensor import (BlockSparseTensor, ChargeArray)
+from tensornetwork.block_sparse.utils import (
+    _find_transposed_diagonal_sparse_blocks, get_real_dtype, SIZE_T)
+from tensornetwork.block_sparse.blocksparsetensor import (BlockSparseTensor,
+                                                          ChargeArray)
 from tensornetwork.block_sparse.index import Index
 import numpy as np
 import warnings
