a861333235e7fe829e3cd44eb0bf49dec9893d0e
Adam GM Lewis <adam.lws@gmail.com>
2020-07-15 14:23:06 -0400

    Linter error in pytorch backend

--- tensornetwork/backends/pytorch/pytorch_backend.py
+++ tensornetwork/backends/pytorch/pytorch_backend.py
@@ -360,4 +360,3 @@ class PyTorchBackend(abstract_backend.AbstractBackend):
     if (tensor1.ndim <= 1) or (tensor2.ndim <= 1):
       raise ValueError("inputs to `matmul` have to be a tensors of order > 1,")
     return torchlib.einsum('...ab,...bc->...ac', tensor1, tensor2)
-
