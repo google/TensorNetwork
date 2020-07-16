c11ad9ac3599910e81fa680318b922e0e54d65d7
Adam Lewis <adam.lws@gmail.com>
2020-07-16 13:28:10 -0400

    Jit test seeds (#730)
    * Add linalg directory and initialization code
    
    * Delete linalg for merge
    
    * Add docstrings and random seed to tests in decorators_test.py
    
    Co-authored-by: Chase Roberts <chaseriley@google.com>

--- tensornetwork/backends/decorators_test.py
+++ tensornetwork/backends/decorators_test.py
@@ -8,16 +8,22 @@ from tensornetwork import backends
 import tensornetwork
 
 def jittest_init(backend):
+  """
+  Helper to initialize data for the other Jit tests.
+  """
   backend_obj = backends.backend_factory.get_backend(backend)
   def fun(x, A, y):
     return backend_obj.multiply(x, backend_obj.multiply(A, y))
-  x = backend_obj.randn((4,))
-  y = backend_obj.randn((4,))
-  A = backend_obj.randn((4, 4))
+  x = backend_obj.randn((4,), seed=11)
+  y = backend_obj.randn((4,), seed=11)
+  A = backend_obj.randn((4, 4), seed=11)
   return (x, y, A, fun)
 
 
 def test_jit(backend):
+  """
+  Tests that tn.jit gives the right answer.
+  """
   x, y, A, fun = jittest_init(backend)
   fun_jit = tensornetwork.jit(fun, backend=backend)
   res1 = fun(x, A, y)
@@ -26,6 +32,9 @@ def test_jit(backend):
 
 
 def test_jit_ampersand(backend):
+  """
+  Tests that tn.jit gives the right answer when used as a decorator.
+  """
   x, y, A, fun = jittest_init(backend)
   @functools.partial(tensornetwork.jit, static_argnums=(3,), backend=backend)
   def fun_jit(x, A, y, dummy):
@@ -37,6 +46,9 @@ def test_jit_ampersand(backend):
 
 
 def test_jit_args(backend):
+  """
+  Tests that tn.jit gives the right answer when given extra arguments.
+  """
   x, y, A, fun = jittest_init(backend)
   fun_jit = tensornetwork.jit(fun, backend=backend)
   res1 = fun(x, A, y)
@@ -47,6 +59,10 @@ def test_jit_args(backend):
 
 
 def test_jit_backend_argnum_is_string(backend):
+  """
+  Tests that tn.jit gives the right answer when the backend is supplied
+  via backend_argnum as a string.
+  """
   x, y, A, fun = jittest_init(backend)
 
   @functools.partial(tensornetwork.jit, backend_argnum=3)
@@ -59,6 +75,10 @@ def test_jit_backend_argnum_is_string(backend):
 
 
 def test_jit_backend_argnum_is_obj(backend):
+  """
+  Tests that tn.jit gives the right answer when the backend is supplied
+  via backend_argnum as a backend object.
+  """
   x, y, A, fun = jittest_init(backend)
 
   @functools.partial(tensornetwork.jit, backend_argnum=3)
@@ -72,10 +92,14 @@ def test_jit_backend_argnum_is_obj(backend):
 
 
 def test_jit_backend_argnum_invalid(backend):
+  """
+  Tests that tn.jit raises ValueError when backend_argnum points to something
+  other than a backend.
+  """
   x, y, A, fun = jittest_init(backend)
 
   with pytest.raises(ValueError):
-    @functools.partial(tensornetwork.jit, backend_argnum=3, backend=backend)
+    @functools.partial(tensornetwork.jit, backend_argnum=3)
     def fun_jit(x, A, y, the_backend):
       _ = the_backend
       return fun(x, A, y)
@@ -83,6 +107,10 @@ def test_jit_backend_argnum_invalid(backend):
 
 
 def test_jit_backend_and_backend_obj_raises_error(backend):
+  """
+  Tests that tn.jit raises ValueError when backend_argnum and backend
+  are both specified.
+  """
   x, y, A, fun = jittest_init(backend)
 
   with pytest.raises(ValueError):
