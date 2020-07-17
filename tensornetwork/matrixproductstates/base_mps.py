eb99c38c850cb6cfcc6a03deb3a4b49a762fd067
Ryan Pederson <vgnc@vt.edu>
2020-07-17 16:17:00 -0700

    svd_decomposition
    NOTE: self.backend.jit(svd_decomposition) does not currently work.

--- tensornetwork/matrixproductstates/base_mps.py
+++ tensornetwork/matrixproductstates/base_mps.py
@@ -92,11 +92,12 @@ class BaseMPS:
     ########################################################################
     ##########       define functions for jitted operations       ##########
     ########################################################################
-    def svd_decomposition(tensor, max_singular_values = None,
-                          max_truncation_error = None, relative = False):
-      return self.backend.svd_decomposition(tensor, 2)
+    def svd_decomposition(tensor, max_singular_values=None):
+      return self.backend.svd_decomposition(tensor=tensor, split_axis=2,
+                                            max_singular_values=
+                                            max_singular_values)
 
-    self.svd_decomposition = self.backend.jit(svd_decomposition)
+    self.svd_decomposition = svd_decomposition
 
     def qr_decomposition(tensor):
       return self.backend.qr_decomposition(tensor, 2)
