a612ace838a07016f4e686ce3e10119473eb3c44
Martin Ganahl <martin.ganahl@gmail.com>
2020-07-24 13:02:20 -0400

    Blocksparse block-data caching (#737)
    * wip
    
    * WIP
    
    * fix test
    
    * change to row-major ordering
    
    * WIP
    
    * fixed tests
    
    * yapfing
    
    * yapfing
    
    * fix bug
    
    * fix tests
    
    * fix tests
    
    * remove future imports
    
    * fizx tests
    
    * fix bug
    
    * fix repr
    
    * remove future imports
    
    * fix tests
    
    * fix tests
    
    * fix tests
    
    * yapf
    
    * yapf
    
    * fix linter complaints
    
    * fix tests
    
    * add better caching support
    
    * add caching class
    
    * docstrings
    
    * doc
    
    * add caching imports, clean up code
    
    * add caching support
    
    * URL in one line
    
    * syntax error
    
    * caching support to eigsh_lanczos
    
    * comment
    
    * imports
    
    * fix caching
    
    * docstring
    
    * fix pytype
    
    * tests added
    
    * linting
    
    * tests updated
    
    * typing
    
    * more typing
    
    * silence the linter
    
    * fix test
    
    * remove hash
    
    * typo
    
    * doc
    
    * fix test
    
    * fix bug
    
    * fix bug
    
    * catch exceptions
    
    * fix cache emptying
    
    * fix typing
    
    * test added
    
    * add caching fto _find_diagonal_sparse_blocks
    
    * _find_diagonal_sparse_blocks -> _find_transposed_diagonal_sparse_blocks
    
    * test added
    
    * remove print statements
    
    * more extensive testing
    
    * typos fixed
    
    * remove superflouous code
    
    * ??
    
    * disable caching after tests
    
    * add abs to fix test
    
    * disable caching after test

--- tensornetwork/block_sparse/charge.py
+++ tensornetwork/block_sparse/charge.py
@@ -38,7 +38,6 @@ class BaseCharge:
   """
 
   class Iterator:
-
     def __init__(self, unique: np.ndarray, labels: np.ndarray):
       self.n = 0
       self.unique = unique
@@ -83,10 +82,8 @@ class BaseCharge:
         self.charge_labels = np.empty(0, dtype=label_dtype)
     else:
       self.charge_labels = np.asarray(charge_labels, dtype=label_dtype)
-
       self.unique_charges = charges.astype(charge_dtype)
-      self.charge_labels = charge_labels.astype(label_dtype)
-
+      
   @staticmethod
   def fuse(charge1, charge2):
     raise NotImplementedError("`fuse` has to be implemented in derived classes")
