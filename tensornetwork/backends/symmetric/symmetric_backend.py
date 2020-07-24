71eb6d85ac76255e499245643d85c84bc124a2d2
mganahl <martin.ganahl@gmail.com>
2020-07-24 14:32:31 -0400

    fix merge conflict

--- tensornetwork/backends/symmetric/symmetric_backend.py
+++ tensornetwork/backends/symmetric/symmetric_backend.py
@@ -165,7 +165,6 @@ class SymmetricBackend(abstract_backend.AbstractBackend):
   def eigh(self, matrix: Tensor) -> Tuple[Tensor, Tensor]:
     return self.bs.eigh(matrix)
 
-<<<<<<< HEAD
   def eigs(self,#pylint: disable=arguments-differ
            A: Callable,
            args: Optional[List] = None,
@@ -287,8 +286,6 @@ class SymmetricBackend(abstract_backend.AbstractBackend):
 
     return eta, eVs
 
-=======
->>>>>>> upstream/master
   def eigsh_lanczos(self, #pylint: disable=arguments-differ
                     A: Callable,
                     args: Optional[List[Tensor]] = None,
@@ -333,11 +330,7 @@ class SymmetricBackend(abstract_backend.AbstractBackend):
         to check convergence.
       reorthogonalize: If `True`, Krylov vectors are kept orthogonal by
         explicit orthogonalization (more costly than `reorthogonalize=False`)
-<<<<<<< HEAD
       enable_caching: If `True`, block-data during calls to `matvec` are cached
-=======
-      enable_caching: If `True`, block-data during calls to `matvec` is cached
->>>>>>> upstream/master
         for later reuse. Note: usually it is safe to enable_caching, unless 
         `matvec` uses matrix decompositions like SVD, QR, eigh, eig or similar.
         In this case, if one does a large number of krylov steps, this can lead 
