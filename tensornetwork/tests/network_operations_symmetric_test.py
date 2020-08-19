12715a7a7d4fe8a38cdd4fd9b0a4b02bedd5ad3a
Adam Lewis <adam.lws@gmail.com>
2020-08-18 21:15:32 -0400

    Tensor initialization (#781)
    * Add linalg directory and initialization code
    
    * Delete linalg for merge
    
    * Correct imports in numpy_backend.py (how did this pass tests?)
    
    * GMRES correctly handles non-matrix shapes
    
    * Handle non-matrix gmres in Jax
    
    * Ensure numpy gmres holds dtype fixed
    
    * Nuke shell backend
    
    * Remove shell from backend factory
    
    * Add the correct default 'optimize' value to abstract_backend.einsum
    
    * Migrate initialization code
    
    * Whoops
    
    * Test mistake
    
    * oops
    
    * oops
    
    Co-authored-by: Martin Ganahl <martin.ganahl@gmail.com>
    Co-authored-by: Chase Roberts <chaseriley@google.com>

--- tensornetwork/tests/network_operations_symmetric_test.py
+++ tensornetwork/tests/network_operations_symmetric_test.py
@@ -19,6 +19,8 @@ from tensornetwork.block_sparse import (U1Charge, BlockSparseTensor, Index,
                                         BaseCharge)
 from tensornetwork.block_sparse.charge import charge_equal
 from tensornetwork.block_sparse.blocksparse_utils import _find_diagonal_sparse_blocks #pylint: disable=line-too-long
+import tensornetwork.linalg
+import tensornetwork.linalg.node_linalg
 
 
 def get_random(shape, num_charges, dtype=np.float64):
@@ -146,7 +148,7 @@ def test_split_node_rq_unitarity(dtype, num_charges):
       get_square_matrix(50, num_charges, dtype=dtype), backend='symmetric')
   r, q = tn.split_node_rq(a, [a[0]], [a[1]])
   r[1] | q[0]
-  qbar = tn.conj(q)
+  qbar = tn.linalg.node_linalg.conj(q)
   q[1] ^ qbar[1]
   u1 = q @ qbar
   qbar[0] ^ q[0]
@@ -199,7 +201,7 @@ def test_split_node_qr_unitarity(dtype, num_charges):
       get_square_matrix(50, num_charges, dtype=dtype), backend='symmetric')
   q, r = tn.split_node_qr(a, [a[0]], [a[1]])
   r[0] | q[1]
-  qbar = tn.conj(q)
+  qbar = tn.linalg.node_linalg.conj(q)
   q[1] ^ qbar[1]
   u1 = q @ qbar
   qbar[0] ^ q[0]
@@ -251,7 +253,7 @@ def test_conj(dtype, num_charges):
   a = tn.Node(
       get_random((6, 7, 8, 9, 10), num_charges=num_charges, dtype=dtype),
       backend='symmetric')
-  abar = tn.conj(a)
+  abar = tn.linalg.node_linalg.conj(a)
   np.testing.assert_allclose(abar.tensor.data, a.backend.conj(a.tensor.data))
   assert np.all([
       charge_equal(abar.tensor._charges[n], a.tensor._charges[n])
@@ -267,7 +269,7 @@ def test_transpose(dtype, num_charges):
       get_random((6, 7, 8, 9, 10), num_charges=num_charges, dtype=dtype),
       backend='symmetric')
   order = [a[n] for n in reversed(range(5))]
-  transpa = tn.transpose(a, [4, 3, 2, 1, 0])
+  transpa = tn.linalg.node_linalg.transpose(a, [4, 3, 2, 1, 0])
   a.reorder_edges(order)
   np.testing.assert_allclose(a.tensor.data, transpa.tensor.data)
 
