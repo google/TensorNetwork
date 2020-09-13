974d33ca6e1362121a248b3e0dd55b1730d1751e
mganahl <martin.ganahl@gmail.com>
2020-09-13 22:24:51 +0200

    remove set_precision

--- tensornetwork/backends/jax/precision.py
+++ tensornetwork/backends/jax/precision.py
@@ -11,21 +11,14 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-JAX_PRECISION = ["DEFAULT"]
 
-
-def get_jax_precision(jax):
-  if JAX_PRECISION[0] == "DEFAULT":
+def get_jax_precision(jax, precision):
+  if precision == "DEFAULT":
     return jax.lax.Precision.DEFAULT
-  if JAX_PRECISION[0] == "HIGH":
+  if precision == "HIGH":
     return jax.lax.Precision.HIGH
-  if JAX_PRECISION[0] == "HIGHEST":
+  if precision == "HIGHEST":
     return jax.lax.Precision.HIGHEST
-  raise ValueError(f"found unknown value JAX_PRECISOIN={JAX_PRECISION}.")
+  raise ValueError(f"unknown value {precision}"
+                   f" for precision.")
 
-def set_jax_precision(value):
-  if value not in ("DEFAULT", "HIGH", "HIGHEST"):
-    raise ValueError(f'{value} is not a valid value'
-                     f' for JAX_PRECISION. Use "DEFAULT", "HIGH" '
-                     f'or "HIGHEST"')
-  JAX_PRECISION[0] = value
