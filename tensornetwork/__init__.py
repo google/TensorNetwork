f72aa3f79eecbecfe2a47786c4bd9eeddffcfeee
mganahl <martin.ganahl@gmail.com>
2020-09-14 12:11:00 +0200

    import configure_backend, rest_backend, get_backend at to level

--- tensornetwork/__init__.py
+++ tensornetwork/__init__.py
@@ -68,5 +68,6 @@ from tensornetwork.block_sparse.blocksparsetensor import ChargeArray
 from tensornetwork.block_sparse.index import Index
 from tensornetwork.block_sparse.charge import U1Charge, BaseCharge, Z2Charge
 from tensornetwork.block_sparse.charge import ZNCharge
-from tensornetwork.backends.jax.precision import (set_jax_precision,
-                                                  get_jax_precision)
+from tensornetwork.backends.backend_factory import (configure_backend,
+                                                    reset_backend,
+                                                    get_backend)
