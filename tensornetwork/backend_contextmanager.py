043e892ef02906171826b6653f331cedce15b14b
mganahl <martin.ganahl@gmail.com>
2020-09-14 12:12:03 +0200

    add backend configuration to set_default_backend

--- tensornetwork/backend_contextmanager.py
+++ tensornetwork/backend_contextmanager.py
@@ -2,7 +2,6 @@ from typing import Text, Union
 from tensornetwork.backends.abstract_backend import AbstractBackend
 from tensornetwork.backends import backend_factory
 
-
 class DefaultBackend():
   """Context manager for setting up backend for nodes"""
 
@@ -37,13 +36,16 @@ def get_default_backend():
   return _default_backend_stack.get_current_backend()
 
 
-def set_default_backend(backend: Union[Text, AbstractBackend]) -> None:
+def set_default_backend(backend: Union[Text, AbstractBackend],
+                        **kwargs) -> None:
   if _default_backend_stack.stack:
     raise AssertionError("The default backend should not be changed "
                          "inside the backend context manager")
   if not isinstance(backend, (Text, AbstractBackend)):
     raise ValueError("Item passed to set_default_backend "
                      "must be Text or BaseBackend")
+
+  backend_factory.configure_backend(backend, **kwargs)
   if isinstance(backend, Text) and backend not in backend_factory._BACKENDS:
     raise ValueError(f"Backend '{backend}' was not found.")
   _default_backend_stack.default_backend = backend
