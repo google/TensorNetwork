d327c1ae1201ed9d596dd150751d13194dc1829d
mganahl <martin.ganahl@gmail.com>
2020-09-21 18:14:48 +0200

    fix error

--- tensornetwork/backends/jax/jitted_functions.py
+++ tensornetwork/backends/jax/jitted_functions.py
@@ -97,8 +97,8 @@ def _generate_jitted_eigsh_lanczos(jax: types.ModuleType) -> Callable:
 
       def body_while(vals):
         pv, kv, _ = vals
-        pv, kv = iterative_classical_gram_schmidt(
-            pv, (i > jax.numpy.arange(ncv + 2))[:, None] * kv)
+        pv = iterative_classical_gram_schmidt(
+          pv, (i > jax.numpy.arange(ncv + 2))[:, None] * kv)
         return [pv, kv, False]
 
       def cond_while(vals):
