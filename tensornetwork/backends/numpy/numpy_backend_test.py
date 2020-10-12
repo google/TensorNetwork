0805faf10e6cb9a21cc423979f62f9a38ed905bf
William Kostuch <37635286+wkostuch@users.noreply.github.com>
2020-10-12 10:32:03 -0500

    Add power method and test to numpy backend. (#849)

--- tensornetwork/backends/numpy/numpy_backend_test.py
+++ tensornetwork/backends/numpy/numpy_backend_test.py
@@ -949,3 +949,17 @@ def test_serialize(dtype):
   s = backend.serialize_tensor(tensor)
   assert isinstance(s, str)
   assert (tensor == backend.deserialize_tensor(s)).all()
+
+@pytest.mark.parametrize('dtype', np_dtypes)
+def test_power(dtype):
+  shape = (4, 3, 2)
+  backend = numpy_backend.NumPyBackend()
+  base_tensor = np.abs(backend.randn(shape, dtype=dtype, seed=10))
+  power_tensor = backend.randn(shape, dtype=dtype, seed=10)
+  actual = backend.power(base_tensor, power_tensor)
+  expected = np.power(base_tensor, power_tensor)
+  np.testing.assert_allclose(expected, actual)
+  power = np.random.rand(1)[0]
+  actual = backend.power(base_tensor, power)
+  expected = np.power(base_tensor, power)
+  np.testing.assert_allclose(expected, actual)
