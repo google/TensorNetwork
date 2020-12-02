d329d57ff84bc36f422597a358774f0e6e5b71b1
maga <martin.ganahl@gmail.com>
2020-12-02 10:12:29 +0100

    appease linter

--- tensornetwork/backends/jax/jax_backend_test.py
+++ tensornetwork/backends/jax/jax_backend_test.py
@@ -823,7 +823,7 @@ def get_ham_params(dtype, N, which):
     if dtype in (np.complex128, np.complex64):
       hop -= 1j * jnp.ones(N - 1, dtype)
   elif which == 'rand':
-    hop = -jnp.array(np.random.rand(N - 1).astype(dtype)-0.5)
+    hop = (-1) * jnp.array(np.random.rand(N - 1).astype(dtype) - 0.5)
     pot = jnp.array(np.random.rand(N).astype(dtype))-0.5
     if dtype in (np.complex128, np.complex64):
       hop -= 1j * jnp.array(np.random.rand(N - 1).astype(dtype)-0.5)
