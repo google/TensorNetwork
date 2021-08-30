44403cdd77d1b3dbe2cdf44f1b327a5c2fa10ff8
Ayush Parikh <ayushparikh332@gmail.com>
2021-08-30 18:11:10 +0530

    Update decompositions.py

--- tensornetwork/backends/pytorch/decompositions.py
+++ tensornetwork/backends/pytorch/decompositions.py
@@ -15,7 +15,11 @@
 
 from typing import Optional, Tuple, Any
 import numpy as np
-
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import tensorly as tl
+from tensorly.decomposition import tucker, partial_tucker
 Tensor = Any
 
 
@@ -219,3 +223,59 @@ def rq(
   r = torch.reshape(r, list(left_dims) + [center_dim])
   q = torch.reshape(q, [center_dim] + list(right_dims))
   return r, q
+
+
+
+def tucker_decomp(layer, rank):
+     
+"""Tucker Decomposition for Convolutional layers 
+The Tucker decomposition can be seen as a generalisation of the CP decomposition: it decomposes the tensor into a small core tensor and factor matrices. CP can be seen as a Tucker decomposition with a super-diagonal core.
+A tensor in its decomposed Tucker form is therefore nothing more than a core tensor with the same order as the original tensor and a list of projection matrices, one for each mode of the core tensor.
+
+Args:
+    tf: The tensorflow module.
+    tensor: A tensor to be decomposed.
+    layer : convolutional layers
+    rank , first_layer, core_layer, last_layer
+    
+Returns:
+    new_layers 
+     
+"""
+
+    W = layer.weight.data
+
+    core, [last, first] = partial_tucker(W, modes=[0,1], ranks=rank, init='svd')
+
+    first_layer = nn.Conv2d(in_channels=first.shape[0],
+                                       out_channels=first.shape[1],
+                                       kernel_size=1,
+                                       padding=0,
+                                       bias=False)
+
+    core_layer = nn.Conv2d(in_channels=core.shape[1],
+                                       out_channels=core.shape[0],
+                                       kernel_size=layer.kernel_size,
+                                       stride=layer.stride,
+                                       padding=layer.padding,
+                                       dilation=layer.dilation,
+                                       bias=False)
+
+    last_layer = nn.Conv2d(in_channels=last.shape[1],
+                                       out_channels=last.shape[0],
+                                       kernel_size=1,
+                                       padding=0,
+                                       bias=True)
+
+    if layer.bias is not None:
+        last_layer.bias.data = layer.bias.data
+
+    fk = first.t_().unsqueeze_(-1).unsqueeze_(-1)
+    lk = last.unsqueeze_(-1).unsqueeze_(-1)
+
+    first_layer.weight.data = fk
+    last_layer.weight.data = lk
+    core_layer.weight.data = core
+
+    new_layers = [first_layer, core_layer, last_layer]
+    return new_layers
