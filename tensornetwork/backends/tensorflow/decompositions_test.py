158430f3e82dad98367e5f47d8eb8a30c28354ff
prat1999 <47169803+prat1999@users.noreply.github.com>
2021-08-31 00:41:20 +0530

    Update decompositions_test.py

--- tensornetwork/backends/tensorflow/decompositions_test.py
+++ tensornetwork/backends/tensorflow/decompositions_test.py
@@ -73,7 +73,8 @@ class DecompositionsTest(tf.test.TestCase):
     unitary1, _, unitary2 = np.linalg.svd(random_matrix)
     singular_values = np.array(range(10))
     val = unitary1.dot(np.diag(singular_values).dot(unitary2.T))
-    u, s, vh, trun = decompositions.svd(tf, val, 1, max_singular_values=7)
+    u, s, vh, trun = decompositions.svd(
+      tf, val, 1, max_singular_values=7)
     self.assertEqual(u.shape, (10, 7))
     self.assertEqual(s.shape, (7,))
     self.assertAllClose(s, np.arange(9, 2, -1))
